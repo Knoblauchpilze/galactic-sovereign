2c0e0c50fb6e8f686d3e4b69da0e2d7a6060f98e
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-03-10 19:23:58 +0100

    feat: Added response envelope middleware autimatically wrapping the responses in an envelope and fixed an issue in which routes are set-up

---
+++ pkg/middleware/response_envelope.go
@@ -0,0 +1,74 @@
+package middleware
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/KnoblauchPilze/user-service/pkg/logger"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type responseEnvelope struct {
+	RequestId uuid.UUID
+	Status    string
+	Details   json.RawMessage `json:",omitempty"`
+}
+
+type envelopeResponseWriter struct {
+	response responseEnvelope
+	writer   http.ResponseWriter
+
+	logger echo.Logger
+}
+
+const RequestIdKey = "requestIdKey"
+
+func ResponseEnvelope() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			id := uuid.Must(uuid.NewRandom())
+			log := logger.New(id.String())
+
+			c.Set(RequestIdKey, id)
+			c.SetLogger(log)
+
+			w := &envelopeResponseWriter{
+				response: responseEnvelope{
+					RequestId: id,
+				},
+				writer: c.Response().Writer,
+				logger: log,
+			}
+			c.Response().Writer = w
+
+			next(c)
+
+			return nil
+		}
+	}
+}
+
+func (erw *envelopeResponseWriter) Header() http.Header {
+	return erw.writer.Header()
+}
+
+func (erw *envelopeResponseWriter) Write(data []byte) (int, error) {
+	erw.response.Details = data
+	out, err := json.Marshal(erw.response)
+	if err != nil {
+		erw.logger.Warnf("Failed to write data %s (err: %v), no response envelope", string(data), err)
+		return erw.writer.Write(data)
+	}
+
+	return erw.writer.Write(out)
+}
+
+func (erw *envelopeResponseWriter) WriteHeader(statusCode int) {
+	if statusCode < 200 || statusCode > 299 {
+		erw.response.Status = "ERROR"
+	} else {
+		erw.response.Status = "SUCCESS"
+	}
+	erw.writer.WriteHeader(statusCode)
+}
