bd02829ee0da65556156e8ea1167011dc308304e
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-03-10 09:52:00 +0100

    feat: Versioning of the API and constants

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/KnoblauchPilze/user-service/cmd/server/routes"
 )
 
+const ENDPOINT = "users"
+const PORT = uint16(60000)
+const VERSION = 1
+
 func main() {
-	s := routes.NewServer("/users", uint16(60000))
+	endpoint := fmt.Sprintf("/v%d/%s", VERSION, ENDPOINT)
+	s := routes.NewServer(endpoint, PORT)
 
 	s.Register(routes.UserRoutes())
 
