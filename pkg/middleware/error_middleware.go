471b38b88884ed8c29522ca192d2f64898298af4
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-03-11 22:25:47 +0100

    feat: Handling of errors with code and created a middleware to propagate them as http errors

---
+++ pkg/middleware/error_middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/labstack/echo/v4"
+)
+
+func ErrorMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			err := next(c)
+			if err == nil {
+				return nil
+			}
+
+			if impl, ok := err.(errors.ErrorWithCode); ok {
+				httpCode := errorCodeToHttpErrorCode(impl.Code())
+				return echo.NewHTTPError(httpCode, impl)
+			}
+
+			return err
+		}
+	}
+}
+
+func errorCodeToHttpErrorCode(code errors.ErrorCode) int {
+	switch code {
+	default:
+		return http.StatusInternalServerError
+	}
+}
