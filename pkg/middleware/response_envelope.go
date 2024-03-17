35d5316f082435dbd910a3c7baa3da5cfaed4d1f
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-03-17 20:24:56 +0100

    feat: Extracted response envelope writer into its own unit

--- pkg/middleware/response_envelope.go
+++ pkg/middleware/response_envelope.go
@@ -1,26 +1,10 @@
 package middleware
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/KnoblauchPilze/user-service/pkg/logger"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
-
-type responseEnvelope struct {
-	RequestId uuid.UUID
-	Status    string
-	Details   json.RawMessage `json:",omitempty"`
-}
-
-type envelopeResponseWriter struct {
-	response responseEnvelope
-	writer   http.ResponseWriter
-
-	logger echo.Logger
-}
 
 const RequestIdKey = "requestIdKey"
 
@@ -33,40 +17,10 @@ func ResponseEnvelope() echo.MiddlewareFunc {
 			c.Set(RequestIdKey, id)
 			c.SetLogger(log)
 
-			w := &envelopeResponseWriter{
-				response: responseEnvelope{
-					RequestId: id,
-				},
-				writer: c.Response().Writer,
-				logger: log,
-			}
+			w := new(c.Response().Writer)
 			c.Response().Writer = w
 
 			return next(c)
 		}
 	}
-}
-
-func (erw *envelopeResponseWriter) Header() http.Header {
-	return erw.writer.Header()
-}
-
-func (erw *envelopeResponseWriter) Write(data []byte) (int, error) {
-	erw.response.Details = data
-	out, err := json.Marshal(erw.response)
-	if err != nil {
-		erw.logger.Warnf("Failed to write data %s (err: %v), no response envelope", string(data), err)
-		return erw.writer.Write(data)
-	}
-
-	return erw.writer.Write(out)
-}
-
-func (erw *envelopeResponseWriter) WriteHeader(statusCode int) {
-	if statusCode < 200 || statusCode > 299 {
-		erw.response.Status = "ERROR"
-	} else {
-		erw.response.Status = "SUCCESS"
-	}
-	erw.writer.WriteHeader(statusCode)
 }
