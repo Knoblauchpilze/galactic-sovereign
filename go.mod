61e481d40317f094550e1414119c6923835095e9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-03-19 23:19:50 +0100

    feat: Added config and skeleton for db connection

--- go.mod
+++ go.mod
@@ -10,10 +10,13 @@ require (
 	github.com/stretchr/testify v1.9.0
 )
 
+require github.com/pkg/errors v0.9.1 // indirect
+
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
---
+++ pkg/db/config_test.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig_ToConnPoolConfig(t *testing.T) {
+	assert := assert.New(t)
+
+	c := Config{
+		Host:     "host",
+		Port:     36,
+		Database: "db",
+		User:     "user",
+		Password: "password",
+
+		ConnectionsPoolSize: 2,
+	}
+
+	actual := c.toConnPoolConfig()
+
+	assert.Equal("host", actual.Host)
+	assert.Equal(uint16(36), actual.Port)
+	assert.Equal("db", actual.Database)
+	assert.Equal("user", actual.User)
+	assert.Equal("password", actual.Password)
+
+	assert.Equal(2, actual.MaxConnections)
+	assert.Equal(time.Duration(0), actual.AcquireTimeout)
+}
