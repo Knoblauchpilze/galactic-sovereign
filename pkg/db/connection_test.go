f9d5c044b133bcd9c1410144574e74869f881ef8
KnoblauchPilze <prolific-programmer@tuta.com>
2024-03-21 22:27:37 +0100

    feat: Coverage

--- pkg/db/connection_test.go
+++ pkg/db/connection_test.go
@@ -55,6 +55,36 @@ func TestConnection_ConnectPropagatesError(t *testing.T) {
 	assert.Equal(errDefault, err)
 }
 
+type mockDbConnection struct {
+	closeCalled int
+}
+
+func (m *mockDbConnection) Close() {
+	m.closeCalled++
+}
+
+func (m *mockDbConnection) Query(sql string, args ...interface{}) (*pgx.Rows, error) {
+	return nil, nil
+}
+
+func (m *mockDbConnection) Exec(sql string, arguments ...interface{}) (pgx.CommandTag, error) {
+	return pgx.CommandTag(""), nil
+}
+
+func TestConnection_CloseReleasesTheDbConnection(t *testing.T) {
+	assert := assert.New(t)
+	t.Cleanup(resetDefaultConnectionFunc)
+
+	m := &mockDbConnection{}
+	conn := connectionImpl{
+		pool: m,
+	}
+
+	conn.Close()
+
+	assert.Equal(1, m.closeCalled)
+}
+
 func resetDefaultConnectionFunc() {
 	pgxConnectionFunc = pgx.NewConnPool
 }
