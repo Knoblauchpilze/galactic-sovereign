767adadf733e31d962ce59db35777f17a62b1ab4
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-03-21 22:18:29 +0100

    feat: Rows to traverse results from a sql query and tests

---
+++ pkg/db/errors.go
@@ -0,0 +1,10 @@
+package db
+
+import (
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+)
+
+const (
+	NoMatchingSqlRows          errors.ErrorCode = 100
+	MoreThanOneMatchingSqlRows errors.ErrorCode = 101
+)
