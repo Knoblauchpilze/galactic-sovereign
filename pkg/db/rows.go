a7d5b042f534b63badbcf0221faacdbc90439756
KnoblauchPilze <prolific-programmer@tuta.com>
2024-03-21 22:18:29 +0100

    feat: Rows to traverse results from a sql query and tests

---
+++ pkg/db/rows.go
@@ -0,0 +1,103 @@
+package db
+
+import (
+	"github.com/KnoblauchPilze/user-service/pkg/common"
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+)
+
+type Rows interface {
+	Err() error
+	Empty() bool
+
+	Close()
+
+	GetSingleValue(parser RowParser) error
+	GetAll(parser RowParser) error
+}
+
+type Scannable interface {
+	Scan(dest ...interface{}) error
+}
+
+type RowParser interface {
+	ScanRow(row Scannable) error
+}
+
+type sqlRows interface {
+	Next() bool
+	Scan(dest ...interface{}) error
+	Close()
+}
+
+type rowsImpl struct {
+	rows sqlRows
+	next bool
+	err  error
+}
+
+func newRows(rows sqlRows, err error) Rows {
+	r := rowsImpl{
+		rows: rows,
+		err:  err,
+	}
+
+	if !common.IsInterfaceNil(r.rows) && r.err == nil {
+		r.next = r.rows.Next()
+	}
+
+	return &r
+}
+
+func (r *rowsImpl) Err() error {
+	return r.err
+}
+
+func (r *rowsImpl) Empty() bool {
+	return r.rows == nil || !r.next
+}
+
+func (r *rowsImpl) Close() {
+	if r.rows != nil {
+		r.rows.Close()
+	}
+}
+
+func (r *rowsImpl) GetSingleValue(parser RowParser) error {
+	if err := r.Err(); err != nil {
+		return err
+	}
+	if r.Empty() {
+		return errors.NewCode(NoMatchingSqlRows)
+	}
+
+	defer r.Close()
+
+	if err := parser.ScanRow(r.rows); err != nil {
+		return err
+	}
+
+	r.next = r.rows.Next()
+	if r.next {
+		return errors.NewCode(MoreThanOneMatchingSqlRows)
+	}
+
+	return nil
+}
+
+func (r *rowsImpl) GetAll(parser RowParser) error {
+	if err := r.Err(); err != nil {
+		return err
+	}
+
+	defer r.Close()
+
+	for r.next {
+		if err := parser.ScanRow(r.rows); err != nil {
+			return err
+		}
+
+		r.next = r.rows.Next()
+	}
+
+	return nil
+}
---
+++ pkg/db/rows_test.go
@@ -0,0 +1,291 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRows_Err_NoError(t *testing.T) {
+	assert := assert.New(t)
+
+	r := newRows(nil, nil)
+	assert.Nil(r.Err())
+}
+
+func TestRows_Err_SomeError(t *testing.T) {
+	assert := assert.New(t)
+
+	r := newRows(nil, errDefault)
+	assert.Equal(errDefault, r.Err())
+}
+
+func TestRows_Close_DoesNotPanicWhenRowsIsNil(t *testing.T) {
+	assert := assert.New(t)
+
+	r := newRows(nil, nil)
+	assert.NotPanics(r.Close)
+}
+
+func TestRows_Close_ClosesRows(t *testing.T) {
+	assert := assert.New(t)
+
+	m := mockSqlRows{}
+	r := newRows(&m, nil)
+	r.Close()
+	assert.Equal(1, m.closeCalls)
+}
+
+type mockSqlRows struct {
+	row        int
+	rowsCount  int
+	scanError  error
+	closeCalls int
+}
+
+func (m *mockSqlRows) Next() bool {
+	out := m.row < m.rowsCount
+	m.row++
+	return out
+}
+
+func (m *mockSqlRows) Scan(dest ...interface{}) error {
+	return m.scanError
+}
+
+func (m *mockSqlRows) Close() {
+	m.closeCalls++
+}
+
+func TestRows_Empty_NoRows(t *testing.T) {
+	assert := assert.New(t)
+
+	r := newRows(nil, nil)
+	assert.True(r.Empty())
+}
+
+func TestRows_Empty_EmptyRows(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{}
+	r := newRows(mr, nil)
+	assert.True(r.Empty())
+}
+
+func TestRows_Empty_SomeRows(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 1,
+	}
+	r := newRows(mr, nil)
+	assert.False(r.Empty())
+}
+
+type mockParser struct {
+	scanCalled int
+	scanErr    error
+}
+
+func (m *mockParser) ScanRow(row Scannable) error {
+	m.scanCalled++
+	return m.scanErr
+}
+
+func TestRows_GetSingleValue_WhenError_Fails(t *testing.T) {
+	assert := assert.New(t)
+
+	mp := &mockParser{}
+
+	r := newRows(nil, errDefault)
+	err := r.GetSingleValue(mp)
+	assert.Equal(errDefault, err)
+	assert.Equal(0, mp.scanCalled)
+}
+
+func TestRows_GetSingleValue_WhenNilRows_Fails(t *testing.T) {
+	assert := assert.New(t)
+
+	mp := &mockParser{}
+
+	r := newRows(nil, nil)
+	err := r.GetSingleValue(mp)
+	assert.True(errors.IsErrorWithCode(err, NoMatchingSqlRows))
+	assert.Equal(0, mp.scanCalled)
+}
+
+func TestRows_GetAll_WhenNoRows_Fails(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{}
+	mp := &mockParser{}
+
+	r := newRows(mr, nil)
+	err := r.GetSingleValue(mp)
+
+	assert.True(errors.IsErrorWithCode(err, NoMatchingSqlRows))
+	assert.Equal(0, mr.closeCalls)
+	assert.Equal(0, mp.scanCalled)
+}
+
+func TestRows_GetSingleValue_WhenRows_Succeeds(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 1,
+	}
+	mp := &mockParser{}
+
+	r := newRows(mr, nil)
+	err := r.GetSingleValue(mp)
+
+	assert.Nil(err)
+	assert.Equal(1, mp.scanCalled)
+}
+
+func TestRows_GetSingleValue_CallsClose(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 1,
+	}
+	mp := &mockParser{}
+
+	r := newRows(mr, nil)
+	r.GetSingleValue(mp)
+
+	assert.Equal(1, mr.closeCalls)
+}
+
+func TestRows_GetSingleValue_ParserError(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 1,
+	}
+	mp := &mockParser{
+		scanErr: errDefault,
+	}
+
+	r := newRows(mr, nil)
+	err := r.GetSingleValue(mp)
+
+	assert.Equal(errDefault, err)
+	assert.Equal(1, mr.closeCalls)
+	assert.Equal(1, mp.scanCalled)
+}
+
+func TestRows_GetSingleValue_WithMultipleValues(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 2,
+	}
+	mp := &mockParser{}
+
+	r := newRows(mr, nil)
+	err := r.GetSingleValue(mp)
+
+	assert.True(errors.IsErrorWithCode(err, MoreThanOneMatchingSqlRows))
+	assert.Equal(1, mr.closeCalls)
+	assert.Equal(1, mp.scanCalled)
+}
+
+func TestRows_GetAll_WhenError_Fails(t *testing.T) {
+	assert := assert.New(t)
+
+	mp := &mockParser{}
+
+	r := newRows(nil, errDefault)
+	err := r.GetAll(mp)
+	assert.Equal(errDefault, err)
+	assert.Equal(0, mp.scanCalled)
+}
+
+func TestRows_GetAll_WhenNilRows_Succeeds(t *testing.T) {
+	assert := assert.New(t)
+
+	mp := &mockParser{}
+
+	r := newRows(nil, nil)
+	err := r.GetAll(mp)
+
+	assert.Nil(err)
+	assert.Equal(0, mp.scanCalled)
+}
+
+func TestRows_GetAll_WhenNoRows_Succeeds(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{}
+	mp := &mockParser{}
+
+	r := newRows(mr, nil)
+	err := r.GetAll(mp)
+
+	assert.Nil(err)
+	assert.Equal(1, mr.closeCalls)
+	assert.Equal(0, mp.scanCalled)
+}
+
+func TestRows_GetAll_WhenRows_Succeeds(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 2,
+	}
+	mp := &mockParser{}
+
+	r := newRows(mr, nil)
+	err := r.GetAll(mp)
+
+	assert.Nil(err)
+	assert.Equal(2, mp.scanCalled)
+	assert.Equal(1, mr.closeCalls)
+}
+
+func TestRows_GetAll_CallsClose(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 1,
+	}
+	mp := &mockParser{}
+
+	r := newRows(mr, nil)
+	r.GetAll(mp)
+
+	assert.Equal(1, mr.closeCalls)
+}
+
+func TestRows_GetAll_ParserError(t *testing.T) {
+	assert := assert.New(t)
+
+	mr := &mockSqlRows{
+		rowsCount: 2,
+	}
+	mp := &mockParser{
+		scanErr: errDefault,
+	}
+
+	r := newRows(mr, nil)
+	err := r.GetAll(mp)
+
+	assert.Equal(errDefault, err)
+	assert.Equal(1, mr.closeCalls)
+	assert.Equal(1, mp.scanCalled)
+}
+
+// func TestRows_GetAll(t *testing.T) {
+// 	assert := assert.New(t)
+
+// 	mp := &mockParser{}
+// 	m := &mockSqlRows{
+// 		numberOfRows: 2,
+// 	}
+// 	r := newRows(m, nil)
+// 	err := r.GetAll(mp)
+// 	assert.Nil(err)
+// 	assert.Equal(2, mp.parseCalled)
+// }
