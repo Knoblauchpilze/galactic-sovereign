befc158a49da59fd9f1164998f6eab883362be84
KnoblauchPilze <prolific-programmer@tuta.com>
2024-03-22 17:19:10 +0100

    feat: Added list routes, users routes are provided the database connection and implemented get operation

--- cmd/server/routes/user.go
+++ cmd/server/routes/user.go
@@ -1,29 +1,57 @@
 package routes
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/KnoblauchPilze/user-service/pkg/repositories"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
-func UserRoutes() Route {
+func UserRoutes(conn db.Connection) Route {
+	repo := repositories.NewUserRepository(conn)
+
 	return Route{
 		Path:        "users",
-		GetRoute:    getUser,
-		PostRoute:   createUser,
-		DeleteRoute: deleteUser,
+		GetRoute:    wrapWithDb(getUser, repo),
+		PostRoute:   wrapWithDb(createUser, repo),
+		DeleteRoute: wrapWithDb(deleteUser, repo),
 	}
 }
 
-func getUser(c echo.Context) error {
-	panic(fmt.Errorf("not implemented"))
+type userHttpHandler func(echo.Context, repositories.UserRepository) error
+
+func wrapWithDb(handler userHttpHandler, repo repositories.UserRepository) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return handler(c, repo)
+	}
 }
 
-func createUser(c echo.Context) error {
-	panic(fmt.Errorf("not implemented"))
+func getUser(c echo.Context, repo repositories.UserRepository) error {
+	maybeId := c.Param("id")
+	id, err := uuid.Parse(maybeId)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid id syntax")
+	}
+
+	out, err := repo.Get(id)
+	if err != nil {
+		if errors.IsErrorWithCode(err, db.NoMatchingSqlRows) {
+			return c.JSON(http.StatusNotFound, "No such user")
+		}
+
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, out)
 }
 
-func deleteUser(c echo.Context) error {
+func createUser(c echo.Context, repo repositories.UserRepository) error {
+	return errors.NewCode(errors.NotImplementedCode)
+}
+
+func deleteUser(c echo.Context, repo repositories.UserRepository) error {
 	return c.String(http.StatusOK, "{\"message\":\"deleted\"}")
 }
