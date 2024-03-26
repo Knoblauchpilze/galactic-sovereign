6f7dcb90152d301db7d3bb2e838360ad73c8a8ef
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-03-26 20:54:31 +0100

    feat: Added support for patch route

--- cmd/server/controllers/users.go
+++ cmd/server/controllers/users.go
@@ -17,19 +17,41 @@ func UserEndpoints(conn db.Connection) routes.Routes {
 
 	var out routes.Routes
 
+	postHandler := generateEchoHandler(createUser, repo)
+	post := routes.NewRoute(http.MethodPost, "/users", postHandler)
+	out = append(out, post)
+
 	getHandler := generateEchoHandler(getUser, repo)
 	get := routes.NewResourceRoute(http.MethodGet, "/users", getHandler)
 	out = append(out, get)
 
-	postHandler := generateEchoHandler(createUser, repo)
-	post := routes.NewRoute(http.MethodPost, "/users", postHandler)
-	out = append(out, post)
+	updateHandler := generateEchoHandler(updateUser, repo)
+	update := routes.NewResourceRoute(http.MethodPatch, "/users", updateHandler)
+	out = append(out, update)
 
 	deleteHandler := generateEchoHandler(deleteUser, repo)
 	delete := routes.NewResourceRoute(http.MethodDelete, "/users", deleteHandler)
 	out = append(out, delete)
 
 	return out
+}
+
+func createUser(c echo.Context, repo repositories.UserRepository) error {
+	// https://echo.labstack.com/docs/binding
+	var userDtoRequest communication.UserDtoRequest
+	err := c.Bind(&userDtoRequest)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid user syntax")
+	}
+
+	user := communication.FromUserDtoRequest(userDtoRequest)
+	err = repo.Create(c.Request().Context(), user)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	out := communication.ToUserDtoResponse(user)
+	return c.JSON(http.StatusCreated, out)
 }
 
 func getUser(c echo.Context, repo repositories.UserRepository) error {
@@ -52,16 +74,23 @@ func getUser(c echo.Context, repo repositories.UserRepository) error {
 	return c.JSON(http.StatusOK, out)
 }
 
-func createUser(c echo.Context, repo repositories.UserRepository) error {
-	// https://echo.labstack.com/docs/binding
+func updateUser(c echo.Context, repo repositories.UserRepository) error {
+	maybeId := c.Param("id")
+	id, err := uuid.Parse(maybeId)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid id syntax")
+	}
+
 	var userDtoRequest communication.UserDtoRequest
-	err := c.Bind(&userDtoRequest)
+	err = c.Bind(&userDtoRequest)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, "Invalid user syntax")
 	}
 
 	user := communication.FromUserDtoRequest(userDtoRequest)
-	err = repo.Create(c.Request().Context(), user)
+	user.Id = id
+
+	user, err = repo.Update(c.Request().Context(), user)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err)
 	}
