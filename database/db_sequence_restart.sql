5eadae6fb2759706f9712f09a1ab71a386a71b84
KnoblauchPilze <prolific-programmer@tuta.com>
2024-03-31 16:36:50 +0200

    fix: Unneeded file

--- database/db_sequence_restart.sql
+++
@@ -1,6 +0,0 @@
-
--- https://stackoverflow.com/questions/41102908/how-to-reset-all-sequences-to-1-before-database-migration-in-postgresql
-SELECT  SETVAL(c.oid, 1)
-  FROM pg_class c JOIN pg_namespace n
-  ON n.oid = c.relnamespace
-  WHERE c.relkind = 'S' AND n.nspname = 'public';
