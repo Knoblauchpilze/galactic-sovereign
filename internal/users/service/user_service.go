329f8ce356fafb7a1a9b47ee4695fa25f6c919b8
KnoblauchPilze <prolific-programmer@tuta.com>
2024-04-01 11:31:09 +0200

    feat: Added UserService and tests

---
+++ internal/users/service/user_service.go
@@ -0,0 +1,74 @@
+package service
+
+import (
+	"context"
+
+	"github.com/KnoblauchPilze/user-service/pkg/communication"
+	"github.com/KnoblauchPilze/user-service/pkg/repositories"
+	"github.com/google/uuid"
+)
+
+type UserService interface {
+	Create(ctx context.Context, user communication.UserDtoRequest) (communication.UserDtoResponse, error)
+	Get(ctx context.Context, id uuid.UUID) (communication.UserDtoResponse, error)
+	List(ctx context.Context) ([]uuid.UUID, error)
+	Update(ctx context.Context, id uuid.UUID, user communication.UserDtoRequest) (communication.UserDtoResponse, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+}
+
+type userServiceImpl struct {
+	repo repositories.UserRepository
+}
+
+func NewUserService(repo repositories.UserRepository) UserService {
+	return &userServiceImpl{
+		repo: repo,
+	}
+}
+
+func (s *userServiceImpl) Create(ctx context.Context, userDto communication.UserDtoRequest) (communication.UserDtoResponse, error) {
+	user := communication.FromUserDtoRequest(userDto)
+	created, err := s.repo.Create(ctx, user)
+	if err != nil {
+		return communication.UserDtoResponse{}, err
+	}
+
+	out := communication.ToUserDtoResponse(created, true)
+	return out, nil
+}
+
+func (s *userServiceImpl) Get(ctx context.Context, id uuid.UUID) (communication.UserDtoResponse, error) {
+	user, err := s.repo.Get(ctx, id)
+	if err != nil {
+		return communication.UserDtoResponse{}, err
+	}
+
+	out := communication.ToUserDtoResponse(user, false)
+	return out, nil
+}
+
+func (s *userServiceImpl) List(ctx context.Context) ([]uuid.UUID, error) {
+	return s.repo.List(ctx)
+}
+
+func (s *userServiceImpl) Update(ctx context.Context, id uuid.UUID, userDto communication.UserDtoRequest) (communication.UserDtoResponse, error) {
+	user, err := s.repo.Get(ctx, id)
+	if err != nil {
+		return communication.UserDtoResponse{}, err
+	}
+
+	user.Email = userDto.Email
+	user.Password = userDto.Password
+
+	updated, err := s.repo.Update(ctx, user)
+	if err != nil {
+		return communication.UserDtoResponse{}, err
+	}
+
+	out := communication.ToUserDtoResponse(updated, false)
+	return out, nil
+}
+
+func (s *userServiceImpl) Delete(ctx context.Context, id uuid.UUID) error {
+	return s.repo.Delete(ctx, id)
+}
