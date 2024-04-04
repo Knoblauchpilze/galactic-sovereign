22e14ac999cdfdec86a82d5e1e61fdf94353939d
KnoblauchPilze <prolific-programmer@tuta.com>
2024-04-04 21:51:25 +0200

    fix: Removed useless methods for api keys repository

--- pkg/repositories/api_key_repository.go
+++ pkg/repositories/api_key_repository.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/KnoblauchPilze/user-service/pkg/db"
-	"github.com/KnoblauchPilze/user-service/pkg/errors"
 	"github.com/KnoblauchPilze/user-service/pkg/persistence"
 	"github.com/google/uuid"
 )
@@ -14,8 +13,6 @@ type ApiKeyRepository interface {
 	Create(ctx context.Context, tx db.Transaction, apiKey persistence.ApiKey) (persistence.ApiKey, error)
 	Get(ctx context.Context, id uuid.UUID) (persistence.ApiKey, error)
 	GetForUser(ctx context.Context, tx db.Transaction, user uuid.UUID) ([]uuid.UUID, error)
-	List(ctx context.Context) ([]uuid.UUID, error)
-	Update(ctx context.Context, apiKey persistence.ApiKey) (persistence.ApiKey, error)
 	Delete(ctx context.Context, tx db.Transaction, ids []uuid.UUID) error
 }
 
@@ -81,52 +78,6 @@ func (r *apiUserRepositoryImpl) GetForUser(ctx context.Context, tx db.Transactio
 	}
 
 	return out, nil
-}
-
-const listApiKeySqlTemplate = "SELECT id FROM api_key"
-
-func (r *apiUserRepositoryImpl) List(ctx context.Context) ([]uuid.UUID, error) {
-	res := r.conn.Query(ctx, listApiKeySqlTemplate)
-	if err := res.Err(); err != nil {
-		return []uuid.UUID{}, err
-	}
-
-	var out []uuid.UUID
-	parser := func(rows db.Scannable) error {
-		var id uuid.UUID
-		err := rows.Scan(&id)
-		if err != nil {
-			return err
-		}
-
-		out = append(out, id)
-		return nil
-	}
-
-	if err := res.GetAll(parser); err != nil {
-		return []uuid.UUID{}, err
-	}
-
-	return out, nil
-}
-
-const updateApiKeySqlTemplate = "UPDATE api_key SET enabled = $1, version = $2 WHERE id = $3 AND version = $4"
-
-func (r *apiUserRepositoryImpl) Update(ctx context.Context, apiKey persistence.ApiKey) (persistence.ApiKey, error) {
-	version := apiKey.Version + 1
-	affected, err := r.conn.Exec(ctx, updateApiKeySqlTemplate, apiKey.Enabled, version, apiKey.Id, apiKey.Version)
-	if err != nil {
-		return apiKey, err
-	}
-	if affected == 0 {
-		return apiKey, errors.NewCode(db.OptimisticLockException)
-	} else if affected != 1 {
-		return apiKey, errors.NewCode(db.MoreThanOneMatchingSqlRows)
-	}
-
-	apiKey.Version = version
-
-	return apiKey, nil
 }
 
 const deleteApiKeysSqlTemplate = "DELETE FROM api_key WHERE id IN (%s)"
