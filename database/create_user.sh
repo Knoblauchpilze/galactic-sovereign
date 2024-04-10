b86f937e79339993af30ea324b2dc190222253e9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-04-10 21:35:39 +0200

    fix: Removed duplicated postgres argument

--- database/create_user.sh
+++ database/create_user.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # https://stackoverflow.com/questions/8208181/create-postgres-database-using-batch-file-with-template-encoding-owner-and
-psql -h localhost -U postgres postgres -f db_user_create.sql
+psql -h localhost -U postgres -f db_user_create.sql
