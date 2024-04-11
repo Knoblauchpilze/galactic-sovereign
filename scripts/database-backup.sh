427e494ae76a22ad4c712a6b27e7df643ceeae83
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-04-11 07:13:40 +0200

    feat: Parameterized backup script and better naming for backup files

--- scripts/database-backup.sh
+++ scripts/database-backup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# https://stackoverflow.com/questions/51649484/how-to-backup-postgresql-database-automatically-on-daily-basis
+DB_HOST="localhost"
+DB_NAME="db_user_service"
+DB_USER="user_service_user"
 
 # https://stackoverflow.com/questions/4018503/is-there-a-date-time-format-that-does-not-have-spaces
-DATE=$(date '+%F-%T')
+DATE=$(date '+%F-%T' | tr ':' '_')
 
 # https://www.postgresql.org/docs/current/app-pgdump.html
-pg_dump -h localhost -U user_service_user -F c -f "db_user_service_${DATE}.bck" db_user_service
+pg_dump -h ${DB_HOST} -U ${DB_USER} -F c -f "${DB_NAME}_dump_${DATE}.bck" ${DB_NAME}
