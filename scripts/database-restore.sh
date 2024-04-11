e38a5d0ac8874b633f18d39807b546f70821f6e7
KnoblauchPilze <prolific-programmer@tuta.com>
2024-04-11 07:17:38 +0200

    fix: Maintenance

---
+++ scripts/database-restore.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+  echo "usage: database-restore.sh [database_dump_file]"
+  exit 1
+fi
+
+DB_HOST="localhost"
+DB_NAME="db_user_service"
+DB_USER="user_service_admin"
+
+DB_DUMP=$1
+
+# https://www.postgresql.org/docs/current/app-pgrestore.html
+pg_restore -h ${DB_HOST} -U ${DB_USER} -cWe --if-exists -d ${DB_NAME} ${DB_DUMP}
