7e9acd03e2ca1a1081b9b437916b528abf46534f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-04-12 22:05:04 +0200

    feat: Updated user creation scripts to not have credentials anymore but expect them from the environment

--- database/db_user_create.sql
+++ database/db_user_create.sql
@@ -1,7 +1,8 @@
 
-CREATE USER user_service_admin WITH CREATEDB PASSWORD 'VCNNGJjsLSmoU5nxnSBBs';
-CREATE USER user_service_manager WITH PASSWORD 'Zyj94bZzKzCr7uG4QvwRB';
-CREATE USER user_service_user WITH PASSWORD 'ksM5Vuj32XRWcqv2FMCJz';
+-- https://stackoverflow.com/questions/72985242/securely-create-role-in-postgres-using-sql-script-and-environment-variables
+CREATE USER user_service_admin WITH CREATEDB PASSWORD :'admin_password';
+CREATE USER user_service_manager WITH PASSWORD :'manager_password';
+CREATE USER user_service_user WITH PASSWORD :'user_password';
 
 GRANT user_service_user TO user_service_manager;
 GRANT user_service_manager TO user_service_admin;
