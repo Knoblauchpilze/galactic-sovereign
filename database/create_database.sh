d5e38d66bbec76b71096f1c6d73923b5c56cae35
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-04-12 17:55:33 +0200

    feat: Configurable options in the db and user creation scripts

--- database/create_database.sh
+++ database/create_database.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-psql -h localhost -U postgres -f db_create.sql
+DB_HOST=${DATABASE_HOST:-localhost}
+DB_PORT=${DATABASE_PORT:-5432}
+DB_USER=${DATABASE_USER:-postgres}
+
+psql -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -f db_create.sql
--- database/create_user.sh
+++ database/create_user.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+DB_HOST=${DATABASE_HOST:-localhost}
+DB_PORT=${DATABASE_PORT:-5432}
+DB_USER=${DATABASE_USER:-postgres}
+
 # https://stackoverflow.com/questions/8208181/create-postgres-database-using-batch-file-with-template-encoding-owner-and
-psql -h localhost -U postgres -f db_user_create.sql
+psql -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -f db_user_create.sql
