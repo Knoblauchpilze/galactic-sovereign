2357830b0e9c19f67c834b8ef62bc738767e1146
KnoblauchPilze <prolific-programmer@tuta.com>
2024-04-12 22:05:04 +0200

    feat: Updated user creation scripts to not have credentials anymore but expect them from the environment

--- database/create_user.sh
+++ database/create_user.sh
@@ -1,8 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 DB_HOST=${DATABASE_HOST:-localhost}
 DB_PORT=${DATABASE_PORT:-5432}
 DB_USER=${DATABASE_USER:-postgres}
 
+# Password should be provided as environment variables.
+if [[ -z "${ADMIN_PASSWORD}" ]]; then
+  echo "DB admin password is not provided as environment variable, aborting"
+  exit 1
+fi
+if [[ -z "${MANAGER_PASSWORD}" ]]; then
+  echo "DB manager password is not provided as environment variable, aborting"
+  exit 1
+fi
+if [[ -z "${USER_PASSWORD}" ]]; then
+  echo "DB user password is not provided as environment variable, aborting"
+  exit 1
+fi
+
 # https://stackoverflow.com/questions/8208181/create-postgres-database-using-batch-file-with-template-encoding-owner-and
-psql -h ${DB_HOST} -p ${DB_PORT} -U ${DB_USER} -f db_user_create.sql
+psql \
+  -v admin_password=${ADMIN_PASSWORD} \
+  -v manager_password=${MANAGER_PASSWORD} \
+  -v user_password=${USER_PASSWORD} \
+  -h ${DB_HOST} \
+  -p ${DB_PORT} \
+  -U ${DB_USER} \
+  -f db_user_create.sql
