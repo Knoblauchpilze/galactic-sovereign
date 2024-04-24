26d51fa8b842ca7fc090fb94c896c17741128611
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-04-24 22:10:27 +0200

    feat: Refactored server a bit to allow unit testing

--- pkg/rest/server_test.go
+++ pkg/rest/server_test.go
@@ -1,8 +1,10 @@
 package rest
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/KnoblauchPilze/user-service/pkg/errors"
 	"github.com/KnoblauchPilze/user-service/pkg/repositories"
@@ -21,12 +23,35 @@ type mockApiKeyRepository struct {
 	repositories.ApiKeyRepository
 }
 
+type mockServerFramework struct {
+	getCalled    int
+	postCalled   int
+	deleteCalled int
+	patchCalled  int
+
+	useCalled int
+
+	startCalled int
+
+	address     string
+	path        string
+	middlewares []echo.MiddlewareFunc
+
+	sleep time.Duration
+	err   error
+}
+
+var errDefault = fmt.Errorf("some error")
+
 func TestServer_Register_UsesPathFromRoute(t *testing.T) {
 	assert := assert.New(t)
 
 	mr := &mockRoute{}
 
-	NewServer(Config{}, &mockApiKeyRepository{}).Register(mr)
+	s := NewServer(Config{}, &mockApiKeyRepository{})
+	defer s.Stop()
+
+	s.Register(mr)
 	assert.Equal(1, mr.generatePathCalled)
 }
 
@@ -38,7 +63,10 @@ func TestServer_Register_PropagatesPathFromConfig(t *testing.T) {
 		Endpoint: "some-endpoint",
 	}
 
-	NewServer(c, &mockApiKeyRepository{}).Register(mr)
+	s := NewServer(c, &mockApiKeyRepository{})
+	defer s.Stop()
+
+	s.Register(mr)
 	assert.Equal(c.Endpoint, mr.endpoint)
 }
 
@@ -50,52 +78,79 @@ func TestServer_Register_SanitizesPath(t *testing.T) {
 		Endpoint: "some-endpoint/",
 	}
 
-	NewServer(c, &mockApiKeyRepository{}).Register(mr)
+	s := NewServer(c, &mockApiKeyRepository{})
+	defer s.Stop()
+
+	s.Register(mr)
 	assert.Equal("some-endpoint", mr.endpoint)
 }
 
 func TestServer_Register_SupportsPost(t *testing.T) {
 	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
 
+	ms := setupMockServer()
 	mr := &mockRoute{
 		method: http.MethodPost,
 	}
 
-	err := NewServer(Config{}, &mockApiKeyRepository{}).Register(mr)
+	s := NewServer(Config{}, &mockApiKeyRepository{})
+	defer s.Stop()
+
+	err := s.Register(mr)
 	assert.Nil(err)
+	assert.Equal(1, ms.postCalled)
 }
 
 func TestServer_Register_SupportsGet(t *testing.T) {
 	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
 
+	ms := setupMockServer()
 	mr := &mockRoute{
 		method: http.MethodGet,
 	}
 
-	err := NewServer(Config{}, &mockApiKeyRepository{}).Register(mr)
+	s := NewServer(Config{}, &mockApiKeyRepository{})
+	defer s.Stop()
+
+	err := s.Register(mr)
 	assert.Nil(err)
+	assert.Equal(1, ms.getCalled)
 }
 
 func TestServer_Register_SupportsPatch(t *testing.T) {
 	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
 
+	ms := setupMockServer()
 	mr := &mockRoute{
 		method: http.MethodPatch,
 	}
 
-	err := NewServer(Config{}, &mockApiKeyRepository{}).Register(mr)
+	s := NewServer(Config{}, &mockApiKeyRepository{})
+	defer s.Stop()
+
+	err := s.Register(mr)
 	assert.Nil(err)
+	assert.Equal(1, ms.patchCalled)
 }
 
 func TestServer_Register_SupportsDelete(t *testing.T) {
 	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
 
+	ms := setupMockServer()
 	mr := &mockRoute{
 		method: http.MethodDelete,
 	}
 
-	err := NewServer(Config{}, &mockApiKeyRepository{}).Register(mr)
+	s := NewServer(Config{}, &mockApiKeyRepository{})
+	defer s.Stop()
+
+	err := s.Register(mr)
 	assert.Nil(err)
+	assert.Equal(1, ms.deleteCalled)
 }
 
 func TestServer_Register_FailsForUnsupportedMethod(t *testing.T) {
@@ -108,15 +163,119 @@ func TestServer_Register_FailsForUnsupportedMethod(t *testing.T) {
 
 	for _, method := range testMethods {
 		t.Run(method, func(t *testing.T) {
+			t.Cleanup(resetCreatorFunc)
+
+			setupMockServer()
+
 			mr := &mockRoute{
 				method: method,
 			}
 
-			err := NewServer(Config{}, &mockApiKeyRepository{}).Register(mr)
+			s := NewServer(Config{}, &mockApiKeyRepository{})
+			defer s.Stop()
+
+			err := s.Register(mr)
 			assert.True(errors.IsErrorWithCode(err, UnsupportedMethod))
 		})
 	}
+}
 
+func TestServer_Start_CallsServerStart(t *testing.T) {
+	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
+
+	ms := setupMockServer()
+
+	s := NewServer(Config{}, mockApiKeyRepository{})
+	s.Start()
+	s.Wait()
+
+	assert.Equal(1, ms.startCalled)
+}
+
+func TestServer_Start_UsesPortFromConfig(t *testing.T) {
+	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
+
+	ms := setupMockServer()
+	conf := Config{
+		Port: 36,
+	}
+
+	s := NewServer(conf, mockApiKeyRepository{})
+	s.Start()
+	s.Wait()
+
+	assert.Equal(":36", ms.address)
+}
+
+func TestServer_Start_ReturnsServerError(t *testing.T) {
+	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
+
+	ms := setupMockServer()
+	ms.err = errDefault
+
+	s := NewServer(Config{}, mockApiKeyRepository{})
+	s.Start()
+	err := s.Wait()
+
+	assert.Equal(errDefault, err)
+}
+
+func TestServer_Wait_WhenNotStarted_ReturnsImmediately(t *testing.T) {
+	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
+
+	setupMockServer()
+
+	s := NewServer(Config{}, mockApiKeyRepository{})
+	err := s.Wait()
+
+	assert.Nil(err)
+}
+
+func TestServer_Wait_WhenStarted_TakesTime(t *testing.T) {
+	assert := assert.New(t)
+	t.Cleanup(resetCreatorFunc)
+
+	ms := setupMockServer()
+
+	ms.sleep = time.Second
+
+	s := NewServer(Config{}, mockApiKeyRepository{})
+	start := time.Now()
+	s.Wait()
+	elapsed := time.Since(start)
+
+	assert.GreaterOrEqual(900*time.Millisecond, elapsed)
+}
+
+func TestRegisterMiddlewares_registersExpectedMiddlewareCount(t *testing.T) {
+	assert := assert.New(t)
+
+	ms := mockServerFramework{}
+
+	c := registerMiddlewares(&ms, mockApiKeyRepository{})
+	defer func() {
+		c <- true
+	}()
+
+	assert.Equal(6, len(ms.middlewares))
+}
+
+func setupMockServer() *mockServerFramework {
+	server := &mockServerFramework{}
+
+	creationFunc = func() serverFramework {
+		return server
+	}
+
+	return server
+}
+
+func resetCreatorFunc() {
+	creationFunc = createServerFramework
 }
 
 func (m *mockRoute) Method() string {
@@ -131,4 +290,40 @@ func (m *mockRoute) GeneratePath(endpoint string) string {
 	m.generatePathCalled++
 	m.endpoint = endpoint
 	return ""
+}
+
+func (m *mockServerFramework) GET(path string, handler echo.HandlerFunc, middlewares ...echo.MiddlewareFunc) *echo.Route {
+	m.getCalled++
+	m.path = path
+	return nil
+}
+
+func (m *mockServerFramework) POST(path string, handler echo.HandlerFunc, middlewares ...echo.MiddlewareFunc) *echo.Route {
+	m.postCalled++
+	m.path = path
+	return nil
+}
+
+func (m *mockServerFramework) DELETE(path string, handler echo.HandlerFunc, middlewares ...echo.MiddlewareFunc) *echo.Route {
+	m.deleteCalled++
+	m.path = path
+	return nil
+}
+
+func (m *mockServerFramework) PATCH(path string, handler echo.HandlerFunc, middlewares ...echo.MiddlewareFunc) *echo.Route {
+	m.patchCalled++
+	m.path = path
+	return nil
+}
+
+func (m *mockServerFramework) Use(middlewares ...echo.MiddlewareFunc) {
+	m.useCalled++
+	m.middlewares = append(m.middlewares, middlewares...)
+}
+
+func (m *mockServerFramework) Start(address string) error {
+	m.startCalled++
+	m.address = address
+	time.Sleep(m.sleep)
+	return m.err
 }
