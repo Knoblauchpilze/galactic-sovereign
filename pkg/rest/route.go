aefb96d5759c3b93a91f409a819360ac48c6d464
KnoblauchPilze <prolific-programmer@tuta.com>
2024-05-01 22:20:34 +0200

    feat: Routes define whether they are public or require authorization

--- pkg/rest/route.go
+++ pkg/rest/route.go
@@ -4,6 +4,7 @@ import "github.com/labstack/echo/v4"
 
 type Route interface {
 	Method() string
+	Authorized() bool
 	Handler() echo.HandlerFunc
 	GeneratePath(basePath string) string
 }
@@ -12,23 +13,26 @@ type Routes []Route
 
 type routeImpl struct {
 	method      string
+	authorized  bool
 	path        string
 	addIdInPath bool
 	handler     echo.HandlerFunc
 }
 
-func NewRoute(method string, path string, handler echo.HandlerFunc) Route {
+func NewRoute(method string, authorized bool, path string, handler echo.HandlerFunc) Route {
 	return &routeImpl{
 		method:      method,
+		authorized:  authorized,
 		path:        path,
 		addIdInPath: false,
 		handler:     handler,
 	}
 }
 
-func NewResourceRoute(method string, path string, handler echo.HandlerFunc) Route {
+func NewResourceRoute(method string, authorized bool, path string, handler echo.HandlerFunc) Route {
 	return &routeImpl{
 		method:      method,
+		authorized:  authorized,
 		path:        path,
 		addIdInPath: true,
 		handler:     handler,
@@ -37,6 +41,10 @@ func NewResourceRoute(method string, path string, handler echo.HandlerFunc) Rout
 
 func (r *routeImpl) Method() string {
 	return r.method
+}
+
+func (r *routeImpl) Authorized() bool {
+	return r.authorized
 }
 
 func (r *routeImpl) Handler() echo.HandlerFunc {
