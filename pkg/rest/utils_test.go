8608cfb64f4b91ad12d38f9abf343c0607c69119
KnoblauchPilze <prolific-programmer@tuta.com>
2024-05-01 15:30:59 +0200

    feat: Simplified sanitizePath

--- pkg/rest/utils_test.go
+++ pkg/rest/utils_test.go
@@ -6,76 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSanitizePath_TrimPrefix_EmptyRoute(t *testing.T) {
+func TestSanitizePath(t *testing.T) {
 	assert := assert.New(t)
 
-	actual := sanitizePath("", trimPrefix)
+	type testCase struct {
+		in       string
+		expected string
+	}
 
-	assert.Equal("", actual)
-}
+	testCases := []testCase{
+		{in: "", expected: "/"},
+		{in: "/", expected: "/"},
+		{in: "path", expected: "/path"},
+		{in: "path/", expected: "/path"},
+		{in: "/path", expected: "/path"},
+		{in: "/path/", expected: "/path"},
+		{in: "path/id", expected: "/path/id"},
+		{in: "path/id/", expected: "/path/id"},
+		{in: "/path/id", expected: "/path/id"},
+		{in: "/path/id/", expected: "/path/id"},
+	}
 
-func TestSanitizePath_TrimPrefix_RemovesLeadingSlash(t *testing.T) {
-	assert := assert.New(t)
+	for _, testCase := range testCases {
+		t.Run("", func(t *testing.T) {
+			actual := sanitizePath(testCase.in)
 
-	actual := sanitizePath("/route", trimPrefix)
-
-	assert.Equal("route", actual)
-}
-
-func TestSanitizePath_TrimPrefix_RemovesTrailingSlash(t *testing.T) {
-	assert := assert.New(t)
-
-	actual := sanitizePath("/route/", trimPrefix)
-
-	assert.Equal("route", actual)
-}
-
-func TestSanitizePath_TrimPrefix_PreservesPath(t *testing.T) {
-	assert := assert.New(t)
-
-	actual := sanitizePath("route/with/path/", trimPrefix)
-
-	assert.Equal("route/with/path", actual)
-}
-
-func TestSanitizePath_AddPrefix_EmptyRoute(t *testing.T) {
-	assert := assert.New(t)
-
-	actual := sanitizePath("", addPrefix)
-
-	assert.Equal("/", actual)
-}
-
-func TestSanitizePath_AddPrefix_AddLeadingSlash(t *testing.T) {
-	assert := assert.New(t)
-
-	actual := sanitizePath("route", addPrefix)
-
-	assert.Equal("/route", actual)
-}
-
-func TestSanitizePath_AddPrefix_DoesNotDuplicateLeadingSlash(t *testing.T) {
-	assert := assert.New(t)
-
-	actual := sanitizePath("/route", addPrefix)
-
-	assert.Equal("/route", actual)
-}
-
-func TestSanitizePath_AddPrefix_RemovesTrailingSlash(t *testing.T) {
-	assert := assert.New(t)
-
-	actual := sanitizePath("/route/", addPrefix)
-
-	assert.Equal("/route", actual)
-}
-
-func TestSanitizePath_AddPrefix_PreservesPath(t *testing.T) {
-	assert := assert.New(t)
-
-	actual := sanitizePath("route/with/path/", addPrefix)
-
-	assert.Equal("/route/with/path", actual)
+			assert.Equal(testCase.expected, actual)
+		})
+	}
 }
 
 func TestConcatenateEndpoints_AllEmpty(t *testing.T) {
