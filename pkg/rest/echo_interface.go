5038fc862d640201d6f45c73352a5c74396ecf6f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-05-01 15:36:39 +0200

    feat: Refactored echo interface to extract a echoRouter

---
+++ pkg/rest/echo_interface.go
@@ -0,0 +1,31 @@
+package rest
+
+import (
+	"github.com/KnoblauchPilze/user-service/pkg/logger"
+	"github.com/labstack/echo/v4"
+)
+
+type echoRouter interface {
+	GET(string, echo.HandlerFunc, ...echo.MiddlewareFunc) *echo.Route
+	POST(string, echo.HandlerFunc, ...echo.MiddlewareFunc) *echo.Route
+	DELETE(string, echo.HandlerFunc, ...echo.MiddlewareFunc) *echo.Route
+	PATCH(string, echo.HandlerFunc, ...echo.MiddlewareFunc) *echo.Route
+
+	Use(...echo.MiddlewareFunc)
+}
+
+type echoServer interface {
+	echoRouter
+
+	Group(string, ...echo.MiddlewareFunc) *echo.Group
+
+	Start(string) error
+}
+
+func createEchoServer() echoServer {
+	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+	e.Logger = logger.New("server")
+	return e
+}
