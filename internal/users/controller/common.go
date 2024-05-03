8c8d0c26262a088ada4097b29f03fea5ce7f59f2
KnoblauchPilze <prolific-programmer@tuta.com>
2024-05-03 16:39:45 +0200

    feat: Creation of a handler using the connection pool

--- internal/users/controller/common.go
+++ internal/users/controller/common.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/KnoblauchPilze/user-service/internal/users/service"
+	"github.com/KnoblauchPilze/user-service/pkg/db"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,5 +11,13 @@ type repositoriesAwareHttpHandler func(echo.Context, service.UserService) error
 func fromRepositoriesAwareHttpHandler(handler repositoriesAwareHttpHandler, service service.UserService) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return handler(c, service)
+	}
+}
+
+type dbAwareHttpHandler func(echo.Context, db.ConnectionPool) error
+
+func fromDbAwareHttpHandler(handler dbAwareHttpHandler, pool db.ConnectionPool) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return handler(c, pool)
 	}
 }
