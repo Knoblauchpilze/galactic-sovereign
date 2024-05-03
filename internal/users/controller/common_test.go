a05cd461edacf95c81daae3b4d58dca9635b2495
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-05-03 16:39:45 +0200

    feat: Creation of a handler using the connection pool

--- internal/users/controller/common_test.go
+++ internal/users/controller/common_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/KnoblauchPilze/user-service/internal/users/service"
+	"github.com/KnoblauchPilze/user-service/pkg/db"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
@@ -39,6 +40,35 @@ func TestFromRepositoriesAwareHttpHandler_PropagatesError(t *testing.T) {
 	}
 
 	h := fromRepositoriesAwareHttpHandler(in, &mockUserService{})
+
+	err := h(mockEchoContext{})
+	assert.Equal(errDefault, err)
+}
+
+func TestFromDbAwareHttpHandler_CallsHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	handlerCalled := false
+	in := func(_ echo.Context, _ db.ConnectionPool) error {
+		handlerCalled = true
+		return nil
+	}
+
+	h := fromDbAwareHttpHandler(in, &mockConnectionPool{})
+
+	err := h(mockEchoContext{})
+	assert.Nil(err)
+	assert.True(handlerCalled)
+}
+
+func TestFromDbAwareHttpHandler_PropagatesError(t *testing.T) {
+	assert := assert.New(t)
+
+	in := func(_ echo.Context, _ db.ConnectionPool) error {
+		return errDefault
+	}
+
+	h := fromDbAwareHttpHandler(in, &mockConnectionPool{})
 
 	err := h(mockEchoContext{})
 	assert.Equal(errDefault, err)
