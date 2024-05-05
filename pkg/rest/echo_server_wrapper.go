148cd4b11f1dfe660fc3754f1baca72b0f9aa427
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-05-05 21:42:50 +0200

    feat: Sanitized echoServer interface

--- pkg/rest/echo_server_wrapper.go
+++ pkg/rest/echo_server_wrapper.go
@@ -6,22 +6,6 @@ type echoServerImpl struct {
 	e *echo.Echo
 }
 
-func (esi *echoServerImpl) GET(path string, h echo.HandlerFunc, m ...echo.MiddlewareFunc) *echo.Route {
-	return esi.e.GET(path, h, m...)
-}
-
-func (esi *echoServerImpl) POST(path string, h echo.HandlerFunc, m ...echo.MiddlewareFunc) *echo.Route {
-	return esi.e.POST(path, h, m...)
-}
-
-func (esi *echoServerImpl) DELETE(path string, h echo.HandlerFunc, m ...echo.MiddlewareFunc) *echo.Route {
-	return esi.e.DELETE(path, h, m...)
-}
-
-func (esi *echoServerImpl) PATCH(path string, h echo.HandlerFunc, m ...echo.MiddlewareFunc) *echo.Route {
-	return esi.e.PATCH(path, h, m...)
-}
-
 func (esi *echoServerImpl) Use(m ...echo.MiddlewareFunc) {
 	esi.e.Use(m...)
 }
