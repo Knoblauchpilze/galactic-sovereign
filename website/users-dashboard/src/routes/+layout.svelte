4ffea9c9cb1274797f7aa140a56258816133377d
KnoblauchPilze <prolific-programmer@tuta.com>
2024-05-10 09:43:06 +0200

    feat: Added common layout for pages

---
+++ website/users-dashboard/src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<div class="root">
+	<div class="panel">
+		<slot></slot>
+	</div>
+</div>
+
+<style>
+	/* https://stackoverflow.com/questions/19026884/flexbox-center-horizontally-and-vertically */
+	/* https://stackoverflow.com/questions/71760177/styling-the-body-element-in-svelte */
+	:global(html),
+	:global(body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	.root {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		/* https://svelte-ux.techniq.dev/theme use dark theme: forest */
+		background-color: #19362d;
+	}
+
+	.panel {
+		width: 50%;
+		height: 50%;
+	}
+</style>
