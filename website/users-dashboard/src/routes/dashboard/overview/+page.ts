b8bb3a0d02145dac75e0390dbf93fb51d7075855
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-05-10 11:02:20 +0200

    feat: More work regarding fetching the user's data

--- website/users-dashboard/src/routes/dashboard/overview/+page.ts
+++ website/users-dashboard/src/routes/dashboard/overview/+page.ts
@@ -1,12 +1,21 @@
 
+import User, { getUser,  } from '$lib/users';
+
 /** @type {import('./$types').PageLoad} */
-export function load({ params }) {
+export async function load({ params }) {
   console.log("params: " + JSON.stringify(params));
 
+  const DUMMY_API_KEY = 'your-key';
+  const DUMMY_USER_ID = 'your-id';
+  const userResponse = await getUser(DUMMY_API_KEY, DUMMY_USER_ID);
+
+  if (userResponse.error()) {
+    // loginError = String(userResponse.details);
+  }
+
+  console.log("response: ", JSON.stringify(userResponse));
+
 	return {
-		id: "00-00-00-01",
-    email: "random@e.mail",
-    password: "juice",
-    createdAt: new Date()
+    user: new User(userResponse),
 	};
 }
