6a4de84bab8edff69fb4135b393fa6dd59a8727f
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-05-11 19:26:20 +0200

    feat: Used the user id from the login endpoint to add a new cookie for the user id

--- website/users-dashboard/src/lib/apiKey.ts
+++ website/users-dashboard/src/lib/apiKey.ts
@@ -2,12 +2,18 @@
 import ResponseEnvelope from './responseEnvelope';
 
 export default class ApiKey {
+	readonly user: string = "00000000-0000-0000-0000-000000000000";
 	readonly key: string = "00000000-0000-0000-0000-000000000000";
 	readonly validUntil: Date = new Date();
 
 	constructor(response: ResponseEnvelope) {
 		if (response.error()) {
 			return
+		}
+
+		const maybeUser = (response.details as any).user;
+		if (typeof maybeUser === "string") {
+			this.user = maybeUser;
 		}
 
 		const maybeKey = (response.details as any).key;
