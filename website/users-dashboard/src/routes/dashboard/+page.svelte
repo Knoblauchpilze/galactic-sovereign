989a156b150f8e14de7c8b627fae70b4ef9b5f2d
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-05-21 09:23:29 +0200

    feat: Added home page to redirect to either login or signup

---
+++ website/users-dashboard/src/routes/dashboard/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	/** @type {import('./$types').ActionData} */
+	export let form: HTMLFormElement;
+
+	function resetFormError() {
+		if (!form) {
+			return;
+		}
+		form.message = '';
+	}
+</script>
+
+<div class="wrapper">
+	<h1>User dashboard</h1>
+
+	<a href="/dashboard/login">Login</a>
+	<a href="/dashboard/signup">Sign up</a>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	h1 {
+		color: #1eb854;
+	}
+
+	a {
+		padding: 1em 3em;
+
+		color: #1eb854;
+	}
+</style>
