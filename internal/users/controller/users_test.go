a955dd714f8cea90f8c5fd92a3d1f1eab06a96cf
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-05-23 22:37:54 +0200

    feat: Used echo.Context in tests for user logout and removed mockEchoContext

--- internal/users/controller/users_test.go
+++ internal/users/controller/users_test.go
@@ -19,17 +19,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type mockContext struct {
-	echo.Context
-
-	params  map[string]string
-	body    communication.UserDtoRequest
-	bindErr error
-
-	status int
-	data   interface{}
-}
-
 type mockUserService struct {
 	ids    []uuid.UUID
 	user   communication.UserDtoResponse
@@ -784,44 +773,37 @@ func TestLoginUserByEmail_ReturnsUserToken(t *testing.T) {
 func TestLogoutUser_WhenNoId_SetsStatusToBadRequest(t *testing.T) {
 	assert := assert.New(t)
 
-	mc := &mockContext{}
+	ctx, rw := generateTestEchoContextWithMethod(http.MethodPost)
 	ms := &mockUserService{}
 
-	err := logoutUser(mc, ms)
+	err := logoutUser(ctx, ms)
 
 	assert.Nil(err)
-	assert.Equal(http.StatusBadRequest, mc.status)
-	assert.Equal("Invalid id syntax", mc.data)
+	assert.Equal(http.StatusBadRequest, rw.Code)
+	assert.Equal("\"Invalid id syntax\"\n", rw.Body.String())
 }
 
 func TestLogoutUser_WhenIdSyntaxIsWrong_SetsStatusToBadRequest(t *testing.T) {
 	assert := assert.New(t)
 
-	mc := &mockContext{
-		params: map[string]string{
-			"id": "not-a-valid-id",
-		},
-	}
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader("not-a-uuid"))
+	ctx, rw := generateTestEchoContextFromRequest(req)
 	ms := &mockUserService{}
 
-	err := logoutUser(mc, ms)
+	err := logoutUser(ctx, ms)
 
 	assert.Nil(err)
-	assert.Equal(http.StatusBadRequest, mc.status)
-	assert.Equal("Invalid id syntax", mc.data)
+	assert.Equal(http.StatusBadRequest, rw.Code)
+	assert.Equal("\"Invalid id syntax\"\n", rw.Body.String())
 }
 
 func TestLogoutUser_CallsServiceLogout(t *testing.T) {
 	assert := assert.New(t)
 
-	mc := &mockContext{
-		params: map[string]string{
-			"id": defaultUuid.String(),
-		},
-	}
+	ctx, _ := generateEchoContextWithUuid(http.MethodPost)
 	ms := &mockUserService{}
 
-	err := logoutUser(mc, ms)
+	err := logoutUser(ctx, ms)
 
 	assert.Nil(err)
 	assert.Equal(1, ms.logoutCalled)
@@ -830,102 +812,53 @@ func TestLogoutUser_CallsServiceLogout(t *testing.T) {
 func TestLogoutUser_WhenServiceFailsWithUnknownError_SetsStatusToInternalServerError(t *testing.T) {
 	assert := assert.New(t)
 
-	mc := &mockContext{
-		params: map[string]string{
-			"id": defaultUuid.String(),
-		},
-	}
+	ctx, rw := generateEchoContextWithUuid(http.MethodPost)
 	ms := &mockUserService{
 		err: errDefault,
 	}
 
-	err := logoutUser(mc, ms)
+	err := logoutUser(ctx, ms)
 
 	assert.Nil(err)
-	assert.Equal(http.StatusInternalServerError, mc.status)
+	assert.Equal(http.StatusInternalServerError, rw.Code)
 }
 
 func TestLogoutUser_WhenServiceFailsWithNoMatchingRows_SetsStatusToNotFound(t *testing.T) {
 	assert := assert.New(t)
 
-	mc := &mockContext{
-		params: map[string]string{
-			"id": defaultUuid.String(),
-		},
-	}
+	ctx, rw := generateEchoContextWithUuid(http.MethodPost)
 	ms := &mockUserService{
 		err: errors.NewCode(db.NoMatchingSqlRows),
 	}
 
-	err := logoutUser(mc, ms)
+	err := logoutUser(ctx, ms)
 
 	assert.Nil(err)
-	assert.Equal(http.StatusNotFound, mc.status)
+	assert.Equal(http.StatusNotFound, rw.Code)
 }
 
 func TestLogoutUser_SetsStatusToNoContent(t *testing.T) {
 	assert := assert.New(t)
 
-	mc := &mockContext{
-		params: map[string]string{
-			"id": defaultUuid.String(),
-		},
-	}
+	ctx, rw := generateEchoContextWithUuid(http.MethodPost)
 	ms := &mockUserService{}
 
-	err := logoutUser(mc, ms)
+	err := logoutUser(ctx, ms)
 
 	assert.Nil(err)
-	assert.Equal(http.StatusNoContent, mc.status)
+	assert.Equal(http.StatusNoContent, rw.Code)
 }
 
 func TestLogoutUser_LogsOutExpectedUser(t *testing.T) {
 	assert := assert.New(t)
 
-	mc := &mockContext{
-		params: map[string]string{
-			"id": defaultUuid.String(),
-		},
-	}
+	ctx, _ := generateEchoContextWithUuid(http.MethodPost)
 	ms := &mockUserService{}
 
-	err := logoutUser(mc, ms)
+	err := logoutUser(ctx, ms)
 
 	assert.Nil(err)
 	assert.Equal(defaultUuid, ms.inId)
-}
-
-func (m *mockContext) Request() *http.Request {
-	return httptest.NewRequest(http.MethodGet, "http://localhost:3000", nil)
-}
-
-func (m *mockContext) Param(key string) string {
-	if m.params == nil {
-		return ""
-	}
-
-	if value, ok := m.params[key]; ok {
-		return value
-	}
-
-	return ""
-}
-
-func (m *mockContext) Bind(i interface{}) error {
-	dto := i.(*communication.UserDtoRequest)
-	*dto = m.body
-	return m.bindErr
-}
-
-func (m *mockContext) JSON(status int, message interface{}) error {
-	m.status = status
-	m.data = message
-	return nil
-}
-
-func (m *mockContext) NoContent(status int) error {
-	m.status = status
-	return nil
 }
 
 func generateTestPostRequest() *http.Request {
