e7a0254be27cdea463be7b5080f7fae9ea082ac2
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-05-25 09:50:14 +0200

    feat: Mutualized some of the tests in the user repository

--- pkg/repositories/user_repository_test.go
+++ pkg/repositories/user_repository_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/KnoblauchPilze/user-service/pkg/persistence"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 type mockConnectionPool struct {
@@ -72,44 +73,24 @@ var defaultUser = persistence.User{
 	Version:   4,
 }
 
-func TestUserRepository_Create_UsesConnectionToExec(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.Create(context.Background(), defaultUser)
-
-	assert.Equal(1, mc.execCalled)
-}
-
-func TestUserRepository_Create_GeneratesValidSql(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.Create(context.Background(), defaultUser)
-
-	assert.Equal("INSERT INTO api_user (id, email, password, created_at) VALUES($1, $2, $3, $4)", mc.sqlQuery)
-	assert.Equal(4, len(mc.args))
-	assert.Equal(defaultUser.Id, mc.args[0])
-	assert.Equal(defaultUser.Email, mc.args[1])
-	assert.Equal(defaultUser.Password, mc.args[2])
-	assert.Equal(defaultUser.CreatedAt, mc.args[3])
-}
-
-func TestUserRepository_Create_PropagatesQueryFailure(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{
-		execErr: errDefault,
+func TestUserRepository_Create(t *testing.T) {
+	s := RepositoryTestSuite{
+		sqlMode: ExecBased,
+		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
+			repo := NewUserRepository(pool)
+			_, err := repo.Create(ctx, defaultUser)
+			return err
+		},
+		expectedSql: `INSERT INTO api_user (id, email, password, created_at) VALUES($1, $2, $3, $4)`,
+		expectedArguments: []interface{}{
+			defaultUser.Id,
+			defaultUser.Email,
+			defaultUser.Password,
+			defaultUser.CreatedAt,
+		},
 	}
-	repo := NewUserRepository(mc)
 
-	_, err := repo.Create(context.Background(), defaultUser)
-
-	assert.Equal(errDefault, err)
+	suite.Run(t, &s)
 }
 
 func TestUserRepository_Create_WhenQueryIndicatesDuplicatedKey_ReturnsDuplicatedKey(t *testing.T) {
@@ -137,43 +118,20 @@ func TestUserRepository_Create_ReturnsInputUser(t *testing.T) {
 	assert.Equal(defaultUser, actual)
 }
 
-func TestUserRepository_Get_UsesConnectionToQuery(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.Get(context.Background(), uuid.UUID{})
-
-	assert.Equal(1, mc.queryCalled)
-}
-
-func TestUserRepository_Get_GeneratesValidSql(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.Get(context.Background(), defaultUserId)
-
-	assert.Equal("SELECT id, email, password, created_at, updated_at, version FROM api_user WHERE id = $1", mc.sqlQuery)
-	assert.Equal(1, len(mc.args))
-	assert.Equal(defaultUserId, mc.args[0])
-}
-
-func TestUserRepository_Get_PropagatesQueryFailure(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{
-		rows: mockRows{
-			err: errDefault,
+func TestUserRepository_Get(t *testing.T) {
+	s := RepositoryTestSuite{
+		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
+			repo := NewUserRepository(pool)
+			_, err := repo.Get(ctx, defaultUserId)
+			return err
+		},
+		expectedSql: `SELECT id, email, password, created_at, updated_at, version FROM api_user WHERE id = $1`,
+		expectedArguments: []interface{}{
+			defaultUserId,
 		},
 	}
-	repo := NewUserRepository(mc)
 
-	_, err := repo.Get(context.Background(), defaultUserId)
-
-	assert.Equal(errDefault, err)
+	suite.Run(t, &s)
 }
 
 func TestUserRepository_Get_CallsGetSingleValue(t *testing.T) {
@@ -257,43 +215,20 @@ func TestUserRepository_Get_ScansUserProperties(t *testing.T) {
 	assert.IsType(&itg, props[5])
 }
 
-func TestUserRepository_GetByEmail_UsesConnectionToQuery(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.GetByEmail(context.Background(), defaultUserEmail)
-
-	assert.Equal(1, mc.queryCalled)
-}
-
-func TestUserRepository_GetByEmail_GeneratesValidSql(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.GetByEmail(context.Background(), defaultUserEmail)
-
-	assert.Equal("SELECT id, email, password, created_at, updated_at, version FROM api_user WHERE email = $1", mc.sqlQuery)
-	assert.Equal(1, len(mc.args))
-	assert.Equal(defaultUserEmail, mc.args[0])
-}
-
-func TestUserRepository_GetByEmail_PropagatesQueryFailure(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{
-		rows: mockRows{
-			err: errDefault,
+func TestUserRepository_GetByEmail(t *testing.T) {
+	s := RepositoryTestSuite{
+		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
+			repo := NewUserRepository(pool)
+			_, err := repo.GetByEmail(ctx, defaultUserEmail)
+			return err
+		},
+		expectedSql: `SELECT id, email, password, created_at, updated_at, version FROM api_user WHERE email = $1`,
+		expectedArguments: []interface{}{
+			defaultUserEmail,
 		},
 	}
-	repo := NewUserRepository(mc)
 
-	_, err := repo.GetByEmail(context.Background(), defaultUserEmail)
-
-	assert.Equal(errDefault, err)
+	suite.Run(t, &s)
 }
 
 func TestUserRepository_GetByEmail_CallsGetSingleValue(t *testing.T) {
@@ -377,42 +312,17 @@ func TestUserRepository_GetByEmail_ScansUserProperties(t *testing.T) {
 	assert.IsType(&itg, props[5])
 }
 
-func TestUserRepository_List_UsesConnectionToQuery(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.List(context.Background())
-
-	assert.Equal(1, mc.queryCalled)
-}
-
-func TestUserRepository_List_GeneratesValidSql(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.List(context.Background())
-
-	assert.Equal("SELECT id FROM api_user", mc.sqlQuery)
-	assert.Equal(0, len(mc.args))
-}
-
-func TestUserRepository_List_PropagatesQueryFailure(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{
-		rows: mockRows{
-			err: errDefault,
+func TestUserRepository_List(t *testing.T) {
+	s := RepositoryTestSuite{
+		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
+			repo := NewUserRepository(pool)
+			_, err := repo.List(ctx)
+			return err
 		},
+		expectedSql: `SELECT id FROM api_user`,
 	}
-	repo := NewUserRepository(mc)
 
-	_, err := repo.List(context.Background())
-
-	assert.Equal(errDefault, err)
+	suite.Run(t, &s)
 }
 
 func TestUserRepository_List_CallsGetAll(t *testing.T) {
@@ -478,45 +388,25 @@ func TestUserRepository_List_ScansIds(t *testing.T) {
 	assert.IsType(&uuid.UUID{}, props[0])
 }
 
-func TestUserRepository_Update_UsesConnectionToQuery(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.Update(context.Background(), defaultUser)
-
-	assert.Equal(1, mc.execCalled)
-}
-
-func TestUserRepository_Update_GeneratesValidSql(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewUserRepository(mc)
-
-	repo.Update(context.Background(), defaultUser)
-
-	assert.Equal("UPDATE api_user SET email = $1, password = $2, version = $3 WHERE id = $4 AND version = $5", mc.sqlQuery)
-	assert.Equal(5, len(mc.args))
-	assert.Equal(defaultUser.Email, mc.args[0])
-	assert.Equal(defaultUser.Password, mc.args[1])
-	assert.Equal(defaultUser.Version+1, mc.args[2])
-	assert.Equal(defaultUser.Id, mc.args[3])
-	assert.Equal(defaultUser.Version, mc.args[4])
-}
-
-func TestUserRepository_Update_PropagatesQueryFailure(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{
-		execErr: errDefault,
+func TestUserRepository_Update(t *testing.T) {
+	s := RepositoryTestSuite{
+		sqlMode: ExecBased,
+		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
+			repo := NewUserRepository(pool)
+			_, err := repo.Update(context.Background(), defaultUser)
+			return err
+		},
+		expectedSql: `UPDATE api_user SET email = $1, password = $2, version = $3 WHERE id = $4 AND version = $5`,
+		expectedArguments: []interface{}{
+			defaultUser.Email,
+			defaultUser.Password,
+			defaultUser.Version + 1,
+			defaultUser.Id,
+			defaultUser.Version,
+		},
 	}
-	repo := NewUserRepository(mc)
 
-	_, err := repo.Update(context.Background(), defaultUser)
-
-	assert.Equal(errDefault, err)
+	suite.Run(t, &s)
 }
 
 func TestUserRepository_Update_WhenAffectedRowsIsZero_ReturnsOptimisticLockException(t *testing.T) {
