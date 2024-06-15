af0941d9b2c8dafb69d7785d008e3fb2984f8876
KnoblauchPilze <prolific-programmer@tuta.com>
2024-06-15 21:02:34 +0200

    fix: Redirect to the root page and not the login page

--- website/user-dashboard/src/routes/+page.server.ts
+++ website/user-dashboard/src/routes/+page.server.ts
@@ -3,5 +3,5 @@ import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	redirect(303, '/dashboard/login');
+	redirect(303, '/dashboard');
 };
