d8d4e05dbd43c088b943972641cebcb81965e4e3
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-06-16 17:37:23 +0200

    feat: Added ground work for demo page for the main website

---
+++ website/stellar-dominion/src/routes/+error.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	let errorMessage: string = 'Unexpected error happened';
+	if ($page.error !== null) {
+		errorMessage = $page.error.message;
+	}
+</script>
+
+<div class="wrapper">
+	<h1>Oh noes, something went wrong...</h1>
+
+	<table>
+		<tr>
+			<td class="label">Error message:</td>
+			<td class="field">{errorMessage}</td>
+		</tr>
+	</table>
+
+	<a href="/">Back to safety</a>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	h1,
+	.label {
+		padding: 0.5em 1.5em;
+
+		color: #b87333;
+	}
+
+	.field {
+		color: #d92d0f;
+	}
+
+	a {
+		padding: 1em 3em;
+
+		color: #b87333;
+	}
+</style>
---
+++ website/stellar-dominion/src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<div class="hero-image">
+	<div class="hero-panel">
+		<slot></slot>
+	</div>
+</div>
+
+<style>
+	/* https://stackoverflow.com/questions/19026884/flexbox-center-horizontally-and-vertically */
+	/* https://stackoverflow.com/questions/71760177/styling-the-body-element-in-svelte */
+	:global(html),
+	:global(body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* https://kit.svelte.dev/docs/images */
+	.hero-image {
+		background-image: url('$lib/assets/background.webp');
+
+		height: 100%;
+
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		position: relative;
+	}
+
+	.hero-panel {
+		text-align: center;
+		position: absolute;
+		padding: 2em;
+		top: 50%;
+		left: 50%;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		font-weight: bold;
+		font-style: italic;
+
+		transform: translate(-50%, -50%);
+		background-color: #0005;
+	}
+</style>
