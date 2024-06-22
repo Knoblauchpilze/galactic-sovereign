d40d373e87991ca1ee2baaf9169c95c3ef4bba06
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-06-22 17:52:14 +0200

    feat: Added UserLimitDtos and tests

--- pkg/communication/user_limit_dto.go
+++ pkg/communication/user_limit_dto.go
@@ -35,3 +35,56 @@ func ToLimitDtoResponse(limit persistence.Limit) LimitDtoResponse {
 		Value: limit.Value,
 	}
 }
+
+type UserLimitDtoRequest struct {
+	Name string    `json:"name"`
+	User uuid.UUID `json:"user"`
+
+	Limits []LimitDtoRequest `json:"limits"`
+}
+
+type UserLimitDtoResponse struct {
+	Id   uuid.UUID `json:"id"`
+	Name string    `json:"name"`
+	User uuid.UUID `json:"user"`
+
+	Limits []LimitDtoResponse `json:"limits"`
+
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func FromUserLimitDtoRequest(userLimit UserLimitDtoRequest) persistence.UserLimit {
+	var limits []persistence.Limit
+	for _, limit := range userLimit.Limits {
+		limits = append(limits, FromLimitDtoRequest(limit))
+	}
+
+	t := time.Now()
+	return persistence.UserLimit{
+		Id:   uuid.New(),
+		Name: userLimit.Name,
+		User: userLimit.User,
+
+		Limits: limits,
+
+		CreatedAt: t,
+		UpdatedAt: t,
+	}
+}
+
+func ToUserLimitDtoResponse(userLimit persistence.UserLimit) UserLimitDtoResponse {
+	var limitsResponseDtos []LimitDtoResponse
+	for _, limit := range userLimit.Limits {
+		limitsResponseDtos = append(limitsResponseDtos, ToLimitDtoResponse(limit))
+	}
+
+	return UserLimitDtoResponse{
+		Id:   userLimit.Id,
+		Name: userLimit.Name,
+		User: userLimit.User,
+
+		Limits: limitsResponseDtos,
+
+		CreatedAt: userLimit.CreatedAt,
+	}
+}
--- pkg/communication/user_limit_dto_test.go
+++ pkg/communication/user_limit_dto_test.go
@@ -61,7 +61,7 @@ func TestToLimitDtoResponse(t *testing.T) {
 	assert.Equal("my-value", actual.Value)
 }
 
-func TestUserLimitResponse_MarshalsToCamelCase(t *testing.T) {
+func TestLimitDtoResponse_MarshalsToCamelCase(t *testing.T) {
 	assert := assert.New(t)
 
 	u := LimitDtoResponse{
@@ -73,4 +73,111 @@ func TestUserLimitResponse_MarshalsToCamelCase(t *testing.T) {
 
 	assert.Nil(err)
 	assert.Equal(`{"name":"my-name","value":"my-value"}`, string(out))
+}
+
+func TestUserLimitDtoRequest_MarshalsToCamelCase(t *testing.T) {
+	assert := assert.New(t)
+
+	u := UserLimitDtoRequest{
+		Name: "my-name",
+		User: uuid.MustParse("7aaa145a-5ad8-4d63-be87-177d6abcf1b5"),
+		Limits: []LimitDtoRequest{
+			{
+				Name:  "limit",
+				Value: "test",
+			},
+		},
+	}
+
+	out, err := json.Marshal(u)
+
+	assert.Nil(err)
+	assert.Equal(`{"name":"my-name","user":"7aaa145a-5ad8-4d63-be87-177d6abcf1b5","limits":[{"name":"limit","value":"test"}]}`, string(out))
+}
+
+func TestFromUserLimitDtoRequest(t *testing.T) {
+	assert := assert.New(t)
+
+	beforeConversion := time.Now()
+
+	u := UserLimitDtoRequest{
+		Name: "my-name",
+		User: uuid.MustParse("7aaa145a-5ad8-4d63-be87-177d6abcf1b5"),
+		Limits: []LimitDtoRequest{
+			{
+				Name:  "limit",
+				Value: "test",
+			},
+		},
+	}
+
+	actual := FromUserLimitDtoRequest(u)
+
+	assert.Nil(uuid.Validate(actual.Id.String()))
+	assert.Equal("my-name", actual.Name)
+	assert.Equal("7aaa145a-5ad8-4d63-be87-177d6abcf1b5", actual.User.String())
+	assert.True(actual.CreatedAt.After(beforeConversion))
+	assert.Equal(actual.CreatedAt, actual.UpdatedAt)
+
+	assert.Equal(1, len(actual.Limits))
+	assert.Nil(uuid.Validate(actual.Limits[0].Id.String()))
+	assert.Equal("limit", actual.Limits[0].Name)
+	assert.Equal("test", actual.Limits[0].Value)
+	assert.True(actual.Limits[0].CreatedAt.After(beforeConversion))
+	assert.Equal(actual.Limits[0].CreatedAt, actual.Limits[0].UpdatedAt)
+}
+
+func TestToUserLimitDtoResponse(t *testing.T) {
+	assert := assert.New(t)
+
+	u := persistence.UserLimit{
+		Id:   uuid.MustParse("2f3b7c63-5b4a-422a-bd9d-7da0f78b6294"),
+		Name: "my-limit",
+		User: uuid.MustParse("3657b088-ba88-497a-a158-9d6c7faae94f"),
+		Limits: []persistence.Limit{
+			{
+				Id:    defaultUuid,
+				Name:  "my-name",
+				Value: "my-value",
+
+				CreatedAt: someTime,
+				UpdatedAt: someTime,
+			},
+		},
+		CreatedAt: someTime,
+		UpdatedAt: someTime,
+	}
+
+	actual := ToUserLimitDtoResponse(u)
+
+	assert.Equal(u.Id, actual.Id)
+	assert.Equal(u.Name, actual.Name)
+	assert.Equal(u.User, actual.User)
+	assert.Equal(u.CreatedAt, actual.CreatedAt)
+
+	assert.Equal(len(u.Limits), len(actual.Limits))
+	assert.Equal(u.Limits[0].Name, actual.Limits[0].Name)
+	assert.Equal(u.Limits[0].Value, actual.Limits[0].Value)
+}
+
+func TestUserLimitDtoResponse_MarshalsToCamelCase(t *testing.T) {
+	assert := assert.New(t)
+
+	u := UserLimitDtoResponse{
+		Id:        uuid.MustParse("2f3b7c63-5b4a-422a-bd9d-7da0f78b6294"),
+		Name:      "my-limit",
+		User:      uuid.MustParse("3657b088-ba88-497a-a158-9d6c7faae94f"),
+		CreatedAt: someTime,
+		Limits: []LimitDtoResponse{
+			{
+				Name:  "limit-1",
+				Value: "my-value",
+			},
+		},
+	}
+
+	out, err := json.Marshal(u)
+
+	assert.Nil(err)
+	assert.Equal(`{"id":"2f3b7c63-5b4a-422a-bd9d-7da0f78b6294","name":"my-limit","user":"3657b088-ba88-497a-a158-9d6c7faae94f","limits":[{"name":"limit-1","value":"my-value"}],"createdAt":"2024-05-05T20:50:18.651387237Z"}`, string(out))
 }
