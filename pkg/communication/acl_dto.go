19803dcd2a51c97557b1da2aad3e0ccacdffc36c
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-06-23 11:03:04 +0200

    fear: Added AclDtoRequest and AclDtoResponse and tests

--- pkg/communication/acl_dto.go
+++ pkg/communication/acl_dto.go
@@ -1,5 +1,55 @@
 package communication
 
+import (
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/google/uuid"
+)
+
+type AclDtoRequest struct {
+	User uuid.UUID `json:"user"`
+
+	Resource    string   `json:"resource"`
+	Permissions []string `json:"permissions"`
+}
+
+type AclDtoResponse struct {
+	Id   uuid.UUID `json:"id"`
+	User uuid.UUID `json:"user"`
+
+	Resource    string   `json:"resource"`
+	Permissions []string `json:"permissions"`
+
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+func FromAclDtoRequest(acl AclDtoRequest) persistence.Acl {
+	t := time.Now()
+	return persistence.Acl{
+		Id:   uuid.New(),
+		User: acl.User,
+
+		Resource:    acl.Resource,
+		Permissions: acl.Permissions,
+
+		CreatedAt: t,
+		UpdatedAt: t,
+	}
+}
+
+func ToAclDtoResponse(acl persistence.Acl) AclDtoResponse {
+	return AclDtoResponse{
+		Id:   acl.Id,
+		User: acl.User,
+
+		Resource:    acl.Resource,
+		Permissions: acl.Permissions,
+
+		CreatedAt: acl.CreatedAt,
+	}
+}
+
 type AclResponseDto struct {
 	Resource    string   `json:"resource"`
 	Permissions []string `json:"permissions"`
