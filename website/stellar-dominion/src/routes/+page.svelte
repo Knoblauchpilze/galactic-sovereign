febe92a843defbc4932a926a1c2e88da471f27c7
KnoblauchPilze <prolific-programmer@tuta.com>
2024-07-03 21:27:38 +0200

    feat: Login and sign up

--- website/stellar-dominion/src/routes/+page.svelte
+++ website/stellar-dominion/src/routes/+page.svelte
@@ -1,19 +1,37 @@
-<div class="main-panel">
-	<a href="/login">Login</a>
-	<a href="/signup">Sign up</a>
+<div>
+	<h1>Stellar Dominion</h1>
+	<p>Battle for the galaxy</p>
+</div>
+
+<div>
+	<a class="action-button" href="/login">Login</a>
+</div>
+<div>
+	<a class="action-button" href="/signup">Sign up</a>
 </div>
 
 <style>
-	.main-panel {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	a {
-		padding: 1em 3em;
+	h1,
+	p {
+		font-weight: bold;
+		font-style: italic;
 
 		color: #b87333;
+	}
+
+	.action-button {
+		padding: 1em 3em;
+		border-radius: 8px;
+
+		color: #b87333;
+		background-color: #263037;
+	}
+
+	.action-button:hover {
+		padding: 1em 3em;
+		border-radius: 8px;
+
+		color: #b87333;
+		background-color: #36454f;
 	}
 </style>
