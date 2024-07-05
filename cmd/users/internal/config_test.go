e785832936b1ffb0ad55d21ef548503e26937f93
KnoblauchPilze <prolific-programmer@tuta.com>
2024-07-05 15:16:03 +0200

    feat: Refactored config package to make it more generic

--- cmd/users/internal/config_test.go
+++ cmd/users/internal/config_test.go
@@ -2,20 +2,20 @@ package internal
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/KnoblauchPilze/user-service/internal/users/service"
+	"github.com/KnoblauchPilze/user-service/pkg/config"
+	"github.com/KnoblauchPilze/user-service/pkg/db"
 	"github.com/KnoblauchPilze/user-service/pkg/rest"
 	"github.com/labstack/gommon/log"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
 var errDefault = fmt.Errorf("some error")
 
-func TestDefaultConfig_Server(t *testing.T) {
+func TestDefaultConfig_ServerIsUnchanged(t *testing.T) {
 	assert := assert.New(t)
 
 	conf := defaultConf()
@@ -26,6 +26,23 @@ func TestDefaultConfig_Server(t *testing.T) {
 		RateLimit: 10,
 	}
 	assert.Equal(expected, conf.Server)
+}
+
+func TestDefaultConfig_ServerDefinesTheDbName(t *testing.T) {
+	assert := assert.New(t)
+
+	conf := defaultConf()
+
+	expected := db.Config{
+		Host:                "172.17.0.1",
+		Port:                5432,
+		Name:                "db_user_service",
+		User:                "",
+		Password:            "",
+		ConnectionsPoolSize: 1,
+		LogLevel:            log.DEBUG,
+	}
+	assert.Equal(expected, conf.Database)
 }
 
 func TestDefaultConfig_ApiKey(t *testing.T) {
@@ -39,221 +56,42 @@ func TestDefaultConfig_ApiKey(t *testing.T) {
 	assert.Equal(expected, conf.ApiKey)
 }
 
-func TestDefaultConfig_Database_AssumesDockerLocalhost(t *testing.T) {
-	assert := assert.New(t)
-
-	conf := defaultConf()
-
-	assert.Equal("172.17.0.1", conf.Database.Host)
-}
-
-func TestDefaultConfig_Database_AssumesPort5432(t *testing.T) {
-	assert := assert.New(t)
-
-	conf := defaultConf()
-
-	assert.Equal(uint16(5432), conf.Database.Port)
-}
-
-func TestDefaultConfig_Database_AssumesPoolSizeOfOne(t *testing.T) {
-	assert := assert.New(t)
-
-	conf := defaultConf()
-
-	assert.Equal(uint(1), conf.Database.ConnectionsPoolSize)
-}
-
-func TestDefaultConfig_Database_AssumesUserServiceDb(t *testing.T) {
-	assert := assert.New(t)
-
-	conf := defaultConf()
-
-	assert.Equal("db_user_service", conf.Database.Name)
-}
-
-func TestDefaultConfig_Database_DoesNotDefineUser(t *testing.T) {
-	assert := assert.New(t)
-
-	conf := defaultConf()
-
-	assert.Equal("", conf.Database.User)
-}
-
-func TestDefaultConfig_Database_DoesNotDefinePassword(t *testing.T) {
-	assert := assert.New(t)
-
-	conf := defaultConf()
-
-	assert.Equal("", conf.Database.Password)
-}
-
-func TestDefaultConfig_Database_AssumesDebugLogLevel(t *testing.T) {
-	assert := assert.New(t)
-
-	conf := defaultConf()
-
-	assert.Equal(log.DEBUG, conf.Database.LogLevel)
-}
-
-type mockConfigurationParser struct {
-	confType       string
-	confPaths      []string
-	confName       string
-	envKeyReplacer *strings.Replacer
-	envPrefix      string
-	automaticEnv   bool
-
-	readCalled int
-	readErr    error
-
-	unmarshalVal  interface{}
-	unmarshalOpts []viper.DecoderConfigOption
-	unmarshalErr  error
-}
-
 const defaultConfigName = "some-config"
-
-func TestLoadConfiguration_LooksForYamlFiles(t *testing.T) {
-	assert := assert.New(t)
-	t.Cleanup(resetConfigurationParser)
-
-	m := mockConfigurationParser{}
-	configurator = &m
-
-	LoadConfiguration(defaultConfigName)
-
-	assert.Equal("yaml", m.confType)
-}
-
-func TestLoadConfiguration_LooksForFilesInExpectedPath(t *testing.T) {
-	assert := assert.New(t)
-	t.Cleanup(resetConfigurationParser)
-
-	m := mockConfigurationParser{}
-	configurator = &m
-
-	LoadConfiguration(defaultConfigName)
-
-	assert.Equal([]string{"configs"}, m.confPaths)
-}
 
 func TestLoadConfiguration_LooksForTheRightFile(t *testing.T) {
 	assert := assert.New(t)
 	t.Cleanup(resetConfigurationParser)
 
-	m := mockConfigurationParser{}
-	configurator = &m
+	loadConf, actualConfigName := generateLoadConfFunc(nil)
 
-	LoadConfiguration(defaultConfigName)
+	loadConf(defaultConfigName, defaultConf())
 
-	assert.Equal(defaultConfigName, m.confName)
-}
-
-func TestLoadConfiguration_AppliesEnvironmentOverrides(t *testing.T) {
-	assert := assert.New(t)
-	t.Cleanup(resetConfigurationParser)
-
-	m := mockConfigurationParser{}
-	configurator = &m
-
-	LoadConfiguration(defaultConfigName)
-
-	assert.True(m.automaticEnv)
-	assert.Equal("ENV", m.envPrefix)
-	assert.Equal(strings.NewReplacer(".", "_"), m.envKeyReplacer)
-}
-
-func TestLoadConfiguration_ReadsConfiguration(t *testing.T) {
-	assert := assert.New(t)
-	t.Cleanup(resetConfigurationParser)
-
-	m := mockConfigurationParser{}
-	configurator = &m
-
-	LoadConfiguration(defaultConfigName)
-
-	assert.Equal(1, m.readCalled)
+	assert.Equal(defaultConfigName, *actualConfigName)
 }
 
 func TestLoadConfiguration_WhenError_ExpectDefaultAndError(t *testing.T) {
 	assert := assert.New(t)
 	t.Cleanup(resetConfigurationParser)
 
-	m := mockConfigurationParser{
-		readErr: errDefault,
-	}
-	configurator = &m
+	loadConf, _ := generateLoadConfFunc(errDefault)
 
-	conf, err := LoadConfiguration(defaultConfigName)
-
-	assert.Equal(errDefault, err)
-	assert.Equal(defaultConf(), conf)
-}
-
-func TestLoadConfiguration_UnmarshalsInConfiguration(t *testing.T) {
-	assert := assert.New(t)
-	t.Cleanup(resetConfigurationParser)
-
-	m := mockConfigurationParser{}
-	configurator = &m
-
-	LoadConfiguration(defaultConfigName)
-
-	assert.IsType(&Configuration{}, m.unmarshalVal)
-	assert.Equal(0, len(m.unmarshalOpts))
-}
-
-func TestLoadConfiguration_WhenUnmarshalFails_ExpectDefaultAndError(t *testing.T) {
-	assert := assert.New(t)
-	t.Cleanup(resetConfigurationParser)
-
-	m := mockConfigurationParser{
-		unmarshalErr: errDefault,
-	}
-	configurator = &m
-
-	conf, err := LoadConfiguration(defaultConfigName)
+	conf, err := loadConf(defaultConfigName, defaultConf())
 
 	assert.Equal(errDefault, err)
 	assert.Equal(defaultConf(), conf)
 }
 
 func resetConfigurationParser() {
-	configurator = viper.New()
+	loadConfFunc = config.LoadConfiguration[Configuration]
 }
 
-func (m *mockConfigurationParser) SetConfigType(extension string) {
-	m.confType = extension
-}
+func generateLoadConfFunc(loadErr error) (loadConfFuncType, *string) {
+	var outConfName string
 
-func (m *mockConfigurationParser) AddConfigPath(path string) {
-	m.confPaths = append(m.confPaths, path)
-}
+	f := func(configName string, defaultConf Configuration) (Configuration, error) {
+		outConfName = configName
+		return defaultConf, loadErr
+	}
 
-func (m *mockConfigurationParser) SetConfigName(fileName string) {
-	m.confName = fileName
-}
-
-func (m *mockConfigurationParser) SetEnvKeyReplacer(replacer *strings.Replacer) {
-	m.envKeyReplacer = replacer
-}
-
-func (m *mockConfigurationParser) SetEnvPrefix(envPrefix string) {
-	m.envPrefix = envPrefix
-}
-
-func (m *mockConfigurationParser) AutomaticEnv() {
-	m.automaticEnv = true
-}
-
-func (m *mockConfigurationParser) ReadInConfig() error {
-	m.readCalled++
-	return m.readErr
-}
-
-func (m *mockConfigurationParser) Unmarshal(rawVal any, opts ...viper.DecoderConfigOption) error {
-	m.unmarshalVal = rawVal
-	m.unmarshalOpts = append(m.unmarshalOpts, opts...)
-
-	return m.unmarshalErr
+	return f, &outConfName
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/rest"
+	"github.com/labstack/gommon/log"
+	"github.com/spf13/viper"
+)
+
+type Configuration struct {
+	Server   rest.Config
+	Database db.Config
+}
+
+type configurationParser interface {
+	SetConfigType(extension string)
+	AddConfigPath(path string)
+
+	SetConfigName(fileName string)
+	ReadInConfig() error
+
+	SetEnvKeyReplacer(replacer *strings.Replacer)
+	SetEnvPrefix(envPrefix string)
+	AutomaticEnv()
+
+	Unmarshal(rawVal any, opts ...viper.DecoderConfigOption) error
+}
+
+var configurator configurationParser = viper.New()
+
+func LoadConfiguration[T any](configName string, defaultConf T) (T, error) {
+	// https://github.com/spf13/viper#reading-config-files
+	configurator.SetConfigType("yaml")
+	configurator.AddConfigPath("configs")
+
+	// https://stackoverflow.com/questions/61585304/issues-with-overriding-config-using-env-variables-in-viper
+	configurator.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	configurator.SetEnvPrefix("ENV")
+	configurator.AutomaticEnv()
+
+	configurator.SetConfigName(configName)
+	if err := configurator.ReadInConfig(); err != nil {
+		return defaultConf, err
+	}
+
+	out := defaultConf
+	if err := configurator.Unmarshal(&out); err != nil {
+		return defaultConf, err
+	}
+
+	return out, nil
+}
+
+func DefaultConf() Configuration {
+	return Configuration{
+		Server: rest.Config{
+			BasePath:  "/v1",
+			Port:      uint16(80),
+			RateLimit: 10,
+		},
+		Database: db.Config{
+			Host:                "172.17.0.1",
+			Port:                5432,
+			ConnectionsPoolSize: 1,
+			LogLevel:            log.DEBUG,
+		},
+	}
+}
