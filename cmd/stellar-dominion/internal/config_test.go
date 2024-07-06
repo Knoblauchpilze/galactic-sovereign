aeeb03c442e361f2748e056858407605919a29e7
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-07-06 22:42:46 +0200

    feat: New stellar-dominion service

---
+++ cmd/stellar-dominion/internal/config_test.go
@@ -0,0 +1,51 @@
+package internal
+
+import (
+	"testing"
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/internal/users/service"
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/rest"
+	"github.com/labstack/gommon/log"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultConfig_LeavesServerUnchanged(t *testing.T) {
+	assert := assert.New(t)
+
+	conf := DefaultConf()
+
+	expected := rest.Config{
+		BasePath:  "/v1",
+		Port:      uint16(80),
+		RateLimit: 10,
+	}
+	assert.Equal(expected, conf.Server)
+}
+
+func TestDefaultConfig_LeavesApiKeyUnchanged(t *testing.T) {
+	assert := assert.New(t)
+
+	conf := DefaultConf()
+
+	expected := service.Config{
+		ApiKeyValidity: 3 * time.Hour,
+	}
+	assert.Equal(expected, conf.ApiKey)
+}
+
+func TestDefaultConfig_ReplacesDatabaseConfiguration(t *testing.T) {
+	assert := assert.New(t)
+
+	conf := DefaultConf()
+
+	expected := db.Config{
+		Host:                "172.17.0.1",
+		Port:                5432,
+		Name:                "db_stellar_dominion",
+		ConnectionsPoolSize: 1,
+		LogLevel:            log.DEBUG,
+	}
+	assert.Equal(expected, conf.Database)
+}
---
+++ cmd/stellar-dominion/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/KnoblauchPilze/user-service/cmd/stellar-dominion/internal"
+	"github.com/KnoblauchPilze/user-service/internal/users/controller"
+	"github.com/KnoblauchPilze/user-service/pkg/config"
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/logger"
+	"github.com/KnoblauchPilze/user-service/pkg/rest"
+)
+
+func determineConfigName() string {
+	if len(os.Args) < 2 {
+		return "stellar-dominion-prod.yml"
+	}
+
+	return os.Args[1]
+}
+
+func main() {
+	conf, err := config.LoadConfiguration(determineConfigName(), internal.DefaultConf())
+	if err != nil {
+		logger.Errorf("Failed to load configuration: %v", err)
+		os.Exit(1)
+	}
+	if conf.Database.User == "" || conf.Database.Password == "" {
+		logger.Errorf("Please provide a user and password to connect to the database")
+		os.Exit(1)
+	}
+
+	pool := db.NewConnectionPool(conf.Database)
+	if err := pool.Connect(context.Background()); err != nil {
+		logger.Errorf("Failed to connect to the database: %v", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(context.Background()); err != nil {
+		logger.Errorf("Failed to ping the database: %v", err)
+		os.Exit(1)
+	}
+
+	s := rest.NewServer(conf.Server)
+
+	for _, route := range controller.HealthCheckEndpoints(pool) {
+		if err := s.Register(route); err != nil {
+			logger.Errorf("Failed to register route: %v", err)
+			os.Exit(1)
+		}
+	}
+
+	s.Start()
+
+	installCleanup(pool, s)
+
+	if err := s.Wait(); err != nil {
+		logger.Errorf("Error while servier was running: %v", err)
+		os.Exit(1)
+	}
+}
+
+func installCleanup(conn db.ConnectionPool, s rest.Server) {
+	// https://stackoverflow.com/questions/11268943/is-it-possible-to-capture-a-ctrlc-signal-sigint-and-run-a-cleanup-function-i
+	interruptChannel := make(chan os.Signal, 2)
+	signal.Notify(interruptChannel, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-interruptChannel
+
+		s.Stop()
+		conn.Close()
+		os.Exit(1)
+	}()
+}
