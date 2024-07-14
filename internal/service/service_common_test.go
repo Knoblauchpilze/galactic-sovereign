11af3f0c0e7842e2b91d0f6398e151ea54f7064e
KnoblauchPilze <prolific-programmer@tuta.com>
2024-07-14 15:33:40 +0200

    feat: Refactored tests for planet service

---
+++ internal/service/service_common_test.go
@@ -0,0 +1,142 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/repositories"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type testFunc func(context.Context, db.ConnectionPool, repositories.Repositories) error
+type returnTestFunc func(context.Context, db.ConnectionPool, repositories.Repositories) interface{}
+type generateValidRepositoriesMock func() repositories.Repositories
+type generateErrorRepositoriesMock func(err error) repositories.Repositories
+
+type verifyMockInteractions func(repositories.Repositories, *require.Assertions)
+
+type errorTestCase struct {
+	generateErrorRepositoriesMock generateErrorRepositoriesMock
+	handler                       testFunc
+}
+
+type repositoryInteractionTestCase struct {
+	generateValidRepositoriesMock generateValidRepositoriesMock
+	handler                       testFunc
+	verifyInteractions            verifyMockInteractions
+}
+
+type returnTestCase struct {
+	generateValidRepositoriesMock generateValidRepositoriesMock
+	handler                       returnTestFunc
+	expectedContent               interface{}
+}
+
+type transactionTestCase struct {
+	generateValidRepositoriesMock generateValidRepositoriesMock
+	handler                       returnTestFunc
+}
+
+type ServiceTestSuite struct {
+	suite.Suite
+
+	generateValidRepositoriesMock generateValidRepositoriesMock
+	generateErrorRepositoriesMock generateErrorRepositoriesMock
+
+	errorTestCases                 map[string]errorTestCase
+	repositoryInteractionTestCases map[string]repositoryInteractionTestCase
+	returnTestCases                map[string]returnTestCase
+	transactionTestCases           map[string]transactionTestCase
+}
+
+func (s *ServiceTestSuite) TestWhenRepositoryFails_ExpectErrorIsPropagated() {
+	for name, testCase := range s.errorTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			var repos repositories.Repositories
+			if testCase.generateErrorRepositoriesMock != nil {
+				repos = testCase.generateErrorRepositoriesMock(errDefault)
+			} else {
+				repos = s.generateErrorRepositoriesMock(errDefault)
+			}
+
+			err := testCase.handler(context.Background(), &mockConnectionPool{}, repos)
+
+			s.Require().Equal(errDefault, err)
+		})
+	}
+}
+
+func (s *ServiceTestSuite) TestWhenRepositorySucceeds_ExpectCorrectInteraction() {
+	for name, testCase := range s.repositoryInteractionTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			var repos repositories.Repositories
+			if testCase.generateValidRepositoriesMock != nil {
+				repos = testCase.generateValidRepositoriesMock()
+			} else {
+				repos = s.generateValidRepositoriesMock()
+			}
+
+			err := testCase.handler(context.Background(), &mockConnectionPool{}, repos)
+
+			s.Require().Nil(err)
+			testCase.verifyInteractions(repos, s.Require())
+		})
+	}
+}
+
+func (s *ServiceTestSuite) TestWhenRepositorySucceeds_ReturnsExpectedValue() {
+	for name, testCase := range s.returnTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			var repos repositories.Repositories
+			if testCase.generateValidRepositoriesMock != nil {
+				repos = testCase.generateValidRepositoriesMock()
+			} else {
+				repos = s.generateValidRepositoriesMock()
+			}
+
+			actual := testCase.handler(context.Background(), &mockConnectionPool{}, repos)
+
+			s.Require().Equal(testCase.expectedContent, actual)
+		})
+	}
+}
+
+func (s *ServiceTestSuite) TestWhenUsingTransaction_ExpectCallsClose() {
+	for name, testCase := range s.transactionTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			var repos repositories.Repositories
+			if testCase.generateValidRepositoriesMock != nil {
+				repos = testCase.generateValidRepositoriesMock()
+			} else {
+				repos = s.generateValidRepositoriesMock()
+			}
+
+			m := &mockConnectionPool{}
+			testCase.handler(context.Background(), m, repos)
+
+			s.Require().Equal(1, m.tx.closeCalled)
+		})
+	}
+}
+
+func (s *ServiceTestSuite) TestWhenCreatingTransactionFails_ExpectErrorIsPropagated() {
+	for name, testCase := range s.transactionTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			var repos repositories.Repositories
+			if testCase.generateValidRepositoriesMock != nil {
+				repos = testCase.generateValidRepositoriesMock()
+			} else {
+				repos = s.generateValidRepositoriesMock()
+			}
+
+			m := &mockConnectionPool{
+				err: errDefault,
+			}
+			err := testCase.handler(context.Background(), m, repos)
+
+			s.Require().Equal(errDefault, err)
+		})
+	}
+}
