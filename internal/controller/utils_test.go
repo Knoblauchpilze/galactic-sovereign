35aa4a45a748d3d6158fea3f081ba09ab6479d93
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-07-14 11:10:54 +0200

    feat: Added utility to fetch query params from echo context

--- internal/controller/utils_test.go
+++ internal/controller/utils_test.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,4 +28,62 @@ func TestMarshalNilToEmptySlice_WhenNotNil_ExpectMarshalCorrectData(t *testing.T
 
 	assert.Nil(err)
 	assert.Equal("[1,2]", string(actual))
+}
+
+var defaultKey = "my-key"
+
+func TestFetchIdFromQueryParam_whenNoId_expectNotExistAndNoError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, _, err := fetchIdFromQueryParam(defaultKey, ctx)
+	assert.False(exists)
+	assert.Nil(err)
+}
+
+func TestFetchIdFromQueryParam_whenIdSetForOtherKey_expectNotExistAndNoError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := generateRequestWithQueryParams("not-the-default-key", defaultUuid.String())
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, _, err := fetchIdFromQueryParam(defaultKey, ctx)
+	assert.False(exists)
+	assert.Nil(err)
+}
+
+func TestFetchIdFromQueryParam_whenIdSyntaxIsWrong_expectExistAndError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := generateRequestWithQueryParams(defaultKey, "not-a-uuid")
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, _, err := fetchIdFromQueryParam(defaultKey, ctx)
+	assert.True(exists)
+	assert.Equal("invalid UUID length: 10", err.Error())
+}
+
+func TestFetchIdFromQueryParam_whenIdIsSet_expectExistCorrectIdAndNoError(t *testing.T) {
+	assert := assert.New(t)
+
+	req := generateRequestWithQueryParams(defaultKey, defaultUuid.String())
+	ctx, _ := generateTestEchoContextFromRequest(req)
+
+	exists, actual, err := fetchIdFromQueryParam(defaultKey, ctx)
+	assert.True(exists)
+	assert.Equal(defaultUuid, actual)
+	assert.Nil(err)
+}
+
+func generateRequestWithQueryParams(key string, value string) *http.Request {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	q := req.URL.Query()
+	q.Add(key, value)
+
+	req.URL.RawQuery = q.Encode()
+
+	return req
 }
