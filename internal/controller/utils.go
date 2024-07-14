30fa9166ca1ebded412c703a0ec59fb33933c0e0
KnoblauchPilze <prolific-programmer@tuta.com>
2024-07-14 11:10:54 +0200

    feat: Added utility to fetch query params from echo context

--- internal/controller/utils.go
+++ internal/controller/utils.go
@@ -1,6 +1,11 @@
 package controller
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
 
 func marshalNilToEmptySlice[T any](in []T) ([]byte, error) {
 	toMarshal := make([]T, 0)
@@ -9,4 +14,15 @@ func marshalNilToEmptySlice[T any](in []T) ([]byte, error) {
 	}
 
 	return json.Marshal(toMarshal)
+}
+
+func fetchIdFromQueryParam(key string, c echo.Context) (exists bool, id uuid.UUID, err error) {
+	maybeId := c.QueryParam(key)
+	exists = (maybeId != "")
+	if maybeId == "" {
+		return exists, uuid.UUID{}, nil
+	}
+
+	id, err = uuid.Parse(maybeId)
+	return exists, id, err
 }
