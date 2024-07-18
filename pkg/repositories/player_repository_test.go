d9e7e5cec04565c757ab9811030e8144cf0c66a2
KnoblauchPilze <prolific-programmer@tuta.com>
2024-07-18 18:42:36 +0200

    feat: Moved part of the player repository tests to new test suite

--- pkg/repositories/player_repository_test.go
+++ pkg/repositories/player_repository_test.go
@@ -26,21 +26,56 @@ var defaultPlayer = persistence.Player{
 	Version:   5,
 }
 
-func TestPlayerRepository_Create_DbInteraction(t *testing.T) {
-	s := RepositoryPoolTestSuite{
-		sqlMode: ExecBased,
-		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
-			repo := NewPlayerRepository(pool)
-			_, err := repo.Create(ctx, defaultPlayer)
-			return err
+func Test_PlayerRepository(t *testing.T) {
+	s := RepositoryTestSuite{
+
+		dbPoolInteractionTestCases: map[string]dbPoolInteractionTestCase{
+			"create": {
+				sqlMode: ExecBased,
+				handler: func(ctx context.Context, pool db.ConnectionPool) error {
+					s := NewPlayerRepository(pool)
+					_, err := s.Create(ctx, defaultPlayer)
+					return err
+				},
+				expectedSql: `INSERT INTO player (id, api_user, universe, name, created_at) VALUES($1, $2, $3, $4, $5)`,
+				expectedArguments: []interface{}{
+					defaultPlayer.Id,
+					defaultPlayer.ApiUser,
+					defaultPlayer.Universe,
+					defaultPlayer.Name,
+					defaultPlayer.CreatedAt,
+				},
+			},
+			"get": {
+				handler: func(ctx context.Context, pool db.ConnectionPool) error {
+					s := NewPlayerRepository(pool)
+					_, err := s.Get(ctx, defaultPlayerId)
+					return err
+				},
+				expectedSql: `SELECT id, api_user, universe, name, created_at, updated_at, version FROM player WHERE id = $1`,
+				expectedArguments: []interface{}{
+					defaultPlayerId,
+				},
+			},
+			"list": {
+				handler: func(ctx context.Context, pool db.ConnectionPool) error {
+					s := NewPlayerRepository(pool)
+					_, err := s.List(ctx)
+					return err
+				},
+				expectedSql: `SELECT id, api_user, universe, name, created_at, updated_at, version FROM player`,
+			},
 		},
-		expectedSql: `INSERT INTO player (id, api_user, universe, name, created_at) VALUES($1, $2, $3, $4, $5)`,
-		expectedArguments: []interface{}{
-			defaultPlayer.Id,
-			defaultPlayer.ApiUser,
-			defaultPlayer.Universe,
-			defaultPlayer.Name,
-			defaultPlayer.CreatedAt,
+
+		dbPoolReturnTestCases: map[string]dbPoolReturnTestCase{
+			"create": {
+				handler: func(ctx context.Context, pool db.ConnectionPool) interface{} {
+					s := NewPlayerRepository(pool)
+					out, _ := s.Create(ctx, defaultPlayer)
+					return out
+				},
+				expectedContent: defaultPlayer,
+			},
 		},
 	}
 
@@ -58,34 +93,6 @@ func TestPlayerRepository_Create_WhenQueryIndicatesDuplicatedKey_ReturnsDuplicat
 	_, err := repo.Create(context.Background(), defaultPlayer)
 
 	assert.True(errors.IsErrorWithCode(err, db.DuplicatedKeySqlKey))
-}
-
-func TestPlayerRepository_Create_ReturnsInputPlayer(t *testing.T) {
-	assert := assert.New(t)
-
-	mc := &mockConnectionPool{}
-	repo := NewPlayerRepository(mc)
-
-	actual, err := repo.Create(context.Background(), defaultPlayer)
-
-	assert.Nil(err)
-	assert.Equal(defaultPlayer, actual)
-}
-
-func TestPlayerRepository_Get_DbInteraction(t *testing.T) {
-	s := RepositoryPoolTestSuite{
-		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
-			repo := NewPlayerRepository(pool)
-			_, err := repo.Get(ctx, defaultPlayerId)
-			return err
-		},
-		expectedSql: `SELECT id, api_user, universe, name, created_at, updated_at, version FROM player WHERE id = $1`,
-		expectedArguments: []interface{}{
-			defaultPlayerId,
-		},
-	}
-
-	suite.Run(t, &s)
 }
 
 func TestPlayerRepository_Get_InterpretDbData(t *testing.T) {
@@ -110,19 +117,6 @@ func TestPlayerRepository_Get_InterpretDbData(t *testing.T) {
 				&dummyInt,
 			},
 		},
-	}
-
-	suite.Run(t, &s)
-}
-
-func TestPlayerRepository_List_DbInteraction(t *testing.T) {
-	s := RepositoryPoolTestSuite{
-		testFunc: func(ctx context.Context, pool db.ConnectionPool) error {
-			repo := NewPlayerRepository(pool)
-			_, err := repo.List(ctx)
-			return err
-		},
-		expectedSql: `SELECT id, api_user, universe, name, created_at, updated_at, version FROM player`,
 	}
 
 	suite.Run(t, &s)
