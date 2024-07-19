c1c741bd168142722301bef11c9caf23d46cb35d
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-07-19 15:19:26 +0200

    feat: Added new mocks which will be used to transition to the new test suite for repositories

--- pkg/repositories/repository_mock_test.go
+++ pkg/repositories/repository_mock_test.go
@@ -6,121 +6,94 @@ import (
 	"github.com/KnoblauchPilze/user-service/pkg/db"
 )
 
-type mockConnectionPool struct {
+type mockConnectionPoolNew struct {
 	db.ConnectionPool
-
-	queryCalled int
-	execCalled  int
-
-	affectedRows int
-	execErr      error
 
 	sqlQuery string
 	args     []interface{}
 
-	rows mockRows
-}
-
-func (m *mockConnectionPool) Query(ctx context.Context, sql string, arguments ...interface{}) db.Rows {
-	m.queryCalled++
-	m.sqlQuery = sql
-	m.args = append(m.args, arguments...)
-	return &m.rows
-}
-
-func (m *mockConnectionPool) Exec(ctx context.Context, sql string, arguments ...interface{}) (int, error) {
-	m.execCalled++
-	m.sqlQuery = sql
-	m.args = append(m.args, arguments...)
-	return m.affectedRows, m.execErr
-}
-
-type mockRows struct {
-	err            error
-	singleValueErr error
-	allErr         error
-
-	singleValueCalled int
-	allCalled         int
-	scanner           *mockScannable
-}
-
-func (m *mockRows) Err() error { return m.err }
-
-func (m *mockRows) Empty() bool { return false }
-
-func (m *mockRows) Close() {}
-
-func (m *mockRows) GetSingleValue(parser db.RowParser) error {
-	m.singleValueCalled++
-	if m.scanner != nil {
-		return parser(m.scanner)
-	}
-	return m.singleValueErr
-}
-
-func (m *mockRows) GetAll(parser db.RowParser) error {
-	m.allCalled++
-	if m.scanner != nil {
-		return parser(m.scanner)
-	}
-	return m.allErr
-}
-
-type mockScannable struct {
-	err error
-
-	scanCalled int
-	props      [][]interface{}
-}
-
-func (m *mockScannable) Scan(dest ...interface{}) error {
-	m.scanCalled++
-
-	var newProps []interface{}
-	newProps = append(newProps, dest...)
-	m.props = append(m.props, newProps)
-
-	return m.err
-}
-
-type mockTransaction struct {
-	db.Transaction
-
 	queryCalled int
 	execCalled  int
 
+	rows         mockRowsNew
 	affectedRows int
 	execErr      error
-
-	sqlQueries []string
-	args       [][]interface{}
-
-	rows mockRows
 }
 
-func (m *mockTransaction) Close(ctx context.Context) {}
+func (m *mockConnectionPoolNew) Query(ctx context.Context, sql string, arguments ...interface{}) db.Rows {
+	m.sqlQuery = sql
+	m.args = append(m.args, arguments...)
 
-func (m *mockTransaction) Query(ctx context.Context, sql string, arguments ...interface{}) db.Rows {
 	m.queryCalled++
-
-	var newArgs []interface{}
-	newArgs = append(newArgs, arguments...)
-	m.args = append(m.args, newArgs)
-
-	m.sqlQueries = append(m.sqlQueries, sql)
 
 	return &m.rows
 }
 
-func (m *mockTransaction) Exec(ctx context.Context, sql string, arguments ...interface{}) (int, error) {
+func (m *mockConnectionPoolNew) Exec(ctx context.Context, sql string, arguments ...interface{}) (int, error) {
+	m.sqlQuery = sql
+	m.args = append(m.args, arguments...)
+
 	m.execCalled++
 
-	var newArgs []interface{}
-	newArgs = append(newArgs, arguments...)
-	m.args = append(m.args, newArgs)
-
-	m.sqlQueries = append(m.sqlQueries, sql)
-
 	return m.affectedRows, m.execErr
+}
+
+type mockRowsNew struct {
+	db.Rows
+
+	closeCalled          int
+	getSingleValueCalled int
+	getAllCalled         int
+
+	err                error
+	getSingleValueErrs []error
+	getAllErrs         []error
+
+	scanner *mockScannable
+}
+
+func (m *mockRowsNew) Err() error { return m.err }
+
+func (m *mockRowsNew) Close() {
+	m.closeCalled++
+}
+
+func (m *mockRowsNew) GetSingleValue(parser db.RowParser) error {
+	if m.scanner != nil {
+		return parser(m.scanner)
+	}
+
+	err := getValueToReturn(m.getSingleValueCalled, m.getSingleValueErrs)
+	m.getSingleValueCalled++
+
+	if err == nil {
+		return nil
+	}
+	return *err
+}
+
+func (m *mockRowsNew) GetAll(parser db.RowParser) error {
+	if m.scanner != nil {
+		return parser(m.scanner)
+	}
+
+	err := getValueToReturn(m.getAllCalled, m.getAllErrs)
+	m.getAllCalled++
+
+	if err == nil {
+		return nil
+	}
+	return *err
+}
+
+func getValueToReturn[T any](count int, values []T) *T {
+	var out *T
+	if count > len(values) {
+		count = 0
+	}
+	if count < len(values) {
+		out = &values[count]
+	}
+
+	return out
 }
