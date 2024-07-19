28a16aa3155c62709ae6bc1761f9ee3cf3b7809b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-07-19 15:50:32 +0200

    feat: Moved part of the planet repository tests to the new get all test suite

--- pkg/repositories/repository_pool_common_test.go
+++ pkg/repositories/repository_pool_common_test.go
@@ -42,11 +42,20 @@ type dbPoolSingleValueTestCase struct {
 	expectedScannedProps        [][]interface{}
 }
 
+type dbPoolGetAllTestCase struct {
+	handler testPoolFunc
+
+	expectedGetAllCalls  int
+	expectedScanCalls    int
+	expectedScannedProps [][]interface{}
+}
+
 type RepositoryTestSuite struct {
 	suite.Suite
 
 	dbPoolInteractionTestCases map[string]dbPoolInteractionTestCase
 	dbPoolSingleValueTestCases map[string]dbPoolSingleValueTestCase
+	dbPoolGetAllTestCases      map[string]dbPoolGetAllTestCase
 	dbPoolReturnTestCases      map[string]dbPoolReturnTestCase
 }
 
@@ -163,6 +172,93 @@ func (s *RepositoryTestSuite) TestGetSingleValue_PropagatesScanError() {
 
 func (s *RepositoryTestSuite) TestGetSingleValue_ExpectedCorrectPropertiesAreScanned() {
 	for name, testCase := range s.dbPoolSingleValueTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			scanner := &mockScannable{}
+			m := &mockConnectionPoolNew{
+				rows: mockRowsNew{
+					scanner: scanner,
+				},
+			}
+
+			testCase.handler(context.Background(), m)
+
+			s.Require().Equal(testCase.expectedScanCalls, scanner.scanCalled)
+			s.Require().Equal(len(testCase.expectedScannedProps), len(scanner.props))
+
+			for id, expectedProps := range testCase.expectedScannedProps {
+				actualProps := scanner.props[id]
+
+				s.Require().Equal(len(expectedProps), len(actualProps))
+
+				for idProp, expectedProp := range expectedProps {
+					actualProp := actualProps[idProp]
+					s.Require().IsType(expectedProp, actualProp)
+				}
+			}
+		})
+	}
+}
+
+func (s *RepositoryTestSuite) TestGetAll_ExpectCorrectNumberOfCalls() {
+	for name, testCase := range s.dbPoolGetAllTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			m := &mockConnectionPoolNew{}
+
+			testCase.handler(context.Background(), m)
+
+			s.Require().Equal(testCase.expectedGetAllCalls, m.rows.getAllCalled)
+		})
+	}
+}
+
+func (s *RepositoryTestSuite) TestGetAll_WhenSuccess_ExpectNoError() {
+	for name, testCase := range s.dbPoolGetAllTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			m := &mockConnectionPoolNew{}
+
+			err := testCase.handler(context.Background(), m)
+
+			s.Require().Nil(err)
+		})
+	}
+}
+
+func (s *RepositoryTestSuite) TestGetAll_PropagatesError() {
+	for name, testCase := range s.dbPoolGetAllTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			m := &mockConnectionPoolNew{
+				rows: mockRowsNew{
+					getAllErrs: []error{errDefault},
+				},
+			}
+
+			err := testCase.handler(context.Background(), m)
+
+			s.Require().Equal(errDefault, err)
+		})
+	}
+}
+
+func (s *RepositoryTestSuite) TestGetAll_PropagatesScanError() {
+	for name, testCase := range s.dbPoolGetAllTestCases {
+		s.T().Run(name, func(t *testing.T) {
+			m := &mockConnectionPoolNew{
+				rows: mockRowsNew{
+					scanner: &mockScannable{
+						err: errDefault,
+					},
+				},
+			}
+
+			err := testCase.handler(context.Background(), m)
+
+			s.Require().Equal(errDefault, err)
+		})
+	}
+}
+
+func (s *RepositoryTestSuite) TestGetAll_ExpectedCorrectPropertiesAreScanned() {
+	for name, testCase := range s.dbPoolGetAllTestCases {
 		s.T().Run(name, func(t *testing.T) {
 			scanner := &mockScannable{}
 			m := &mockConnectionPoolNew{
