4a36462a3f001a74e898574f67528fd9a710c603
KnoblauchPilze <prolific-programmer@tuta.com>
2024-07-21 16:08:54 +0200

    feat: Login page requires to set the name of the player and fetches its id as a cookie

--- website/stellar-dominion/src/routes/login/+page.server.ts
+++ website/stellar-dominion/src/routes/login/+page.server.ts
@@ -1,7 +1,7 @@
 import { error, redirect } from '@sveltejs/kit';
-import { loginUser } from '$lib/sessions';
+import { fetchPlayerFromApiUser, responseToPlayerArray } from '$lib/players';
+import ApiKey, { loginUser } from '$lib/sessions';
 import Universe, { getUniverses } from '$lib/universes';
-import ApiKey from '$lib/apiKey.js';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ cookies }) {
@@ -41,8 +41,19 @@ export const actions = {
 	login: async ({ cookies, request }) => {
 		const data = await request.formData();
 
+		const universeId = data.get('universe');
 		const email = data.get('email');
 		const password = data.get('password');
+		const playerName = data.get('player');
+		if (!universeId) {
+			return {
+				success: false,
+				missing: true,
+				message: 'Please select a universe',
+
+				universeId
+			};
+		}
 		if (!email) {
 			return {
 				success: false,
@@ -61,9 +72,17 @@ export const actions = {
 				email
 			};
 		}
+		if (!playerName) {
+			return {
+				success: false,
+				missing: true,
+				message: 'Please choose a name',
+
+				email
+			};
+		}
 
 		const loginResponse = await loginUser(email as string, password as string);
-
 		if (loginResponse.error()) {
 			return {
 				success: false,
@@ -76,11 +95,40 @@ export const actions = {
 
 		const apiKey = new ApiKey(loginResponse);
 
+		const playerResponse = await fetchPlayerFromApiUser(apiKey.user, apiKey.key);
+		if (playerResponse.error()) {
+			return {
+				success: false,
+				incorrect: true,
+				message: playerResponse.failureMessage(),
+
+				email
+			};
+		}
+
+		const players = responseToPlayerArray(playerResponse);
+		const maybePlayer = players.find(
+			(player) => player.universe === universeId && player.name === playerName
+		);
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+		if (maybePlayer === undefined) {
+			return {
+				success: false,
+				incorrect: true,
+				message: 'No such player',
+
+				email
+			};
+		}
+
+		console.log('player: ', JSON.stringify(maybePlayer));
+
 		const opts = {
 			path: '/'
 		};
 		cookies.set('api-user', apiKey.user, opts);
 		cookies.set('api-key', apiKey.key, opts);
+		cookies.set('player-id', maybePlayer.id, opts);
 
 		// TODO: Should fetch the planet and redirect to the overview
 		redirect(303, '/dashboard/overview');
