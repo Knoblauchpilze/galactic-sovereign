b18cb86ace82cca9c2b6517e601ab82b64a7208b
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-07-24 22:45:32 +0200

    feat: Added matcher to make sure planet route slug is an id

--- website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.server.ts
+++ website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.server.ts
@@ -2,15 +2,8 @@ import { error, redirect } from '@sveltejs/kit';
 import Planet, { getPlanet } from '$lib/planets';
 import { ApiFailureReason } from '$lib/responseEnvelope.js';
 
-// https://stackoverflow.com/questions/7905929/how-to-test-valid-uuid-guid
-const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, cookies }) {
-	if (!(typeof params.planet === 'string') || !UUID_REGEX.test(params.planet)) {
-		error(404, 'Planet not found');
-	}
-
 	const apiKey = cookies.get('api-key');
 	if (!apiKey) {
 		redirect(303, '/login');
