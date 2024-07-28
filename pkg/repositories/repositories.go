b8d923f5ef81023f0d164d576e42a2f47e165ca4
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-07-28 15:44:15 +0200

    feat: Added the planet resource repo as attribute of the planet service

--- pkg/repositories/repositories.go
+++ pkg/repositories/repositories.go
@@ -1,11 +1,12 @@
 package repositories
 
 type Repositories struct {
-	Acl       AclRepository
-	ApiKey    ApiKeyRepository
-	UserLimit UserLimitRepository
-	User      UserRepository
-	Universe  UniverseRepository
-	Player    PlayerRepository
-	Planet    PlanetRepository
+	Acl            AclRepository
+	ApiKey         ApiKeyRepository
+	UserLimit      UserLimitRepository
+	User           UserRepository
+	Universe       UniverseRepository
+	Planet         PlanetRepository
+	PlanetResource PlanetResourceRepository
+	Player         PlayerRepository
 }
