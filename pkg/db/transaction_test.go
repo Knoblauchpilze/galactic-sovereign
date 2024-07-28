7993b12012cfbddb2270188936dba9e9d0327697
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-07-28 16:33:49 +0200

    feat: Restored timestamp in transaction

--- pkg/db/transaction_test.go
+++ pkg/db/transaction_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
@@ -174,6 +175,19 @@ func TestTransaction_Close_WhenNoError_CallsCommit(t *testing.T) {
 	tx.Close(context.Background())
 
 	assert.Equal(1, mt.commitCalled)
+}
+
+func TestTransation_TimeStamp_ReturnsInputTimeStamp(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := time.Now()
+
+	tx := transactionImpl{
+		tx:        &mockPgxTransaction{},
+		timeStamp: expected,
+	}
+
+	assert.Equal(expected, tx.TimeStamp())
 }
 
 func (m *mockPgxTransaction) Rollback(ctx context.Context) error {
