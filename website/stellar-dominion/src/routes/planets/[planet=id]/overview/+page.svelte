bf48f74c18489ff79579dac7f23cf996823b733d
KnoblauchPilze <prolific-programmer@tuta.com>
2024-08-02 17:06:56 +0200

    feat: Define the size of the hero container for the overview page

--- website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
+++ website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
@@ -2,7 +2,8 @@
 	import '$styles/app.css';
 	import { CenteredWrapper, StyledText, StyledTitle } from '$lib/components';
 
-	import heroImage from '$lib/heroImage';
+	import heroImage from '$lib/stores/heroImage';
+	import heroContainer from '$lib/stores/heroContainer';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -13,9 +14,14 @@
 
 	// https://stackoverflow.com/questions/75616911/sveltekit-fetching-on-the-server-and-updating-the-writable-store
 	heroImage.set('bg-overview');
+	heroContainer.set({
+		width: 'w-full',
+		height: 'h-full',
+		color: 'bg-transparent'
+	});
 </script>
 
-<CenteredWrapper>
+<CenteredWrapper width='w-4/5' height='h-4/5' bgColor='bg-overlay'>
 	<StyledTitle text="Welcome to {name}!" />
 	<StyledText text="Your id is {id}, and you are in the empire of {player}" />
 	<StyledText text="This page will soon contain more information!" />
