4e77c2e40fdc9b99ab093db9c3bc99d10f93a769
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-08-02 17:06:27 +0200

    feat: Header and Footer components

---
+++ website/stellar-dominion/src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let height: string = 'h-1/10';
+</script>
+
+<div class="fixed bottom-0 w-full {height} flex justify-around items-center bg-black text-white">
+	<slot />
+</div>
