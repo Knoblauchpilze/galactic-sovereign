1d0b787c2108da4b32958cfabb0f19144454798d
KnoblauchPilze <prolific-programmer@tuta.com>
2024-08-02 15:52:19 +0200

    feat: Also use components for the overview page

--- website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
+++ website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+	import '$styles/app.css';
+	import {
+		CenteredWrapper,
+		StyledText,
+		StyledTitle
+	} from '$lib/components';
+
 	/** @type {import('./$types').PageData} */
 	export let data;
 
@@ -7,23 +14,8 @@
 	let name: string = data.planet.name;
 </script>
 
-<div class="wrapper">
-	<h1>Welcome to {name}!</h1>
-	<h2>Your id is {id}, and you are in the empire of {player}</h2>
-	<h2>This page will soon contain more information!</h2>
-</div>
-
-<style>
-	.wrapper {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	h1, h2 {
-		padding: 0.5em 1.5em;
-
-		color: #b87333;
-	}
-</style>
+<CenteredWrapper>
+	<StyledTitle text="Welcome to {name}!" />
+	<StyledText text="Your id is {id}, and you are in the empire of {player}" />
+	<StyledText text="This page will soon contain more information!" />
+</CenteredWrapper>
