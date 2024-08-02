5e60e99c148459881576c3027cdf9af6ffbbb81b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-08-02 17:04:08 +0200

    feat: Background color in CenteredWrapper can be configured

--- website/stellar-dominion/src/lib/components/CenteredWrapper.svelte
+++ website/stellar-dominion/src/lib/components/CenteredWrapper.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	export let width: string = 'w-full';
 	export let height: string = 'h-full';
+
+	export let bgColor: string = 'bg-transparent';
 </script>
 
 <!-- https://stackoverflow.com/questions/67852559/pass-svelte-component-as-props -->
 <!-- https://svelte.dev/tutorial/slots -->
-<div class="{width} {height} flex flex-col justify-around items-center">
+<div class="{width} {height} flex flex-col justify-around items-center {bgColor}">
 	<slot />
 </div>
