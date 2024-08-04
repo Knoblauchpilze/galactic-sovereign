9825f516ab1cab01f37e9b74ea99fcba251471ab
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-08-04 18:17:43 +0200

    feat: Convenience method to load cookies from the page data

--- website/stellar-dominion/src/lib/cookies.ts
+++ website/stellar-dominion/src/lib/cookies.ts
@@ -11,6 +11,8 @@ const COOKIE_KEY_API_KEY = 'api-key';
 const COOKIE_KEY_PLAYER_ID = 'player-id';
 const COOKIE_KEY_UNIVERSE_ID = 'universe-id';
 
+export { COOKIE_KEY_API_USER, COOKIE_KEY_API_KEY, COOKIE_KEY_PLAYER_ID, COOKIE_KEY_UNIVERSE_ID };
+
 export function resetCookies(cookies: Cookies) {
 	cookies.set(COOKIE_KEY_API_USER, '', DEFAULT_COOKIES_OPT);
 	cookies.set(COOKIE_KEY_API_KEY, '', DEFAULT_COOKIES_OPT);
@@ -23,4 +25,37 @@ export function setCookies(cookies: Cookies, apiKey: ApiKey, player: Player) {
 	cookies.set(COOKIE_KEY_API_KEY, apiKey.key, DEFAULT_COOKIES_OPT);
 	cookies.set(COOKIE_KEY_PLAYER_ID, player.id, DEFAULT_COOKIES_OPT);
 	cookies.set(COOKIE_KEY_UNIVERSE_ID, player.universe, DEFAULT_COOKIES_OPT);
+}
+
+export interface GameCookies {
+	readonly apiUser: string;
+	readonly apiKey: string;
+	readonly playerId: string;
+	readonly universeId: string;
+}
+
+function validOrEmptyString(maybeValue: string | undefined, valid: boolean): string {
+	return valid ? (maybeValue as string) : '';
+}
+
+export function loadCookies(cookies: Cookies): [boolean, GameCookies] {
+	const maybeApiUser = cookies.get(COOKIE_KEY_API_USER);
+	const maybeApiKey = cookies.get(COOKIE_KEY_API_KEY);
+	const maybePlayerId = cookies.get(COOKIE_KEY_PLAYER_ID);
+	const maybeUniverseId = cookies.get(COOKIE_KEY_UNIVERSE_ID);
+
+	const validApiUser = maybeApiUser !== undefined;
+	const validApiKey = maybeApiKey !== undefined;
+	const validPlayerId = maybePlayerId !== undefined;
+	const validUniverseId = maybeUniverseId !== undefined;
+	const valid = validApiUser || validApiKey || validPlayerId || validUniverseId;
+
+	const out: GameCookies = {
+		apiUser: validOrEmptyString(maybeApiUser, validApiUser),
+		apiKey: validOrEmptyString(maybeApiUser, validApiKey),
+		playerId: validOrEmptyString(maybeApiUser, validPlayerId),
+		universeId: validOrEmptyString(maybeApiUser, validUniverseId)
+	};
+
+	return [valid, out];
 }
