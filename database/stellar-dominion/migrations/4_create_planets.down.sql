8fcada4734aec99d80d6bbc5d4d54495be9da811
KnoblauchPilze <prolific-programmer@tuta.com>
2024-08-05 21:44:38 +0200

    feat: Defined buildings and buildings costs in the DB

--- database/stellar-dominion/migrations/4_create_planets.down.sql
+++ database/stellar-dominion/migrations/4_create_planets.down.sql
@@ -1,7 +1,10 @@
 
+DROP TRIGGER trigger_building_updated_at ON building;
 DROP TRIGGER trigger_resource_updated_at ON resource;
 DROP TRIGGER trigger_planet_updated_at ON planet;
 
+DROP TABLE building_cost;
+DROP TABLE building;
 DROP TABLE planet_resource;
 DROP TABLE resource;
 DROP TABLE homeworld;
--- database/stellar-dominion/migrations/4_create_planets.up.sql
+++ database/stellar-dominion/migrations/4_create_planets.up.sql
@@ -49,3 +49,25 @@ CREATE TABLE planet_resource (
   FOREIGN KEY (resource) REFERENCES resource(id),
   UNIQUE (planet, resource)
 );
+
+CREATE TABLE building (
+  id uuid NOT NULL,
+  name text NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+CREATE TRIGGER trigger_building_updated_at
+  BEFORE UPDATE OR INSERT ON building
+  FOR EACH ROW
+  EXECUTE FUNCTION update_updated_at();
+
+CREATE TABLE building_cost (
+  building uuid NOT NULL,
+  resource uuid NOT NULL,
+  cost integer NOT NULL,
+  FOREIGN KEY (building) REFERENCES building(id),
+  FOREIGN KEY (resource) REFERENCES resource(id),
+  UNIQUE (building, resource)
+);
