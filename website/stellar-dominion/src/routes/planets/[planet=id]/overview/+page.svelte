940f9839cf4b266ae14e9ab5c1a51cda44c7bc7f
KnoblauchPilze <prolific-programmer@tuta.com>
2024-08-06 22:28:42 +0200

    feat: Dummy content to display the buildings

--- website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
+++ website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
@@ -6,6 +6,7 @@
 	import heroContainer, { GAME_HERO_CONTAINER_PROPS } from '$lib/stores/ui/heroContainer';
 
 	import { mapPlanetResourcesToApiResources } from '$lib/resources';
+	import { mapPlanetBuildingsToApiBuildings } from '$lib/buildings';
 
 	// https://svelte.dev/blog/zero-config-type-safety
 	export let data;
@@ -13,19 +14,20 @@
 	const id: string = data.planet.id;
 	const player: string = data.planet.player;
 	const planetName: string = data.planet.name;
-	const universeName : string = data.universe.name;
+	const universeName: string = data.universe.name;
 
 	// https://stackoverflow.com/questions/75616911/sveltekit-fetching-on-the-server-and-updating-the-writable-store
 	heroImage.set(GAME_HERO_IMAGE);
 	heroContainer.set(GAME_HERO_CONTAINER_PROPS);
 
 	const resources = mapPlanetResourcesToApiResources(data.planet.resources, data.resources);
+	const buildings = mapPlanetBuildingsToApiBuildings(data.planet.buildings, data.buildings);
 </script>
 
 <CenteredWrapper width="w-4/5" height="h-4/5" bgColor="bg-overlay">
 	<Header>
-		<StyledText text={universeName} textColor="text-white"/>
-		<StyledText text={planetName} textColor="text-white"/>
+		<StyledText text={universeName} textColor="text-white" />
+		<StyledText text={planetName} textColor="text-white" />
 		<form method="POST" action="?/logout">
 			<button class="hover:underline">Logout</button>
 		</form>
@@ -35,6 +37,12 @@
 		<div class="flex justify-around bg-black">
 			{#each resources as resource}
 				<StyledText text="{resource.name}: {resource.amount}" textColor="text-white" />
+			{/each}
+		</div>
+		<!-- TODO: Interpret this with a new component -->
+		<div class="flex justify-around bg-black">
+			{#each buildings as building}
+				<StyledText text="{building.name}: {building.level}" textColor="text-white" />
 			{/each}
 		</div>
 
