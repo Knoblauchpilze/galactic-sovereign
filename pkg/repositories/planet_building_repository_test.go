d02afbb35add34f09b31db7938f7f8fc54022244
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-08-06 21:26:48 +0200

    feat: Added planet building repository

---
+++ pkg/repositories/planet_building_repository_test.go
@@ -0,0 +1,91 @@
+package repositories
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/suite"
+)
+
+func Test_PlanetBuildingRepository_Transaction(t *testing.T) {
+	var dummyInt int
+
+	s := RepositoryTransactionTestSuite{
+		dbInteractionTestCases: map[string]dbTransactionInteractionTestCase{
+			"listForPlanet": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetBuildingRepository()
+					_, err := s.ListForPlanet(ctx, tx, defaultPlanetId)
+					return err
+				},
+				expectedSqlQueries: []string{
+					`
+SELECT
+	planet,
+	building,
+	level,
+	created_at,
+	updated_at,
+	version
+FROM
+	planet_building
+WHERE
+	planet = $1
+`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultPlanetId,
+					},
+				},
+			},
+			"deleteForPlanet": {
+				sqlMode: ExecBased,
+				generateMock: func() db.Transaction {
+					return &mockTransaction{
+						affectedRows: 1,
+					}
+				},
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetBuildingRepository()
+					return s.DeleteForPlanet(ctx, tx, defaultPlanetId)
+				},
+				expectedSqlQueries: []string{
+					`DELETE FROM planet_building WHERE planet = $1`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultPlanetId,
+					},
+				},
+			},
+		},
+
+		dbGetAllTestCases: map[string]dbTransactionGetAllTestCase{
+			"listForPlanet": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					repo := NewPlanetBuildingRepository()
+					_, err := repo.ListForPlanet(ctx, tx, defaultPlanetId)
+					return err
+				},
+				expectedGetAllCalls: 1,
+				expectedScanCalls:   1,
+				expectedScannedProps: [][]interface{}{
+					{
+						&uuid.UUID{},
+						&uuid.UUID{},
+						&dummyInt,
+						&time.Time{},
+						&time.Time{},
+						&dummyInt,
+					},
+				},
+			},
+		},
+	}
+
+	suite.Run(t, &s)
+}
