5a3a4aa06d9f3a9f5f01ffc07d59cb17128ccfab
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-08-08 21:28:57 +0200

    feat: Added building cost repository

---
+++ pkg/repositories/building_cost_repository_test.go
@@ -0,0 +1,72 @@
+package repositories
+
+import (
+	"context"
+	"testing"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/suite"
+)
+
+var defaultBuildingId = uuid.MustParse("9c5a9f5c-b53e-4f5c-af6f-cb04e47abd95")
+var defaultBuildingCost = persistence.BuildingCost{
+	Building: defaultBuildingId,
+	Resource: defaultResourceId,
+	Cost:     73,
+}
+
+func Test_BuildingCostRepository_Transaction(t *testing.T) {
+	var dummyInt int
+
+	s := RepositoryTransactionTestSuite{
+		dbInteractionTestCases: map[string]dbTransactionInteractionTestCase{
+			"listForBuilding": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewBuildingCostRepository()
+					_, err := s.ListForBuilding(ctx, tx, defaultBuildingId)
+					return err
+				},
+				expectedSqlQueries: []string{
+					`
+SELECT
+	building,
+	resource,
+	cost
+FROM
+	building_cost
+WHERE
+	building = $1
+`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultBuildingId,
+					},
+				},
+			},
+		},
+
+		dbGetAllTestCases: map[string]dbTransactionGetAllTestCase{
+			"listForBuilding": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					repo := NewBuildingCostRepository()
+					_, err := repo.ListForBuilding(ctx, tx, defaultBuildingId)
+					return err
+				},
+				expectedGetAllCalls: 1,
+				expectedScanCalls:   1,
+				expectedScannedProps: [][]interface{}{
+					{
+						&uuid.UUID{},
+						&uuid.UUID{},
+						&dummyInt,
+					},
+				},
+			},
+		},
+	}
+
+	suite.Run(t, &s)
+}
