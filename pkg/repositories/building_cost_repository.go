ebf623a47975d9964e0d4bcb1c66ee0c48d8abb3
KnoblauchPilze <prolific-programmer@tuta.com>
2024-08-08 21:28:57 +0200

    feat: Added building cost repository

---
+++ pkg/repositories/building_cost_repository.go
@@ -0,0 +1,55 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/google/uuid"
+)
+
+type BuildingCostRepository interface {
+	ListForBuilding(ctx context.Context, tx db.Transaction, building uuid.UUID) ([]persistence.BuildingCost, error)
+}
+
+type buildingCostRepositoryImpl struct{}
+
+func NewBuildingCostRepository() BuildingCostRepository {
+	return &buildingCostRepositoryImpl{}
+}
+
+const listBuildingCostForBuildingSqlTemplate = `
+SELECT
+	building,
+	resource,
+	cost
+FROM
+	building_cost
+WHERE
+	building = $1
+`
+
+func (r *buildingCostRepositoryImpl) ListForBuilding(ctx context.Context, tx db.Transaction, building uuid.UUID) ([]persistence.BuildingCost, error) {
+	res := tx.Query(ctx, listBuildingCostForBuildingSqlTemplate, building)
+	if err := res.Err(); err != nil {
+		return []persistence.BuildingCost{}, err
+	}
+
+	var out []persistence.BuildingCost
+	parser := func(rows db.Scannable) error {
+		var cost persistence.BuildingCost
+		err := rows.Scan(&cost.Building, &cost.Resource, &cost.Cost)
+		if err != nil {
+			return err
+		}
+
+		out = append(out, cost)
+		return nil
+	}
+
+	if err := res.GetAll(parser); err != nil {
+		return []persistence.BuildingCost{}, err
+	}
+
+	return out, nil
+}
