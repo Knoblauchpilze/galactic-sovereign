aa6fbcaf432b1c4137b8ef9a6b0b53e02076f679
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-08-09 17:42:30 +0200

    feat: Updated tab title based on the current page

--- website/stellar-dominion/src/routes/+layout.svelte
+++ website/stellar-dominion/src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import '$styles/app.css';
 	import { HeroContainer } from '$lib/components';
+
+	import pageTitle from "$lib/stores/ui/pageTitle";
 </script>
 
+<!-- https://stackoverflow.com/questions/74243719/how-to-change-page-title-dynamically-in-sveltekit -->
+<svelte:head>
+	<title>{$pageTitle}</title>
+</svelte:head>
 <!-- https://stackoverflow.com/questions/70805041/background-image-in-tailwindcss-using-dynamic-url-react-js -->
 <HeroContainer>
 	<slot/>
