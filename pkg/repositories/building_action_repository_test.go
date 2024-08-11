a3bfd4c6e763c059282c8329e5552b82bf274bc3
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-08-11 13:48:12 +0200

    feat: Added building action repository and tests and added an index to filter building actions per planet

---
+++ pkg/repositories/building_action_repository_test.go
@@ -0,0 +1,72 @@
+package repositories
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/suite"
+)
+
+func Test_BuildingActionRepository_Transaction(t *testing.T) {
+	var dummyInt int
+
+	s := RepositoryTransactionTestSuite{
+		dbInteractionTestCases: map[string]dbTransactionInteractionTestCase{
+			"listForPlanet": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewBuildingActionRepository()
+					_, err := s.ListForPlanet(ctx, tx, defaultPlanetId)
+					return err
+				},
+				expectedSqlQueries: []string{
+					`
+SELECT
+	id,
+	planet,
+	building,
+	current_level,
+	desired_level,
+	created_at,
+	completed_at
+FROM
+	building_action
+WHERE
+	planet = $1`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultPlanetId,
+					},
+				},
+			},
+		},
+
+		dbGetAllTestCases: map[string]dbTransactionGetAllTestCase{
+			"listForPlanet": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					repo := NewBuildingActionRepository()
+					_, err := repo.ListForPlanet(ctx, tx, defaultPlanetId)
+					return err
+				},
+				expectedGetAllCalls: 1,
+				expectedScanCalls:   1,
+				expectedScannedProps: [][]interface{}{
+					{
+						&uuid.UUID{},
+						&uuid.UUID{},
+						&uuid.UUID{},
+						&dummyInt,
+						&dummyInt,
+						&time.Time{},
+						&time.Time{},
+					},
+				},
+			},
+		},
+	}
+
+	suite.Run(t, &s)
+}
