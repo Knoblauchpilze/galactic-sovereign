b8121d93569a92a30769aaf17da38b35583be28d
KnoblauchPilze <prolific-programmer@tuta.com>
2024-08-14 21:01:50 +0200

    feat: Added completion time logic and tests

---
+++ pkg/game/completion_time.go
@@ -0,0 +1,54 @@
+package game
+
+import (
+	"math"
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+)
+
+const resourceUnitsPerHour float64 = 2500.0
+
+func findResourceByName(name string, resources []persistence.Resource) *persistence.Resource {
+	for _, resource := range resources {
+		if resource.Name == name {
+			return &resource
+		}
+	}
+
+	return nil
+}
+
+func findResourceRequirementByName(name string, resources []persistence.Resource, costs []persistence.BuildingCost) (float64, error) {
+	resource := findResourceByName(name, resources)
+	if resource == nil {
+		return 0.0, errors.NewCode(NoSuchResource)
+	}
+
+	for _, cost := range costs {
+		if cost.Resource == resource.Id {
+			return float64(cost.Cost), nil
+		}
+	}
+
+	return 0.0, nil
+}
+
+func buildingCompletionTimeFromCost(resources []persistence.Resource, costs []persistence.BuildingCost) (time.Duration, error) {
+	// https://ogame.fandom.com/wiki/Buildings
+	metal, err := findResourceRequirementByName("metal", resources, costs)
+	if err != nil {
+		return 0, err
+	}
+	crystal, err := findResourceRequirementByName("crystal", resources, costs)
+	if err != nil {
+		return 0, err
+	}
+
+	buildTimeHour := (metal + crystal) / resourceUnitsPerHour
+
+	hours := math.Ceil(buildTimeHour * float64(time.Hour.Nanoseconds()))
+
+	return time.Duration(hours), nil
+}
---
+++ pkg/game/completion_time_test.go
@@ -0,0 +1,127 @@
+package game
+
+import (
+	"testing"
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+var defaultMetalId = uuid.MustParse("24c2a21a-3de8-42dd-bee4-8652e8368a5c")
+var defaultCrystalId = uuid.MustParse("4e8a8ee5-668e-42c4-a4a5-938e5a68741c")
+
+func TestBuildingCompletionTimeFromCost_whenMetalNotFound_expectError(t *testing.T) {
+	assert := assert.New(t)
+
+	resources := []persistence.Resource{}
+	costs := []persistence.BuildingCost{}
+
+	_, err := buildingCompletionTimeFromCost(resources, costs)
+
+	assert.True(errors.IsErrorWithCode(err, NoSuchResource))
+}
+
+func TestBuildingCompletionTimeFromCost_whenCrystalNotFound_expectError(t *testing.T) {
+	assert := assert.New(t)
+
+	resources := []persistence.Resource{
+		{
+			Id:   defaultMetalId,
+			Name: "metal",
+		},
+	}
+	costs := []persistence.BuildingCost{}
+
+	_, err := buildingCompletionTimeFromCost(resources, costs)
+
+	assert.True(errors.IsErrorWithCode(err, NoSuchResource))
+}
+
+func TestBuildingCompletionTimeFromCost_onlyMetalCost(t *testing.T) {
+	assert := assert.New(t)
+
+	resources := []persistence.Resource{
+		{
+			Id:   defaultMetalId,
+			Name: "metal",
+		},
+		{
+			Id:   defaultCrystalId,
+			Name: "crystal",
+		},
+	}
+	costs := []persistence.BuildingCost{
+		{
+			Resource: defaultMetalId,
+			Cost:     1250,
+		},
+	}
+
+	duration, err := buildingCompletionTimeFromCost(resources, costs)
+
+	assert.Nil(err)
+	assert.Equal(30*time.Minute, duration)
+}
+
+func TestBuildingCompletionTimeFromCost_onlyCrystalCost(t *testing.T) {
+	assert := assert.New(t)
+
+	resources := []persistence.Resource{
+		{
+			Id:   defaultMetalId,
+			Name: "metal",
+		},
+		{
+			Id:   defaultCrystalId,
+			Name: "crystal",
+		},
+	}
+	costs := []persistence.BuildingCost{
+		{
+			Resource: defaultCrystalId,
+			Cost:     3000,
+		},
+	}
+
+	duration, err := buildingCompletionTimeFromCost(resources, costs)
+
+	assert.Nil(err)
+	expectedDuration, err2 := time.ParseDuration("1h12m")
+	assert.Nil(err2)
+	assert.Equal(expectedDuration, duration)
+}
+
+func TestBuildingCompletionTimeFromCost_metalAndCrystal(t *testing.T) {
+	assert := assert.New(t)
+
+	resources := []persistence.Resource{
+		{
+			Id:   defaultMetalId,
+			Name: "metal",
+		},
+		{
+			Id:   defaultCrystalId,
+			Name: "crystal",
+		},
+	}
+	costs := []persistence.BuildingCost{
+		{
+			Resource: defaultMetalId,
+			Cost:     5,
+		},
+		{
+			Resource: defaultCrystalId,
+			Cost:     5,
+		},
+	}
+
+	duration, err := buildingCompletionTimeFromCost(resources, costs)
+
+	assert.Nil(err)
+	expectedDuration, err2 := time.ParseDuration("14.4s")
+	assert.Nil(err2)
+	assert.Equal(expectedDuration, duration)
+}
