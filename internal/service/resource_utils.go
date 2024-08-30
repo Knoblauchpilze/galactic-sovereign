3fce7f258f3a7605034822e2c6a8602c99c35fd6
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-08-30 17:17:00 +0200

    feat: Extracted a separate module to update resources, added tests and also restored resources when an action is deleted

---
+++ internal/service/resource_utils.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/KnoblauchPilze/user-service/pkg/repositories"
+)
+
+type updateFunc func(existing float64, change float64) float64
+
+var addResource = func(existing float64, change float64) float64 {
+	return existing + change
+}
+var subtractResource = func(existing float64, change float64) float64 {
+	return existing - change
+}
+
+func findResourceForCost(resources []persistence.PlanetResource, cost persistence.BuildingActionCost) (persistence.PlanetResource, error) {
+	for _, resource := range resources {
+		if resource.Resource == cost.Resource {
+			return resource, nil
+		}
+	}
+
+	return persistence.PlanetResource{}, errors.NewCode(noSuchResource)
+}
+
+func updatePlanetResourceWithCosts(ctx context.Context, tx db.Transaction, repo repositories.PlanetResourceRepository, resources []persistence.PlanetResource, costs []persistence.BuildingActionCost, update updateFunc) error {
+	for _, cost := range costs {
+		resource, err := findResourceForCost(resources, cost)
+		if err != nil {
+			if errors.IsErrorWithCode(err, noSuchResource) {
+				return errors.NewCode(FailedToCreateAction)
+			}
+
+			return err
+		}
+
+		planetResource := resource
+		planetResource.Amount = update(planetResource.Amount, float64(cost.Amount))
+
+		_, err = repo.Update(ctx, tx, planetResource)
+		if err != nil {
+			if errors.IsErrorWithCode(err, db.OptimisticLockException) {
+				return errors.NewCode(ConflictingStateForAction)
+			}
+
+			return err
+		}
+	}
+
+	return nil
+}
