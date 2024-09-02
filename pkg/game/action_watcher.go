cc8658c753284ccca9a97052190ace251365f24b
KnoblauchPilze <prolific-programmer@tuta.com>
2024-09-02 22:12:28 +0200

    fix: Missing action watcher code

---
+++ pkg/game/action_watcher.go
@@ -0,0 +1,42 @@
+package game
+
+import (
+	"context"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ActionService interface {
+	ProcessActionsUntil(ctx context.Context, until time.Time) error
+}
+
+type actionProcessingData struct {
+	lock    sync.Mutex
+	service ActionService
+}
+
+func ActionWatcher(service ActionService, next echo.HandlerFunc) echo.HandlerFunc {
+	data := actionProcessingData{
+		service: service,
+	}
+
+	return func(c echo.Context) error {
+		err := data.schedulePendingActions(c.Request().Context())
+		if err != nil {
+			c.Logger().Errorf("Failed to scheduled pending actions %v", err)
+			return c.JSON(http.StatusInternalServerError, "Failed to process action")
+		}
+
+		return next(c)
+	}
+}
+
+func (data *actionProcessingData) schedulePendingActions(ctx context.Context) error {
+	data.lock.Lock()
+	defer data.lock.Unlock()
+
+	return data.service.ProcessActionsUntil(ctx, time.Now())
+}
