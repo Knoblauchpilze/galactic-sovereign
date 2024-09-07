e87eb02db43bf423758bf039ccfe9e81a50f30da
KnoblauchPilze <prolific-programmer@tuta.com>
2024-09-07 11:08:41 +0200

    feat: Added factor to scale building costs with the level

--- database/stellar-dominion/migrations/100_seed_game_data.up.sql
+++ database/stellar-dominion/migrations/100_seed_game_data.up.sql
@@ -17,34 +17,38 @@ INSERT INTO stellar_dominion_schema.resource ("id", "name", "start_amount")
 INSERT INTO stellar_dominion_schema.building ("id", "name")
   VALUES ('d176e82d-f2ca-4611-996b-c4804096caef', 'metal mine');
 
-INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost")
+INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost", "progress")
   VALUES (
     'd176e82d-f2ca-4611-996b-c4804096caef',
     'b4419b6b-b3bf-4576-aa92-055283addbc8',
-    60
+    60,
+    1.5
   );
 
-INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost")
+INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost", "progress")
   VALUES (
     'd176e82d-f2ca-4611-996b-c4804096caef',
     'cd2ac9aa-9968-4ff5-b746-88f1f810fbb3',
-    15
+    15,
+    1.5
   );
 
 
 INSERT INTO stellar_dominion_schema.building ("id", "name")
   VALUES ('3904d34d-9a7e-47d4-a332-091700e2c5c3', 'crystal mine');
 
-INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost")
+INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost", "progress")
   VALUES (
     '3904d34d-9a7e-47d4-a332-091700e2c5c3',
     'b4419b6b-b3bf-4576-aa92-055283addbc8',
-    48
+    48,
+    1.6
   );
 
-INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost")
+INSERT INTO stellar_dominion_schema.building_cost ("building", "resource", "cost", "progress")
   VALUES (
     '3904d34d-9a7e-47d4-a332-091700e2c5c3',
     'cd2ac9aa-9968-4ff5-b746-88f1f810fbb3',
-    24
+    24,
+    1.6
   );
