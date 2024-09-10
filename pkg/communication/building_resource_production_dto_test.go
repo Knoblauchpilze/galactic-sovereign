ac9d1d398817fc0da81076dc3e76a0b9a8ef4bb1
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-09-10 17:42:43 +0200

    feat: Added new dto for building resource production

---
+++ pkg/communication/building_resource_production_dto_test.go
@@ -0,0 +1,49 @@
+package communication
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/stretchr/testify/assert"
+)
+
+var defaultBuildingResourceProduction = persistence.BuildingResourceProduction{
+	Building: defaultBuildingId,
+	Resource: defaultResourceId,
+	Base:     54,
+	Progress: 1.3,
+}
+var defaultBuildingResourceProductionDtoResponse = BuildingResourceProductionDtoResponse{
+	Building: defaultBuildingId,
+	Resource: defaultResourceId,
+	Base:     54,
+	Progress: 1.3,
+}
+
+func TestToBuildingResourceProductionDtoResponse(t *testing.T) {
+	assert := assert.New(t)
+
+	actual := ToBuildingResourceProductionDtoResponse(defaultBuildingResourceProduction)
+
+	assert.Equal(defaultBuildingId, actual.Building)
+	assert.Equal(defaultResourceId, actual.Resource)
+	assert.Equal(54, actual.Base)
+	assert.Equal(1.3, actual.Progress)
+}
+
+func TestBuildingResourceProductionDtoResponse_MarshalsToCamelCase(t *testing.T) {
+	assert := assert.New(t)
+
+	out, err := json.Marshal(defaultBuildingResourceProductionDtoResponse)
+
+	assert.Nil(err)
+	expectedJson := `
+	{
+		"building": "461ba465-86e6-4234-94b8-fc8fab03fa74",
+		"resource": "97ddca58-8eee-41af-8bda-f37a3080f618",
+		"base": 54,
+		"progress": 1.3
+	}`
+	assert.JSONEq(expectedJson, string(out))
+}
