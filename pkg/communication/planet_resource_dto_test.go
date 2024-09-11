284c5d85529029e17e777e9fb665d5d0522a2007
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-09-11 21:54:33 +0200

    feat: Added production to planet resource dto

--- pkg/communication/planet_resource_dto_test.go
+++ pkg/communication/planet_resource_dto_test.go
@@ -11,19 +11,21 @@ import (
 
 var someOtherTime = time.Date(2024, 07, 28, 10, 30, 02, 651387236, time.UTC)
 var defaultPlanetResource = persistence.PlanetResource{
-	Planet:   defaultPlanetId,
-	Resource: defaultResourceId,
-	Amount:   1234.567,
+	Planet:     defaultPlanetId,
+	Resource:   defaultResourceId,
+	Amount:     1234.567,
+	Production: 57,
 
 	CreatedAt: someTime,
 	UpdatedAt: someOtherTime,
 }
 var defaultPlanetResourceDtoResponse = PlanetResourceDtoResponse{
-	Planet:    defaultPlanetId,
-	Resource:  defaultResourceId,
-	Amount:    1234.567,
-	CreatedAt: someTime,
-	UpdatedAt: someOtherTime,
+	Planet:     defaultPlanetId,
+	Resource:   defaultResourceId,
+	Amount:     1234.567,
+	Production: 57,
+	CreatedAt:  someTime,
+	UpdatedAt:  someOtherTime,
 }
 
 func TestToPlanetResourceDtoResponse(t *testing.T) {
@@ -34,6 +36,7 @@ func TestToPlanetResourceDtoResponse(t *testing.T) {
 	assert.Equal(defaultPlanetId, actual.Planet)
 	assert.Equal(defaultResourceId, actual.Resource)
 	assert.Equal(1234.567, actual.Amount)
+	assert.Equal(57, actual.Production)
 	assert.Equal(someTime, actual.CreatedAt)
 	assert.Equal(someOtherTime, actual.UpdatedAt)
 }
@@ -49,6 +52,7 @@ func TestPlanetResourceDtoResponse_MarshalsToCamelCase(t *testing.T) {
 		"planet": "65801b9b-84e6-411d-805f-2eb89587c5a7",
 		"resource": "97ddca58-8eee-41af-8bda-f37a3080f618",
 		"amount": 1234.567,
+		"production": 57,
 		"createdAt": "2024-05-05T20:50:18.651387237Z",
 		"updatedAt": "2024-07-28T10:30:02.651387236Z"
 	}`
