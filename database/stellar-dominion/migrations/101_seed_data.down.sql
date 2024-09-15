e7500b0b9a334aa1f3936b379548d6f2d99c4339
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-09-15 11:27:39 +0200

    feat: Improved structure for planet resource production by extracting a separate table

--- database/stellar-dominion/migrations/101_seed_data.down.sql
+++ database/stellar-dominion/migrations/101_seed_data.down.sql
@@ -3,6 +3,7 @@ DELETE FROM building_action_resource_production;
 DELETE FROM building_action_cost;
 DELETE FROM building_action;
 DELETE FROM planet_building;
+DELETE FROM planet_resource_production;
 DELETE FROM planet_resource;
 DELETE FROM homeworld;
 DELETE FROM planet;
--- database/stellar-dominion/migrations/6_create_planets.up.sql
+++ database/stellar-dominion/migrations/6_create_planets.up.sql
@@ -27,7 +27,6 @@ CREATE TABLE planet_resource (
   planet uuid NOT NULL,
   resource uuid NOT NULL,
   amount numeric(15, 5) NOT NULL,
-  production INTEGER NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   version INTEGER DEFAULT 0,
@@ -37,6 +36,30 @@ CREATE TABLE planet_resource (
 );
 
 CREATE INDEX planet_resource_planet_index ON planet_resource (planet);
+
+CREATE TABLE planet_resource_production (
+  planet uuid NOT NULL,
+  building uuid,
+  resource uuid NOT NULL,
+  production INTEGER NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  version INTEGER DEFAULT 0,
+  FOREIGN KEY (planet) REFERENCES planet(id),
+  FOREIGN KEY (building) REFERENCES building(id),
+  FOREIGN KEY (resource) REFERENCES resource(id),
+  -- TODO: Migrating to psql 15 would allow NULLS NOT DISTINCT to make sure
+  -- that we don't allow multiple productions from NULL buildings.
+  -- See: https://stackoverflow.com/questions/8289100/create-unique-constraint-with-null-columns
+  UNIQUE (planet, building)
+);
+
+CREATE INDEX planet_resource_production_planet_index ON planet_resource_production (planet);
+
+CREATE TRIGGER trigger_planet_resource_production_updated_at
+  BEFORE UPDATE OR INSERT ON planet_resource_production
+  FOR EACH ROW
+  EXECUTE FUNCTION update_updated_at();
 
 CREATE TABLE planet_building (
   planet uuid NOT NULL,
