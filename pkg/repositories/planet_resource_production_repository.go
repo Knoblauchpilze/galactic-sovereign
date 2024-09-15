645d9a595872b39682b10f7528fb08d257f6b04a
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-09-15 14:59:08 +0200

    feat: Added repository to access planet resource production

---
+++ pkg/repositories/planet_resource_production_repository.go
@@ -0,0 +1,110 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/google/uuid"
+)
+
+type PlanetResourceProductionRepository interface {
+	Create(ctx context.Context, tx db.Transaction, production persistence.PlanetResourceProduction) (persistence.PlanetResourceProduction, error)
+	ListForPlanet(ctx context.Context, tx db.Transaction, planet uuid.UUID) ([]persistence.PlanetResourceProduction, error)
+	Update(ctx context.Context, tx db.Transaction, production persistence.PlanetResourceProduction) (persistence.PlanetResourceProduction, error)
+	DeleteForPlanet(ctx context.Context, tx db.Transaction, planet uuid.UUID) error
+}
+
+type planetResourceProductionRepositoryImpl struct{}
+
+func NewPlanetResourceProductionRepository() PlanetResourceProductionRepository {
+	return &planetResourceProductionRepositoryImpl{}
+}
+
+const createPlanetResourceProductionSqlTemplate = "INSERT INTO planet_resource_production (planet, building, resource, production, created_at) VALUES($1, $2, $3, $4, $5)"
+
+func (r *planetResourceProductionRepositoryImpl) Create(ctx context.Context, tx db.Transaction, production persistence.PlanetResourceProduction) (persistence.PlanetResourceProduction, error) {
+	_, err := tx.Exec(ctx, createPlanetResourceProductionSqlTemplate, production.Planet, production.Building, production.Resource, production.Production, production.CreatedAt)
+	return production, err
+}
+
+const listPlanetResourceProductionForPlanetSqlTemplate = `
+SELECT
+	planet,
+	building,
+	resource,
+	production,
+	created_at,
+	updated_at,
+	version
+FROM
+	planet_resource_production
+WHERE
+	planet = $1
+`
+
+func (r *planetResourceProductionRepositoryImpl) ListForPlanet(ctx context.Context, tx db.Transaction, planet uuid.UUID) ([]persistence.PlanetResourceProduction, error) {
+	res := tx.Query(ctx, listPlanetResourceProductionForPlanetSqlTemplate, planet)
+	if err := res.Err(); err != nil {
+		return []persistence.PlanetResourceProduction{}, err
+	}
+
+	var out []persistence.PlanetResourceProduction
+	parser := func(rows db.Scannable) error {
+		var production persistence.PlanetResourceProduction
+		err := rows.Scan(&production.Planet, &production.Building, &production.Resource, &production.Production, &production.CreatedAt, &production.UpdatedAt, &production.Version)
+		if err != nil {
+			return err
+		}
+
+		out = append(out, production)
+		return nil
+	}
+
+	if err := res.GetAll(parser); err != nil {
+		return []persistence.PlanetResourceProduction{}, err
+	}
+
+	return out, nil
+}
+
+const updatePlanetResourceProductionSqlTemplate = `
+UPDATE
+	planet_resource_production
+SET
+	production = $1,
+	updated_at = $2,
+	version = $3
+WHERE
+	planet = $4
+	AND building = $5
+	AND resource = $6
+	AND version = $7
+`
+
+func (r *planetResourceProductionRepositoryImpl) Update(ctx context.Context, tx db.Transaction, production persistence.PlanetResourceProduction) (persistence.PlanetResourceProduction, error) {
+	version := production.Version + 1
+	affected, err := tx.Exec(ctx, updatePlanetResourceProductionSqlTemplate,
+		production.Production, production.UpdatedAt, version,
+		production.Planet, production.Building, production.Resource, production.Version)
+	if err != nil {
+		return production, err
+	}
+	if affected == 0 {
+		return production, errors.NewCode(db.OptimisticLockException)
+	} else if affected != 1 {
+		return production, errors.NewCode(db.MoreThanOneMatchingSqlRows)
+	}
+
+	production.Version = version
+
+	return production, nil
+}
+
+const deletePlanetResourceProductionSqlTemplate = "DELETE FROM planet_resource_production WHERE planet = $1"
+
+func (r *planetResourceProductionRepositoryImpl) DeleteForPlanet(ctx context.Context, tx db.Transaction, planet uuid.UUID) error {
+	_, err := tx.Exec(ctx, deletePlanetResourceProductionSqlTemplate, planet)
+	return err
+}
---
+++ pkg/repositories/planet_resource_production_repository_test.go
@@ -0,0 +1,227 @@
+package repositories
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/KnoblauchPilze/user-service/pkg/db"
+	"github.com/KnoblauchPilze/user-service/pkg/errors"
+	"github.com/KnoblauchPilze/user-service/pkg/persistence"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+var defaultPlanetResourceProduction = persistence.PlanetResourceProduction{
+	Planet:     defaultPlanetId,
+	Building:   &defaultBuildingId,
+	Resource:   defaultResourceId,
+	Production: 89,
+	CreatedAt:  time.Date(2024, 9, 15, 11, 57, 02, 651387250, time.UTC),
+	UpdatedAt:  time.Date(2024, 9, 15, 11, 57, 03, 651387250, time.UTC),
+	Version:    5,
+}
+var defaultUpdatedPlanetResourceProduction = persistence.PlanetResourceProduction{
+	Planet:     defaultPlanetId,
+	Building:   &defaultBuildingId,
+	Resource:   defaultResourceId,
+	Production: 321,
+	CreatedAt:  time.Date(2024, 9, 15, 11, 57, 02, 651387250, time.UTC),
+	UpdatedAt:  time.Date(2024, 9, 15, 11, 58, 53, 651387250, time.UTC),
+	Version:    6,
+}
+
+func Test_PlanetResourceProductionRepository_Transaction(t *testing.T) {
+	var dummyUuid *uuid.UUID
+	var dummyInt int
+
+	s := RepositoryTransactionTestSuite{
+		dbInteractionTestCases: map[string]dbTransactionInteractionTestCase{
+			"create": {
+				sqlMode: ExecBased,
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetResourceProductionRepository()
+					_, err := s.Create(ctx, tx, defaultPlanetResourceProduction)
+					return err
+				},
+				expectedSqlQueries: []string{
+					`INSERT INTO planet_resource_production (planet, building, resource, production, created_at) VALUES($1, $2, $3, $4, $5)`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultPlanetResourceProduction.Planet,
+						defaultPlanetResourceProduction.Building,
+						defaultPlanetResourceProduction.Resource,
+						defaultPlanetResourceProduction.Production,
+						defaultPlanetResourceProduction.CreatedAt,
+					},
+				},
+			},
+			"listForPlanet": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetResourceProductionRepository()
+					_, err := s.ListForPlanet(ctx, tx, defaultPlanetId)
+					return err
+				},
+				expectedSqlQueries: []string{
+					`
+SELECT
+	planet,
+	building,
+	resource,
+	production,
+	created_at,
+	updated_at,
+	version
+FROM
+	planet_resource_production
+WHERE
+	planet = $1
+`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultPlanetId,
+					},
+				},
+			},
+			"update": {
+				sqlMode: ExecBased,
+				generateMock: func() db.Transaction {
+					return &mockTransaction{
+						affectedRows: []int{1},
+					}
+				},
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetResourceProductionRepository()
+					_, err := s.Update(ctx, tx, defaultUpdatedPlanetResourceProduction)
+					return err
+				},
+				expectedSqlQueries: []string{
+					`
+UPDATE
+	planet_resource_production
+SET
+	production = $1,
+	updated_at = $2,
+	version = $3
+WHERE
+	planet = $4
+	AND building = $5
+	AND resource = $6
+	AND version = $7
+`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultUpdatedPlanetResourceProduction.Production,
+						defaultUpdatedPlanetResourceProduction.UpdatedAt,
+						defaultUpdatedPlanetResourceProduction.Version + 1,
+						defaultUpdatedPlanetResourceProduction.Planet,
+						defaultUpdatedPlanetResourceProduction.Building,
+						defaultUpdatedPlanetResourceProduction.Resource,
+						defaultUpdatedPlanetResourceProduction.Version,
+					},
+				},
+			},
+			"deleteForPlanet": {
+				sqlMode: ExecBased,
+				generateMock: func() db.Transaction {
+					return &mockTransaction{
+						affectedRows: []int{1},
+					}
+				},
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetResourceProductionRepository()
+					return s.DeleteForPlanet(ctx, tx, defaultPlanetId)
+				},
+				expectedSqlQueries: []string{
+					`DELETE FROM planet_resource_production WHERE planet = $1`,
+				},
+				expectedArguments: [][]interface{}{
+					{
+						defaultPlanetId,
+					},
+				},
+			},
+		},
+
+		dbGetAllTestCases: map[string]dbTransactionGetAllTestCase{
+			"listForPlanet": {
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					repo := NewPlanetResourceProductionRepository()
+					_, err := repo.ListForPlanet(ctx, tx, defaultPlanetId)
+					return err
+				},
+				expectedGetAllCalls: 1,
+				expectedScanCalls:   1,
+				expectedScannedProps: [][]interface{}{
+					{
+						&uuid.UUID{},
+						&dummyUuid,
+						&uuid.UUID{},
+						&dummyInt,
+						&time.Time{},
+						&time.Time{},
+						&dummyInt,
+					},
+				},
+			},
+		},
+
+		dbReturnTestCases: map[string]dbTransactionReturnTestCase{
+			"create": {
+				handler: func(ctx context.Context, tx db.Transaction) interface{} {
+					s := NewPlanetResourceProductionRepository()
+					out, _ := s.Create(ctx, tx, defaultPlanetResourceProduction)
+					return out
+				},
+				expectedContent: defaultPlanetResourceProduction,
+			},
+			"update": {
+				handler: func(ctx context.Context, tx db.Transaction) interface{} {
+					s := NewPlanetResourceProductionRepository()
+					out, _ := s.Update(ctx, tx, defaultUpdatedPlanetResourceProduction)
+					return out
+				},
+				expectedContent: defaultUpdatedPlanetResourceProduction,
+			},
+		},
+
+		dbErrorTestCases: map[string]dbTransactionErrorTestCase{
+			"update_optimisticLockException": {
+				generateMock: func() db.Transaction {
+					return &mockTransaction{
+						affectedRows: []int{0},
+					}
+				},
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetResourceProductionRepository()
+					_, err := s.Update(ctx, tx, defaultUpdatedPlanetResourceProduction)
+					return err
+				},
+				verifyError: func(err error, assert *require.Assertions) {
+					assert.True(errors.IsErrorWithCode(err, db.OptimisticLockException))
+				},
+			},
+			"update_moreThanOneRowAffected": {
+				generateMock: func() db.Transaction {
+					return &mockTransaction{
+						affectedRows: []int{2},
+					}
+				},
+				handler: func(ctx context.Context, tx db.Transaction) error {
+					s := NewPlanetResourceProductionRepository()
+					_, err := s.Update(ctx, tx, defaultUpdatedPlanetResourceProduction)
+					return err
+				},
+				verifyError: func(err error, assert *require.Assertions) {
+					assert.True(errors.IsErrorWithCode(err, db.MoreThanOneMatchingSqlRows))
+				},
+			},
+		},
+	}
+
+	suite.Run(t, &s)
+}
