1e8e01c878decf0b4b8d95d2274a084624f39c02
KnoblauchPilze <prolific-programmer@tuta.com>
2024-09-29 20:33:00 +0200

    feat: Added images to represent buildings

--- website/stellar-dominion/src/lib/components/Building.svelte
+++ website/stellar-dominion/src/lib/components/Building.svelte
@@ -3,6 +3,15 @@
 	import { type UiResource } from '$lib/game/resources';
 	import { StyledActionButton, StyledText } from '$lib/components';
 
+	// https://kit.svelte.dev/docs/images#sveltejs-enhanced-img-dynamically-choosing-an-image
+	// https://github.com/vitejs/vite/issues/9599#issuecomment-1209333753
+	const modules = import.meta.glob<Record<string, string>>('$lib/assets/buildings/*.webp', {
+		eager: true,
+		query: {
+			enhanced: true
+		}
+	});
+
 	export let building: UiBuilding;
 	export let availableResources: UiResource[];
 	export let buildingActionAlreadyRunning: boolean;
@@ -21,7 +30,19 @@
 	}
 
 	// https://stackoverflow.com/questions/49296458/capitalize-first-letter-of-a-string-using-angular-or-typescript
-	const title = building.name[0].toUpperCase() + building.name.slice(1);
+	$: title = building.name[0].toUpperCase() + building.name.slice(1);
+
+	// https://stackoverflow.com/questions/68060723/glob-import-of-image-urls-in-sveltekit
+	// https://stackoverflow.com/questions/423376/how-to-get-the-file-name-from-a-full-path-using-javascript
+	$: images = Object.keys(modules).map((imagePath) => {
+		return {
+			building: imagePath
+				.replace(/^.*[\\/]/, '')
+				.replace(/\..*$/, '')
+				.replace(/\_/, ' '),
+			data: modules[imagePath].default
+		};
+	});
 
 	$: costs = building.costs.map((c) => ({
 		resource: c.resource,
@@ -36,6 +57,8 @@
 	}));
 	$: needsGainsSection = gains.length > 0;
 
+	$: buildingImage = images.find((image) => image.building === building.name);
+
 	const isAffordable = building.costs.reduce(
 		(currentlyAffordable, cost) => currentlyAffordable && canAfford(cost, availableResources),
 		true
@@ -44,6 +67,9 @@
 
 <div class="p-4 m-2 bg-overlay">
 	<StyledText text="{title} (level {building.level})" styling="font-bold" />
+	{#if buildingImage !== undefined}
+		<enhanced:img src={buildingImage.data} alt="Building visual" width="100" height="100" />
+	{/if}
 	<StyledText text="Required for level {building.level + 1}:" textColor="text-white" />
 	<table>
 		{#each costs as cost}
