072bebb8781101ed95f0080e22376da9046be76d
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-10-04 16:07:01 +0200

    fix: Missing css property in header

--- website/stellar-dominion/src/lib/components/Header.svelte
+++ website/stellar-dominion/src/lib/components/Header.svelte
@@ -2,6 +2,8 @@
 	export let height: string = 'h-1/10';
 </script>
 
-<div class="fixed top-0 w-full {height} flex justify-around items-center bg-black text-white">
+<div
+	class="fixed top-0 left-0 w-full {height} flex justify-around items-center bg-black text-white"
+>
 	<slot />
 </div>
