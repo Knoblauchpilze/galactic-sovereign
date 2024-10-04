37e116d9e9816724514b0020ba9d7588159004ee
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-10-04 16:08:11 +0200

    feat: First draft of multi-page website with overview and buildings page

--- website/stellar-dominion/src/routes/planets/[planet=id]/+layout.svelte
+++ website/stellar-dominion/src/routes/planets/[planet=id]/+layout.svelte
@@ -3,16 +3,26 @@
 	import { CenteredWrapper, Footer, StyledLink } from '$lib/components';
 </script>
 
-<!-- https://stackoverflow.com/questions/70805041/background-image-in-tailwindcss-using-dynamic-url-react-js -->
-<CenteredWrapper>
-	<slot />
-	<Footer>
-		<p>Meet the <StyledLink text="author" link="https://github.com/Knoblauchpilze" /> on github!</p>
-		<p>
-			Found an issue? Report it <StyledLink
-				text="here"
-				link="https://github.com/Knoblauchpilze/user-service/issues"
-			/>
-		</p>
-	</Footer>
-</CenteredWrapper>
+<div class="w-[90%] h-[90%] flex justify-center items-stretch bg-transparent">
+	<div class=" flex items-center">
+		<ul class="list-none p-0 m-0">
+			<li><a href="overview">Overview</a></li>
+			<li><a href="buildings">Buildings</a></li>
+		</ul>
+	</div>
+
+	<CenteredWrapper>
+		<slot />
+		<Footer>
+			<p>
+				Meet the <StyledLink text="author" link="https://github.com/Knoblauchpilze" /> on github!
+			</p>
+			<p>
+				Found an issue? Report it <StyledLink
+					text="here"
+					link="https://github.com/Knoblauchpilze/user-service/issues"
+				/>
+			</p>
+		</Footer>
+	</CenteredWrapper>
+</div>
--- website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
+++ website/stellar-dominion/src/routes/planets/[planet=id]/overview/+page.svelte
@@ -1,36 +1,23 @@
 <script lang="ts">
 	import '$styles/app.css';
-	import {
-		CenteredWrapper,
-		Header,
-		StyledText,
-		StyledTitle,
-		Building,
-		BuildingAction
-	} from '$lib/components';
 
 	import heroImage, { GAME_HERO_IMAGE } from '$lib/stores/ui/heroImage';
 	import heroContainer, { GAME_HERO_CONTAINER_PROPS } from '$lib/stores/ui/heroContainer';
 	import pageTitle, { HOMEPAGE_TITLE } from '$lib/stores/ui/pageTitle';
 
-	import { type UiResource, mapPlanetResourcesToUiResources } from '$lib/game/resources';
-	import { mapPlanetBuildingsToUiBuildings } from '$lib/game/buildings';
-	import { mapBuildingActionsToUiActions } from '$lib/game/actions.js';
-	import { floorToInteger, toFlooredShortString } from '$lib/displayUtils';
-	import { invalidate } from '$app/navigation';
+	import { CenteredWrapper, GamePageWrapper } from '$lib/components';
 
-	// https://svelte.dev/blog/zero-config-type-safety
+	import { mapPlanetResourcesToUiResources } from '$lib/game/resources';
+
 	export let data;
 
-	// https://stackoverflow.com/questions/77047087/why-is-page-svelte-not-reloading-after-page-server-ts-executes-load-function
-	const planetName = data.planet.name;
-	const universeName = data.universe.name;
-
-	// https://stackoverflow.com/questions/75616911/sveltekit-fetching-on-the-server-and-updating-the-writable-store
 	heroImage.set(GAME_HERO_IMAGE);
 	heroContainer.set(GAME_HERO_CONTAINER_PROPS);
-	const title = HOMEPAGE_TITLE + ' - ' + data.planet.name;
-	pageTitle.set(title);
+	$: title = HOMEPAGE_TITLE + ' - ' + data.planet.name;
+	$: pageTitle.set(title);
+
+	$: planetName = data.planet.name;
+	$: universeName = data.universe.name;
 
 	$: resources = mapPlanetResourcesToUiResources(
 		data.planet.resources,
@@ -38,88 +25,8 @@
 		data.planet.storages,
 		data.resources
 	);
-	$: buildings = mapPlanetBuildingsToUiBuildings(
-		data.planet.id,
-		data.planet.buildings,
-		data.planet.buildingActions,
-		data.buildings,
-		data.resources
-	);
-	$: actions = mapBuildingActionsToUiActions(data.planet.buildingActions, data.buildings);
-
-	$: anyBuildingActionRunning = data.planet.buildingActions.length !== 0;
-
-	function onActionCompleted() {
-		invalidate('data:planet');
-	}
-
-	function resourceTextColor(resource: UiResource): string {
-		if (resource.amount < resource.storage) {
-			return 'text-white';
-		}
-		return 'text-disabled';
-	}
-	function productionTextColor(resource: UiResource): string {
-		if (resource.amount < resource.storage) {
-			return 'text-enabled';
-		}
-		return 'text-disabled';
-	}
 </script>
 
-<CenteredWrapper width="w-4/5" height="h-4/5" bgColor="bg-overlay">
-	<Header>
-		<StyledText text={universeName} textColor="text-white" />
-		<StyledText text={planetName} textColor="text-white" />
-		<form method="POST" action="?/logout">
-			<button class="hover:underline">Logout</button>
-		</form>
-	</Header>
-
-	<div class="flex flex-col justify-start flex-grow w-full">
-		<div class="flex justify-around bg-black justify-items-stretch">
-			{#each resources as resource}
-				<div class="flex space-between">
-					<StyledText text="{resource.name}:" textColor="text-white" />
-					<StyledText
-						text={floorToInteger(resource.amount).toString()}
-						textColor={resourceTextColor(resource)}
-						styling="px-1"
-					/>
-					<StyledText
-						text="(+{floorToInteger(resource.production)}/h)"
-						textColor={productionTextColor(resource)}
-						styling="pr-1"
-					/>
-					<StyledText
-						text="(storage: {toFlooredShortString(resource.storage)})"
-						textColor="text-white"
-					/>
-				</div>
-			{/each}
-		</div>
-
-		<CenteredWrapper>
-			<StyledTitle text="Buildings on {planetName}" />
-			<!-- https://tailwindcss.com/docs/align-items -->
-			<div class="w-full h-full flex flex-wrap items-start bg-transparent">
-				{#each buildings as building}
-					<Building
-						{building}
-						availableResources={resources}
-						buildingActionAlreadyRunning={anyBuildingActionRunning}
-					/>
-				{/each}
-			</div>
-		</CenteredWrapper>
-
-		<CenteredWrapper>
-			<StyledTitle text="Actions running on {planetName}" />
-			<div class="w-full h-full flex flex-wrap items-start bg-transparent">
-				{#each actions as action}
-					<BuildingAction {action} onCompleted={onActionCompleted} />
-				{/each}
-			</div>
-		</CenteredWrapper>
-	</div>
-</CenteredWrapper>
+<GamePageWrapper {universeName} {planetName} {resources}>
+	<CenteredWrapper><p class="text-white">This is being built...</p></CenteredWrapper>
+</GamePageWrapper>
