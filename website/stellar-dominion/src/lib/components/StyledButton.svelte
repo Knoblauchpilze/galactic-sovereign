d14d48a7c1a4ba5733479b1a1f6367b6aab5da6d
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-10-05 16:26:50 +0200

    feat: Used StyledButton to represent a link when appropriate

--- website/stellar-dominion/src/lib/components/StyledButton.svelte
+++ website/stellar-dominion/src/lib/components/StyledButton.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	export let text: string = 'Click me';
+	export let enabled: boolean = true;
 </script>
 
-<button class="px-12 py-4 rounded-[8px] bg-primary hover:bg-primary-hover text-secondary"
-	>{text}</button
->
+{#if enabled}
+	<button class="px-12 py-4 rounded-[8px] bg-primary hover:bg-primary-hover text-secondary"
+		>{text}</button
+	>
+{:else}
+	<!-- https://stackoverflow.com/questions/34684467/dont-redirect-on-button-click-with-css -->
+	<button disabled class="px-12 py-4 rounded-[8px] bg-primary-selected text-secondary"
+		>{text}</button
+	>
+{/if}
