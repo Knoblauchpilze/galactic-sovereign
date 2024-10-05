3f0d3e2e5536668c2ee7719e7f2c26f09d4be0bf
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-10-05 16:26:50 +0200

    feat: Used StyledButton to represent a link when appropriate

--- website/stellar-dominion/src/lib/components/StyledLink.svelte
+++ website/stellar-dominion/src/lib/components/StyledLink.svelte
@@ -1,14 +1,25 @@
 <!-- https://svelte.dev/docs/svelte-components -->
 <script lang="ts">
-	export let text: string = "Home";
-	export let link: string = "/";
+	import { StyledButton } from '$lib/components';
+
+	export let text: string = 'Home';
+	export let link: string = '/';
 	export let showAsButton: boolean = false;
+	export let disabled: boolean = false;
 </script>
 
 <!-- https://tailwindcss.com/docs/padding -->
 <!-- https://tailwindcss.com/docs/hover-focus-and-other-states -->
 {#if showAsButton}
-	<a class="px-12 py-4 rounded-[8px] bg-primary hover:bg-primary-hover text-secondary" href={link}>{text}</a>
+	{#if disabled}
+		<a href={link}>
+			<StyledButton {text} enabled={false} />
+		</a>
+	{:else}
+		<a href={link}>
+			<StyledButton {text} />
+		</a>
+	{/if}
 {:else}
 	<a class="text-secondary hover:text-secondary-hover" href={link}>{text}</a>
 {/if}
