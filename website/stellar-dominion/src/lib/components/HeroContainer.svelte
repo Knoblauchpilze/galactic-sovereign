60231031b26576b6f92b57abc59b6b290be793d8
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2024-10-06 22:15:41 +0200

    feat: HeroContainer uses a FlexContainer

--- website/stellar-dominion/src/lib/components/HeroContainer.svelte
+++ website/stellar-dominion/src/lib/components/HeroContainer.svelte
@@ -2,13 +2,21 @@
 	// https://stackoverflow.com/questions/70927735/pass-variable-up-from-page-to-svelte-layout-via-slot
 	import heroImage from '$lib/stores/ui/heroImage';
 	import heroContainer from '$lib/stores/ui/heroContainer';
+
+	import { FlexContainer } from '$lib/components';
+
+	$: style =
+		'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ' +
+		$heroContainer.width +
+		' ' +
+		$heroContainer.height;
 </script>
 
 <!-- https://stackoverflow.com/questions/70805041/background-image-in-tailwindcss-using-dynamic-url-react-js -->
-<div  class="relative h-full bg-center bg-no-repeat bg-cover {$heroImage}">
-	<div class="flex flex-col items-center justify-around absolute left-1/2 top-1/2 {$heroContainer.width} {$heroContainer.height} -translate-x-1/2 -translate-y-1/2 {$heroContainer.color}">
-		<slot/>
-	</div>
+<div class="relative h-full bg-center bg-no-repeat bg-cover {$heroImage}">
+	<FlexContainer bgColor={$heroContainer.color} styling={style}>
+		<slot />
+	</FlexContainer>
 </div>
 
 <style>
