ceaf2e52129fa50962c091c9b89dd65823bd991f
KnoblauchPilze <prolific-programmer@tuta.com>
2024-10-07 18:06:28 +0200

    feat: Better naming for properties of FlexContainer

--- website/stellar-dominion/src/lib/components/FlexContainer.svelte
+++ website/stellar-dominion/src/lib/components/FlexContainer.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	export let horizontal: boolean = false;
-	export let center: boolean = false;
-	export let stretch: boolean = false;
+	export let justify: string = 'around';
+	export let align: string = 'center';
 	export let extensible: boolean = true;
 	export let bgColor: string = 'bg-transparent';
 	export let styling: string = '';
 
 	$: direction = horizontal ? 'flex-row' : 'flex-col';
-	$: justify = center ? 'justify-center' : 'justify-around';
-	$: items = stretch ? 'items-stretch' : 'items-center';
+	$: justification = 'justify-' + justify;
+	$: alignment = 'items-' + align;
 	$: grow = extensible ? 'flex-1' : 'flex-none';
 </script>
 
 <!-- https://stackoverflow.com/questions/29467660/how-to-stretch-children-to-fill-cross-axis -->
-<div class="flex {direction} {justify} {items} {bgColor} {grow} {styling}">
+<div class="flex {direction} {justification} {alignment} {bgColor} {grow} {styling}">
 	<slot />
 </div>
