f5a1e0275305d3d177f44082fba0e33064a1e38e
KnoblauchPilze <prolific-programmer@tuta.com>
2024-10-07 17:29:31 +0200

    feat: CenteredWrapper uses the FlexContainer also for the wrapper

--- website/stellar-dominion/src/lib/components/FlexContainer.svelte
+++ website/stellar-dominion/src/lib/components/FlexContainer.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	export let horizontal: boolean = false;
 	export let center: boolean = false;
+	export let stretch: boolean = false;
 	export let extensible: boolean = true;
 	export let bgColor: string = 'bg-transparent';
 	export let styling: string = '';
 
 	$: direction = horizontal ? 'flex-row' : 'flex-col';
-	$: space = center ? 'justify-center' : 'justify-around';
+	$: justify = center ? 'justify-center' : 'justify-around';
+	$: items = stretch ? 'items-stretch' : 'items-center';
 	$: grow = extensible ? 'flex-1' : 'flex-none';
 </script>
 
 <!-- https://stackoverflow.com/questions/29467660/how-to-stretch-children-to-fill-cross-axis -->
-<div class="flex {direction} {space} items-center {bgColor} {grow} {styling}">
+<div class="flex {direction} {justify} {items} {bgColor} {grow} {styling}">
 	<slot />
 </div>
