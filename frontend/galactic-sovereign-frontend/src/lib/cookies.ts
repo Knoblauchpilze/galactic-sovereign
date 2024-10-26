e763d87caa53d738d56f1ec0bdff513e78e736d7
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-10-26 10:16:45 +0200

    feat: Refactored login workflow and added lobby

--- frontend/galactic-sovereign-frontend/src/lib/cookies.ts
+++ frontend/galactic-sovereign-frontend/src/lib/cookies.ts
@@ -20,51 +20,85 @@ export {
 	COOKIE_KEY_UNIVERSE_ID
 };
 
-export function resetCookies(cookies: Cookies) {
-	cookies.set(COOKIE_KEY_API_USER, '', DEFAULT_COOKIES_OPT);
-	cookies.set(COOKIE_KEY_API_KEY, '', DEFAULT_COOKIES_OPT);
-	cookies.set(COOKIE_KEY_PLAYER_ID, '', DEFAULT_COOKIES_OPT);
-	cookies.set(COOKIE_KEY_PLAYER_NAME, '', DEFAULT_COOKIES_OPT);
-	cookies.set(COOKIE_KEY_UNIVERSE_ID, '', DEFAULT_COOKIES_OPT);
-}
-
-export function setCookies(cookies: Cookies, apiKey: ApiKey, player: Player) {
+export function setSessionCookies(cookies: Cookies, apiKey: ApiKey) {
 	cookies.set(COOKIE_KEY_API_USER, apiKey.user, DEFAULT_COOKIES_OPT);
 	cookies.set(COOKIE_KEY_API_KEY, apiKey.key, DEFAULT_COOKIES_OPT);
+}
+
+export function resetSessionCookies(cookies: Cookies) {
+	cookies.set(COOKIE_KEY_API_USER, '', DEFAULT_COOKIES_OPT);
+	cookies.set(COOKIE_KEY_API_KEY, '', DEFAULT_COOKIES_OPT);
+}
+
+export function setGameCookies(cookies: Cookies, player: Player) {
 	cookies.set(COOKIE_KEY_PLAYER_ID, player.id, DEFAULT_COOKIES_OPT);
 	cookies.set(COOKIE_KEY_PLAYER_NAME, player.name, DEFAULT_COOKIES_OPT);
 	cookies.set(COOKIE_KEY_UNIVERSE_ID, player.universe, DEFAULT_COOKIES_OPT);
 }
 
-export interface GameCookies {
-	readonly apiUser: string;
-	readonly apiKey: string;
-	readonly playerId: string;
-	readonly playerName: string;
-	readonly universeId: string;
+export function resetGameCookies(cookies: Cookies) {
+	cookies.set(COOKIE_KEY_PLAYER_ID, '', DEFAULT_COOKIES_OPT);
+	cookies.set(COOKIE_KEY_PLAYER_NAME, '', DEFAULT_COOKIES_OPT);
+	cookies.set(COOKIE_KEY_UNIVERSE_ID, '', DEFAULT_COOKIES_OPT);
+}
+
+// TODO: To be removed.
+export function setCookies(cookies: Cookies, apiKey: ApiKey, player: Player) {
+	setSessionCookies(cookies, apiKey);
+	setGameCookies(cookies, player);
+}
+
+export function resetCookies(cookies: Cookies) {
+	resetSessionCookies(cookies);
+	resetGameCookies(cookies);
 }
 
 function validOrEmptyString(maybeValue: string | undefined, valid: boolean): string {
 	return valid ? (maybeValue as string) : '';
 }
 
-export function loadCookies(cookies: Cookies): [boolean, GameCookies] {
+export interface SessionCookies {
+	readonly apiUser: string;
+	readonly apiKey: string;
+}
+
+export function loadSessionCookies(cookies: Cookies): [boolean, SessionCookies] {
 	const maybeApiUser = cookies.get(COOKIE_KEY_API_USER);
 	const maybeApiKey = cookies.get(COOKIE_KEY_API_KEY);
+
+	const validApiUser = maybeApiUser !== undefined;
+	const validApiKey = maybeApiKey !== undefined;
+	const valid = validApiUser || validApiKey;
+
+	const out: SessionCookies = {
+		apiUser: validOrEmptyString(maybeApiUser, validApiUser),
+		apiKey: validOrEmptyString(maybeApiKey, validApiKey)
+	};
+
+	return [valid, out];
+}
+
+export interface GameCookies {
+	readonly session: SessionCookies;
+	readonly playerId: string;
+	readonly playerName: string;
+	readonly universeId: string;
+}
+
+export function loadCookies(cookies: Cookies): [boolean, GameCookies] {
 	const maybePlayerId = cookies.get(COOKIE_KEY_PLAYER_ID);
 	const maybePlayerName = cookies.get(COOKIE_KEY_PLAYER_NAME);
 	const maybeUniverseId = cookies.get(COOKIE_KEY_UNIVERSE_ID);
 
-	const validApiUser = maybeApiUser !== undefined;
-	const validApiKey = maybeApiKey !== undefined;
+	const [validSession, session] = loadSessionCookies(cookies);
+
 	const validPlayerId = maybePlayerId !== undefined;
 	const validPlayerName = maybePlayerName !== undefined;
 	const validUniverseId = maybeUniverseId !== undefined;
-	const valid = validApiUser || validApiKey || validPlayerId || validUniverseId;
+	const valid = validSession || validPlayerId || validUniverseId;
 
 	const out: GameCookies = {
-		apiUser: validOrEmptyString(maybeApiUser, validApiUser),
-		apiKey: validOrEmptyString(maybeApiKey, validApiKey),
+		session: session,
 		playerId: validOrEmptyString(maybePlayerId, validPlayerId),
 		playerName: validOrEmptyString(maybePlayerName, validPlayerName),
 		universeId: validOrEmptyString(maybeUniverseId, validUniverseId)
---
+++ frontend/galactic-sovereign-frontend/src/routes/lobby/login/+page.server.ts
@@ -0,0 +1,100 @@
+import { error, redirect } from '@sveltejs/kit';
+import { resetGameCookies, setGameCookies, loadSessionCookies } from '$lib/cookies';
+import { fetchPlayerFromApiUser, responseToPlayerArray } from '$lib/game/players';
+import { getUniverses, responseToUniverseArray } from '$lib/game/universes';
+import { fetchPlanetsFromPlayer, responseToPlanetArray } from '$lib/game/planets';
+
+export async function load({ cookies }) {
+	resetGameCookies(cookies);
+
+	const universesResponse = await getUniverses();
+
+	if (universesResponse.error()) {
+		error(404, { message: universesResponse.failureMessage() });
+	}
+
+	const universes = responseToUniverseArray(universesResponse);
+
+	return {
+		universes: universes.map((u) => u.toJson())
+	};
+}
+
+export const actions = {
+	login: async ({ cookies, request }) => {
+		const [valid, sessionCookies] = loadSessionCookies(cookies);
+		if (!valid) {
+			redirect(303, '/login');
+		}
+
+		const data = await request.formData();
+
+		const universeId = data.get('universe');
+		const playerName = data.get('player');
+		if (!universeId) {
+			return {
+				success: false,
+				missing: true,
+				message: 'Please select a universe'
+			};
+		}
+		if (!playerName) {
+			return {
+				success: false,
+				missing: true,
+				message: 'Please choose a name'
+			};
+		}
+
+		const playerResponse = await fetchPlayerFromApiUser(
+			sessionCookies.apiUser,
+			sessionCookies.apiKey
+		);
+		if (playerResponse.error()) {
+			return {
+				success: false,
+				incorrect: true,
+				message: playerResponse.failureMessage()
+			};
+		}
+
+		const players = responseToPlayerArray(playerResponse);
+		const maybePlayer = players.find(
+			(player) => player.universe === universeId && player.name === playerName
+		);
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+		if (maybePlayer === undefined) {
+			return {
+				success: false,
+				incorrect: true,
+				message: 'No such player'
+			};
+		}
+
+		const planetsResponse = await fetchPlanetsFromPlayer(maybePlayer.id, sessionCookies.apiKey);
+		if (planetsResponse.error()) {
+			return {
+				success: false,
+				incorrect: true,
+				message: planetsResponse.failureMessage()
+			};
+		}
+
+		const planets = responseToPlanetArray(planetsResponse);
+
+		// https://stackoverflow.com/questions/35605548/get-first-object-from-array
+		const [maybePlanet] = planets;
+
+		if (maybePlanet === undefined) {
+			return {
+				success: false,
+				incorrect: true,
+				message: 'Player does not have any planet'
+			};
+		}
+
+		setGameCookies(cookies, maybePlayer);
+
+		redirect(303, '/planets/' + maybePlanet.id + '/overview');
+	}
+};
