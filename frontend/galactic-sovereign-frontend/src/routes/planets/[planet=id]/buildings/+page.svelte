5d415a3ba749c755566d83aecbbe1ce6ebe0645b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-11-02 16:20:59 +0100

    feat: Refactored the buildings page to not use legacy Svelte behavior

--- frontend/galactic-sovereign-frontend/src/routes/planets/[planet=id]/buildings/+page.svelte
+++ frontend/galactic-sovereign-frontend/src/routes/planets/[planet=id]/buildings/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { run } from 'svelte/legacy';
-
 	import heroImage, { GAME_HERO_IMAGE } from '$lib/stores/ui/heroImage';
 	import heroContainer, { GAME_HERO_CONTAINER_PROPS } from '$lib/stores/ui/heroContainer';
 	import pageTitle, { HOMEPAGE_TITLE } from '$lib/stores/ui/pageTitle';
@@ -16,55 +14,25 @@
 
 	import { invalidate } from '$app/navigation';
 
-	import { mapPlanetResourcesToUiResources } from '$lib/game/resources';
-	import { mapPlanetBuildingsToUiBuildings } from '$lib/game/buildings';
-	import { mapBuildingActionsToUiActions } from '$lib/game/actions.js';
-
 	// https://svelte.dev/blog/zero-config-type-safety
-	interface Props {
-		data: any;
-	}
-
-	let { data }: Props = $props();
+	let { data } = $props();
 
 	// https://stackoverflow.com/questions/77047087/why-is-page-svelte-not-reloading-after-page-server-ts-executes-load-function
 	let playerName = $derived(data.playerName);
 	let planetName = $derived(data.planet.name);
 	let universeName = $derived(data.universe.name);
 
+	let resources = $derived(data.resources);
+	let buildings = $derived(data.buildings);
+	let buildingActions = $derived(data.buildingActions);
+
 	// https://stackoverflow.com/questions/75616911/sveltekit-fetching-on-the-server-and-updating-the-writable-store
 	heroImage.set(GAME_HERO_IMAGE);
 	heroContainer.set(GAME_HERO_CONTAINER_PROPS);
-	let title = $derived(HOMEPAGE_TITLE + ' - ' + planetName);
-	run(() => {
-		pageTitle.set(title);
-	});
-	run(() => {
-		activeScreen.set('buildings');
-	});
+	pageTitle.set(data.pageTitle);
+	activeScreen.set('buildings');
 
-	let resources = $derived(
-		mapPlanetResourcesToUiResources(
-			data.planet.resources,
-			data.planet.productions,
-			data.planet.storages,
-			data.resources
-		)
-	);
-	let buildings = $derived(
-		mapPlanetBuildingsToUiBuildings(
-			data.planet.id,
-			data.planet.buildings,
-			data.planet.buildingActions,
-			data.buildings,
-			data.resources
-		)
-	);
-	let actions = $derived(
-		mapBuildingActionsToUiActions(data.planet.buildingActions, data.buildings)
-	);
-
-	let anyBuildingActionRunning = $derived(data.planet.buildingActions.length !== 0);
+	let anyBuildingActionRunning = $derived(data.buildingActions.length !== 0);
 
 	function onActionCompleted() {
 		invalidate('data:planet');
@@ -89,8 +57,8 @@
 	<FlexContainer align={'stretch'}>
 		<StyledTitle text="Actions running on {planetName}" />
 		<FlexContainer vertical={false} justify={'start'} align={'start'} styling={'flex-wrap'}>
-			{#each actions as action}
-				<BuildingAction {action} onCompleted={onActionCompleted} />
+			{#each buildingActions as buildingAction}
+				<BuildingAction action={buildingAction} onCompleted={onActionCompleted} />
 			{/each}
 		</FlexContainer>
 	</FlexContainer>
