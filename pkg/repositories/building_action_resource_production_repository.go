1172cf41375a1cbbaea8f50b512b8a222eecd48b
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-12-05 23:08:44 +0100

    feat: Used backend-toolkit/pkg/db in building action resource production building

--- pkg/repositories/building_action_resource_production_repository.go
+++ pkg/repositories/building_action_resource_production_repository.go
@@ -3,8 +3,7 @@ package repositories
 import (
 	"context"
 
-	"github.com/KnoblauchPilze/backend-toolkit/pkg/errors"
-	"github.com/KnoblauchPilze/galactic-sovereign/pkg/db"
+	"github.com/KnoblauchPilze/backend-toolkit/pkg/db"
 	"github.com/KnoblauchPilze/galactic-sovereign/pkg/persistence"
 	"github.com/google/uuid"
 )
@@ -29,10 +28,6 @@ INSERT INTO
 
 func (r *buildingActionResourceProductionRepositoryImpl) Create(ctx context.Context, tx db.Transaction, production persistence.BuildingActionResourceProduction) (persistence.BuildingActionResourceProduction, error) {
 	_, err := tx.Exec(ctx, createBuildingActionResourceProductionSqlTemplate, production.Action, production.Resource, production.Production)
-	if err != nil && duplicatedKeySqlErrorRegexp.MatchString(err.Error()) {
-		return production, errors.NewCode(db.DuplicatedKeySqlKey)
-	}
-
 	return production, err
 }
 
@@ -47,31 +42,10 @@ WHERE
 	action = $1`
 
 func (r *buildingActionResourceProductionRepositoryImpl) ListForAction(ctx context.Context, tx db.Transaction, action uuid.UUID) ([]persistence.BuildingActionResourceProduction, error) {
-	res := tx.Query(ctx, listBuildingActionResourceProductionForActionSqlTemplate, action)
-	if err := res.Err(); err != nil {
-		return []persistence.BuildingActionResourceProduction{}, err
-	}
-
-	var out []persistence.BuildingActionResourceProduction
-	parser := func(rows db.Scannable) error {
-		var production persistence.BuildingActionResourceProduction
-		err := rows.Scan(&production.Action, &production.Resource, &production.Production)
-		if err != nil {
-			return err
-		}
-
-		out = append(out, production)
-		return nil
-	}
-
-	if err := res.GetAll(parser); err != nil {
-		return []persistence.BuildingActionResourceProduction{}, err
-	}
-
-	return out, nil
+	return db.QueryAllTx[persistence.BuildingActionResourceProduction](ctx, tx, listBuildingActionResourceProductionForActionSqlTemplate, action)
 }
 
-const deleteBuildingActionResourceProductionForActionSqlTemplate = "DELETE FROM building_action_resource_production WHERE action = $1"
+const deleteBuildingActionResourceProductionForActionSqlTemplate = `DELETE FROM building_action_resource_production WHERE action = $1`
 
 func (r *buildingActionResourceProductionRepositoryImpl) DeleteForAction(ctx context.Context, tx db.Transaction, action uuid.UUID) error {
 	_, err := tx.Exec(ctx, deleteBuildingActionResourceProductionForActionSqlTemplate, action)
