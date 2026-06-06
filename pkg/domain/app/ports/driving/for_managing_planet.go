51c9d8471f26662d14ce34499c833e94e238262f
Knoblauchpilze <prolific-programmer@tuta.com>
2026-06-06 15:42:31 +0200

    feat: Driving port to manage planet

---
+++ pkg/domain/app/ports/driving/for_managing_planet.go
@@ -0,0 +1,18 @@
+package drivingports
+
+import (
+	"context"
+
+	"github.com/Knoblauchpilze/galactic-sovereign/pkg/domain/app/models"
+	"github.com/Knoblauchpilze/galactic-sovereign/pkg/domain/app/models/request"
+	"github.com/google/uuid"
+)
+
+type ForManagingPlanet interface {
+	Create(ctx context.Context, req request.PlanetCreationRequest) (models.Planet, error)
+	Get(ctx context.Context, id uuid.UUID) (models.Planet, error)
+	List(ctx context.Context) ([]models.Planet, error)
+	ListForPlayer(ctx context.Context, player uuid.UUID) ([]models.Planet, error)
+	Delete(ctx context.Context, id uuid.UUID) error
+	DeleteForPlayer(ctx context.Context, player uuid.UUID) error
+}
