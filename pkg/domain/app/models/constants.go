8759186db9c6c9f2709ea31af6dc88a8daf86083
Knoblauchpilze <prolific-programmer@tuta.com>
2026-06-17 08:03:25 +0200

    feat: Extracted constants in a dedicated file

---
+++ pkg/domain/app/models/constants.go
@@ -0,0 +1,9 @@
+package models
+
+import "github.com/google/uuid"
+
+var (
+	// Keep in sync with the values in 100_seed_game_data.up.sql
+	metalResourceId   = uuid.MustParse("b4419b6b-b3bf-4576-aa92-055283addbc8")
+	crystalResourceId = uuid.MustParse("cd2ac9aa-9968-4ff5-b746-88f1f810fbb3")
+)
