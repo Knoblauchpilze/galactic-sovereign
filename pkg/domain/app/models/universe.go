c5dd4b5262efabbf144c732ab60050d27b1d89c5
Knoblauchpilze <prolific-programmer@tuta.com>
2026-06-22 21:03:15 +0200

    feat: Universe domain model allows to create a planet

--- pkg/domain/app/models/universe.go
+++ pkg/domain/app/models/universe.go
@@ -17,3 +17,59 @@ type Universe struct {
 	Resources []Resource
 	Buildings []Building
 }
+
+func (u Universe) CreatePlanet(player uuid.UUID, homeworld bool) Planet {
+	createdAt := time.Now()
+	planetResources := make([]PlanetResource, 0, len(u.Resources))
+	planetStorages := make([]PlanetResourceStorage, 0, len(u.Resources))
+	planetProductions := make([]PlanetResourceProduction, 0, len(u.Resources))
+	planetBuildings := make([]PlanetBuilding, 0, len(u.Buildings))
+
+	for _, r := range u.Resources {
+		pr := PlanetResource{
+			Resource: r.Id,
+			Amount:   float64(r.StartAmount),
+		}
+		planetResources = append(planetResources, pr)
+
+		ps := PlanetResourceStorage{
+			Resource: r.Id,
+			Storage:  r.StartStorage,
+		}
+		planetStorages = append(planetStorages, ps)
+
+		pp := PlanetResourceProduction{
+			Resource:   r.Id,
+			Production: r.StartProduction,
+		}
+		planetProductions = append(planetProductions, pp)
+	}
+
+	for _, b := range u.Buildings {
+		pb := PlanetBuilding{
+			Building: b.Id,
+			Level:    0,
+		}
+		planetBuildings = append(planetBuildings, pb)
+	}
+
+	name := homeworldDefaultName
+	if !homeworld {
+		name = planetDefaultName
+	}
+
+	return Planet{
+		Id:             uuid.New(),
+		Player:         player,
+		Name:           name,
+		Homeworld:      homeworld,
+		CreatedAt:      createdAt,
+		UpdatedAt:      createdAt,
+		Version:        0,
+		Resources:      planetResources,
+		Storages:       planetStorages,
+		Productions:    planetProductions,
+		Buildings:      planetBuildings,
+		BuildingAction: nil,
+	}
+}
---
+++ pkg/domain/app/models/universe_test.go
@@ -0,0 +1,181 @@
+package models
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnit_Universe_CreatePlanet(t *testing.T) {
+	playerId := uuid.New()
+
+	t.Run("creates a homeworld belonging to the player", func(t *testing.T) {
+		u := Universe{
+			Id:        uuid.New(),
+			Resources: generateTestResources(),
+			Buildings: generateTestBuildings(),
+		}
+
+		beforeCreation := time.Now()
+		actual := u.CreatePlanet(playerId, true)
+
+		assert.Equal(t, playerId, actual.Player)
+		assert.Equal(t, "homeworld", actual.Name)
+		assert.True(t, actual.Homeworld)
+		assert.True(t, beforeCreation.Before(actual.CreatedAt))
+		assert.Equal(t, actual.CreatedAt, actual.UpdatedAt)
+		assert.Zero(t, actual.Version)
+		assert.Nil(t, actual.BuildingAction)
+	})
+
+	t.Run("creates a colony belonging to the player", func(t *testing.T) {
+		u := Universe{
+			Id:        uuid.New(),
+			Resources: generateTestResources(),
+			Buildings: generateTestBuildings(),
+		}
+
+		beforeCreation := time.Now()
+		actual := u.CreatePlanet(playerId, false)
+
+		assert.Equal(t, playerId, actual.Player)
+		assert.Equal(t, "colony", actual.Name)
+		assert.False(t, actual.Homeworld)
+		assert.True(t, beforeCreation.Before(actual.CreatedAt))
+		assert.Equal(t, actual.CreatedAt, actual.UpdatedAt)
+		assert.Zero(t, actual.Version)
+		assert.Nil(t, actual.BuildingAction)
+	})
+
+	t.Run("assigns start amount for each resource", func(t *testing.T) {
+		u := Universe{
+			Id:        uuid.New(),
+			Resources: generateTestResources(),
+			Buildings: generateTestBuildings(),
+		}
+
+		actual := u.CreatePlanet(playerId, false)
+
+		expected := []PlanetResource{
+			{
+				Resource: metalResourceId,
+				Amount:   145,
+			},
+			{
+				Resource: crystalResourceId,
+				Amount:   325,
+			},
+		}
+		assert.Equal(t, expected, actual.Resources)
+	})
+
+	t.Run("assigns start storage for each resource", func(t *testing.T) {
+		u := Universe{
+			Id:        uuid.New(),
+			Resources: generateTestResources(),
+			Buildings: generateTestBuildings(),
+		}
+
+		actual := u.CreatePlanet(playerId, false)
+
+		expected := []PlanetResourceStorage{
+			{
+				Resource: metalResourceId,
+				Storage:  123,
+			},
+			{
+				Resource: crystalResourceId,
+				Storage:  421,
+			},
+		}
+		assert.Equal(t, expected, actual.Storages)
+	})
+
+	t.Run("assigns start production for each resource", func(t *testing.T) {
+		u := Universe{
+			Id:        uuid.New(),
+			Resources: generateTestResources(),
+			Buildings: generateTestBuildings(),
+		}
+
+		actual := u.CreatePlanet(playerId, false)
+
+		expected := []PlanetResourceProduction{
+			{
+				Resource:   metalResourceId,
+				Production: 985,
+			},
+			{
+				Resource:   crystalResourceId,
+				Production: 752,
+			},
+		}
+		assert.Equal(t, expected, actual.Productions)
+	})
+
+	t.Run("creates each building with level 0", func(t *testing.T) {
+		u := Universe{
+			Id:        uuid.New(),
+			Resources: generateTestResources(),
+			Buildings: generateTestBuildings(),
+		}
+
+		actual := u.CreatePlanet(playerId, false)
+
+		expected := []PlanetBuilding{
+			{
+				Building: u.Buildings[0].Id,
+				Level:    0,
+			},
+			{
+				Building: u.Buildings[1].Id,
+				Level:    0,
+			},
+		}
+		assert.Equal(t, expected, actual.Buildings)
+	})
+}
+
+func generateTestResources() []Resource {
+	return []Resource{
+		{
+			Id:              metalResourceId,
+			StartAmount:     145,
+			StartStorage:    123,
+			StartProduction: 985,
+		},
+		{
+			Id:              crystalResourceId,
+			StartAmount:     325,
+			StartStorage:    421,
+			StartProduction: 752,
+		},
+	}
+}
+
+func generateTestBuildings() []Building {
+	return []Building{
+		{
+			Id: uuid.New(),
+			Costs: []BuildingCost{
+				{
+					Resource: metalResourceId,
+					Cost:     550,
+					Progress: 26.4,
+				},
+			},
+		},
+		{
+			Id: uuid.New(),
+			Storages: []BuildingResourceStorage{
+				{
+					Resource: metalResourceId,
+					Scale:    1500.0,
+					Progress: 26.4,
+				},
+			},
+		},
+	}
+}
