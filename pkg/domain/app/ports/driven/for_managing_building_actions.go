c47d8da410840ad8a0cf48aa92737b0095d0b2ac
Knoblauchpilze <prolific-programmer@tuta.com>
2026-07-02 08:19:06 +0200

    chore: Deprecation TODO

--- pkg/domain/app/ports/driven/for_managing_building_actions.go
+++ pkg/domain/app/ports/driven/for_managing_building_actions.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Knoblauchpilze/galactic-sovereign/pkg/domain/app/models"
 )
 
+// TODO: This can be removed and replaced by a mutation of the planet
 type ForManagingBuildingActions interface {
 	Create(ctx context.Context, planet models.Planet) error
 	Delete(ctx context.Context, planet models.Planet) error
