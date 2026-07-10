6b544053ebdea2b34aa8fd3abc6775070ea6962e
Knoblauchpilze <prolific-programmer@tuta.com>
2026-07-10 18:30:36 +0200

    chore: Linting error

--- pkg/domain/app/usecases/manage_planet_test.go
+++ pkg/domain/app/usecases/manage_planet_test.go
@@ -23,8 +23,6 @@ var (
 	t3 = time.Date(2026, time.July, 3, 17, 58, 31, 0, time.UTC)
 	t4 = time.Date(2026, time.July, 3, 18, 58, 31, 0, time.UTC)
 
-	someTime = time.Date(2026, time.June, 25, 22, 22, 49, 0, time.UTC)
-
 	metalResourceId   = uuid.MustParse("b4419b6b-b3bf-4576-aa92-055283addbc8")
 	crystalResourceId = uuid.MustParse("cd2ac9aa-9968-4ff5-b746-88f1f810fbb3")
 
