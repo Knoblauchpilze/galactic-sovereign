44fb7644f8ca55e5d52b2ce5b3b9fc3992b877b9
Knoblauchpilze <prolific-programmer@tuta.com>
2026-08-30 15:12:41 +0200

    chore: Removed unnecessary constants

--- pkg/domain/app/models/universe_test.go
+++ pkg/domain/app/models/universe_test.go
@@ -8,6 +8,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	metalResourceId   = uuid.MustParse("b4419b6b-b3bf-4576-aa92-055283addbc8")
+	crystalResourceId = uuid.MustParse("cd2ac9aa-9968-4ff5-b746-88f1f810fbb3")
+)
+
 func TestUnit_Universe_CreatePlanet(t *testing.T) {
 	playerId := uuid.New()
 
